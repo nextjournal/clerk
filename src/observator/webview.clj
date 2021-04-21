d6c86bc857460145f921445faaa60b4ca01aebab
Jack Rusher <jack@rusher.com>
2021-04-21 09:49:38 +0200

    ein bisschen Stil

--- src/observator/webview.clj
+++ src/observator/webview.clj
@@ -3,6 +3,7 @@
 
   Mostly taken from https://gist.github.com/jackrusher/626e0d97282c089cf56e"
   (:require [clojure.string :as str]
+            [glow.core :as glow]
             [observator.lib :as obs.lib]
             [observator.core :as obs]
             [observator.hashing :as hashing])
@@ -71,8 +72,49 @@
 (defonce frame (setup-web-view!))
 
 (defn +html-head [html]
-  (str "<html><head>"
-       "</head><body>" html "</body></html>"))
+  (str "<html><head><style>"
+       "
+body {
+  font: 20px Georgia;
+  padding: 0.5em;
+}
+pre {
+  font: 16px 'Fira Code';  
+}
+.code {
+  background-color: rgb(245, 245, 245);
+  padding: 0.5em;
+}
+.result {
+  padding-left: 0.5em;
+}
+
+span.background { color: #f6f6f6; } /* not used? */
+span.s-exp { color: #777777; }
+span.reader-char { color: #777777; }
+span.regex { color: #777777; }
+span.comment { color: #777777; }
+span.number { color: #777777; }
+
+span.definition { color: #DE5772; }
+span.special-form { color: #DE5772; }
+span.macro { color: #DE5772; }
+
+span.keyword { color: #BF60B4; }
+span.nil { color: #BF60B4; }
+span.boolean { color: #BF60B4; }
+
+span.core-fn { color: #9085DA; }
+span.symbol { color: #9085DA; }
+
+span.repeat { color: #2CAB76; }
+span.exception { color: #2CAB76; }
+span.conditional { color: #2CAB76; }
+span.character { color: #2CAB76; }
+
+span.string { color: #C7877B; }
+span.variable { color: #268bd2; }"
+       "</style></head><body>" html "</body></html>"))
 
 (defn file->html
   [file]
@@ -81,10 +123,10 @@
          (hashing/parse-file {:markdown? true})
          (map (fn [{:keys [type text]}]
                 (cond-> (case type
-                          :code (str "<pre>" text "</pre>")
+                          :code (str "<pre class='code'>" (glow/highlight-html text) "</pre>")
                           :markdown (obs/md->html text))
                   (= :code type)
-                  (str "<pre>" (obs/format-eval-output (obs/read+eval-cached var->hash text)) "</pre>"))))
+                  (str "<pre class='result'>" (obs/format-eval-output (obs/read+eval-cached var->hash text)) "</pre>"))))
          str/join
          +html-head)))
 
