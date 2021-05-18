f12cb34655843ed200be48c3a08492b18b1fa8a7
Martin Kavalar <mk@katercalling.com>
2021-05-18 21:00:52 +0200

    Add more samples to how it works notebook

--- notebooks/how_clerk_works.clj
+++ notebooks/how_clerk_works.clj
@@ -2,10 +2,18 @@
 ;; Clerk uses static analysis and dataflow to avoid needless recomputation.
 ;; * we use `rewrite-clj` to parse the notebook file
 ;; * each form is analysed using `tools.analyzer`
-(require '[nextjournal.clerk.hashing :as h]
-         '[clojure.string :as str])
+(ns how-clerk-works
+  (:require [nextjournal.clerk.hashing :as h]
+            [weavejester.dependency :as dep])
+  (:import (io.methvin.watcher DirectoryChangeEvent)
+           (java.util UUID)))
+
 
 ;; We also look at where a given symbol is coming from, this can be
 ;; * from Clojure source either form a jar or from the classpath
 ;; * from a java class in a jar
 ;; * built-in to the JDK
+(comment ;; TODO fix error
+  (into {}
+        (map (juxt identity h/find-qlocation))
+        [#'h/find-location #'inc #'dep/depend DirectoryChangeEvent UUID]))
