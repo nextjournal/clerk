7e1b1f240c95201f90e1bafd81694403da104725
Dieter Komendera <dieter@komendera.com>
2021-05-20 12:04:51 +0200

    Add a toggle-dev! fn to user.clj for easy switching between prod and dev js and css

--- src/user.clj
+++ src/user.clj
@@ -1,9 +1,15 @@
 (ns user)
 
 (require '[nextjournal.clerk :as clerk :refer [show!]]
+         '[nextjournal.clerk.view]
          '[nextjournal.clerk.webserver :as webserver])
 
 (webserver/start! {})
 
+(defn toggle-dev! []
+  (alter-var-root #'nextjournal.clerk.view/live-js? not))
+
 (comment
+  (toggle-dev!)
+
   (show! "notebooks/elements.clj"))
