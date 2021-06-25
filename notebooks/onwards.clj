a5d25731f0917d692cfdcb9fa3e932002ce7b0fb
Martin Kavalar <mk@katercalling.com>
2021-06-25 15:12:40 +0200

    Update onwards notebook

--- notebooks/onwards.clj
+++ notebooks/onwards.clj
@@ -1,24 +1,21 @@
 ;; # Onwards 🏔
 ;; Notes about what currently breaks 💥 and what could be better tomorrow.
-(ns errors)
-
 ;; - [x] Lists spacing is broken
 ;; - [x] Nested lists are not formatted correctly
 ;; - [x] Errors are global, but should be local
 ;; - [x] Printing a var
+;; - [x] Switch to fast serialization using Nippy
+;; - [x] Store results as CAS files, only write when needed
 ;; - [ ] Don't thaw result if it's already in memory
 ;; - [ ] Don't cache things that are fast to evaluate
 ;; - [ ] Figure out distributed caching using CAS + cloud bucket
 ;; - [ ] Markdown should support Nextjournal Flavored Markdown including:
 ;;   - [ ] TODO lists 😹
 ;;   - [ ] Inline Formulas $E^2=m^2+\vec{p}^2$
-;; - [ ] Error display
+;; - [ ] Better Error display
 ;; - [x] Fix jar hashing (contents, not filename)
 ;; - [x] Printing of very large values
+;; - [ ] Status log
 ;; - [ ] Datafy + Nav
 ;; - [ ] Allow to pin notebook
 ;; - [ ] Make viewer api open with predicates
-
-
-;; Error: _No reader function for tag namespace._ Splice the comment to see the error.
-(find-ns 'clojure.core)
