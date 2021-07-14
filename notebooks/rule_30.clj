6f79787ad45ed20c2751452899cdf42fe05eac55
Martin Kavalar <mk@katercalling.com>
2021-07-14 10:39:05 +0200

    Move Rule 30 viewers to the top

--- notebooks/rule_30.clj
+++ notebooks/rule_30.clj
@@ -3,6 +3,16 @@
 (ns rule-30
   (:require [nextjournal.viewer :as v]))
 
+(v/register-viewers!
+ {:number #(v/html [:div.inline-block {:class (if (zero? %)
+                                                "bg-white border-solid border-2 border-black"
+                                                "bg-black")
+                                       :style {:width 16 :height 16}}])
+  :vector (fn [x options]
+            (v/html (into [:div.flex.inline-flex] (map (partial v/inspect options)) x)))
+  :list (fn [x options]
+          (v/html (into [:div.flex.flex-col] (map (partial v/inspect options)) x)))})
+
 ;; Now let's define Rule 30 as a map. It maps a vector of three cells to a new value for a cell. Notice how the map viewer can be used as-is and uses our number and vector viewers.
 (def rule-30
   {[1 1 1] 0
@@ -22,14 +32,3 @@
 ;; Finally, we can evolve the board.
 (let [evolve #(mapv rule-30 (partition 3 1 (repeat 0) (cons 0 %)))]
   (->> first-generation (iterate evolve) (take 17)))
-
-
-(v/register-viewers!
- {:number #(v/html [:div.inline-block {:class (if (zero? %)
-                                                "bg-white border-solid border-2 border-black"
-                                                "bg-black")
-                                       :style {:width 16 :height 16}}])
-  :vector (fn [x options]
-            (v/html (into [:div.flex.inline-flex] (map (partial v/inspect options)) x)))
-  :list (fn [x options]
-          (v/html (into [:div.flex.flex-col] (map (partial v/inspect options)) x)))})
