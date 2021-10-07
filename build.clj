227d4fb76a8dea7e7bd20789370b3a52295d7395
Martin Kavalar <mk@katercalling.com>
2021-10-07 16:20:42 +0200

    Clean before building jar in same jvm

--- build.clj
+++ build.clj
@@ -7,10 +7,8 @@
 (def basis (b/create-basis {:project "deps.edn"}))
 (def jar-file (format "target/%s-%s.jar" (name lib) version))
 
-(defn clean [_]
-  (b/delete {:path "target"}))
-
 (defn jar [_]
+  (b/delete {:path "target"})
   (println "Producing jar:" jar-file)
   (b/write-pom {:class-dir class-dir
                 :lib lib
