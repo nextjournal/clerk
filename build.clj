06a195fe97df504c1d90684ca18613cddbc4d82c
Martin Kavalar <mk@katercalling.com>
2021-10-07 14:42:07 +0200

    Build jar using tools.build

---
+++ build.clj
@@ -0,0 +1,23 @@
+(ns build
+  (:require [clojure.tools.build.api :as b]))
+
+(def lib 'io.github.nextjournal/clerk)
+(def version (format "0.1.%s" (b/git-count-revs nil)))
+(def class-dir "target/classes")
+(def basis (b/create-basis {:project "deps.edn"}))
+(def jar-file (format "target/%s-%s.jar" (name lib) version))
+
+(defn clean [_]
+  (b/delete {:path "target"}))
+
+(defn jar [_]
+  (println "Producing jar:" jar-file)
+  (b/write-pom {:class-dir class-dir
+                :lib lib
+                :version version
+                :basis basis
+                :src-dirs ["src"]})
+  (b/copy-dir {:src-dirs ["src"]
+               :target-dir class-dir})
+  (b/jar {:class-dir class-dir
+          :jar-file jar-file}))
