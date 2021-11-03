9e637f5eaff6e6ea037abdc2802a5c30046cf618
Philippa Markovics <philippa@markovics.com>
2021-11-03 12:09:07 +0100

    Make index work on mobile

--- src/nextjournal/clerk/static_app.cljs
+++ src/nextjournal/clerk/static_app.cljs
@@ -19,21 +19,21 @@
    [sci-viewer/root]])
 
 (defn index [_]
-  [:div.bg-gray-100.flex.items-center.justify-center.overflow-y-auto.w-screen.h-screen
-   [:div.mt-12 {:style {:min-width 500}}
+  [:div.bg-gray-100.flex.justify-center.overflow-y-auto.w-screen.h-screen.p-4.md:p-0
+   [:div.md:my-12.w-full.md:max-w-lg
     [:div.bg-white.shadow-lg.rounded-lg.border
-     [:div.px-8.py-3
+     [:div.px-4.md:px-8.py-3
       [:h1.text-xl "Clerk"]]
      (into [:ul]
            (map (fn [path]
                   [:li.border-t
-                   [:a.pl-8.pr-4.py-2.flex.w-full.items-center.justify-between.hover:bg-indigo-50
+                   [:a.pl-4.md:pl-8.pr-4.py-2.flex.w-full.items-center.justify-between.hover:bg-indigo-50
                     {:href (rfe/href ::show {:path path})}
-                    [:span.text-md.monospace.whitespace-nowrap path]
-                    [:svg.h-4.w-4 {:xmlns "http://www.w3.org/2000/svg" :fill "none" :viewBox "0 0 24 24" :stroke "currentColor"}
+                    [:span.text-sm.md:text-md.monospace.flex-auto.block.truncate path]
+                    [:svg.h-4.w-4.flex-shrink-0 {:xmlns "http://www.w3.org/2000/svg" :fill "none" :viewBox "0 0 24 24" :stroke "currentColor"}
                      [:path {:stroke-linecap "round" :stroke-linejoin "round" :stroke-width "2" :d "M9 5l7 7-7 7"}]]]]))
            (sort (keys @path->doc)))]
-    [:div.mt-4.text-xs.text-gray-400.sans-serif.px-8
+    [:div.my-4.md:mb-0.text-xs.text-gray-400.sans-serif.px-4.md:px-8
      [:a.hover:text-indigo-600
       {:href "https://github.com/nextjournal/clerk"}
       "Generated with Clerk."]]]])
