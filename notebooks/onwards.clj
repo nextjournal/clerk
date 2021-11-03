9f194d671fa8497ecb905a667cadca062950e833
Martin Kavalar <mk@katercalling.com>
2021-11-03 10:52:22 +0100

    Fix link style when code cell uses markdown viewer

--- notebooks/onwards.clj
+++ notebooks/onwards.clj
@@ -55,12 +55,12 @@
 ;; - [x] Limit description to make large datasets work
 ;; - [x] Debug & fix extra calls to inspect / rendering
 ;; - [x] Setup up static builds
+;; - [x] Fix [link](#) style when code cell uses markdown viewer
 ;; - [ ] Do pass over README
 ;; - [x] Add CHANGELOG
 ;; - [ ] Release 0.2
 ;; - [ ] Make github repo public
 ;; ## 🛠 Up Next
-;; - [ ] Fix [link](#) style
 ;; - [ ] Let consumers (SICMUitls) extend sci ns by putting it into an atom
 ;; - [ ] Support setting `:nextjournal.clerk/no-cache` on namespaces as well
 ;; - [ ] Support setting `:nextjournal.clerk/cache-dir` on namespace
