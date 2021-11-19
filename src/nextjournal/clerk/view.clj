b32427530ed8b0342a1bb28bdc3f3c4324b127c9
Martin Kavalar <mk@katercalling.com>
2021-11-19 16:43:15 +0100

    Bump viewers with inline latex fix, simplify deps, add sicmutils nb

--- src/nextjournal/clerk/view.clj
+++ src/nextjournal/clerk/view.clj
@@ -44,7 +44,6 @@
 
 (defn ->result [ns {:keys [result blob-id]} lazy-load?]
   (let [described-result (v/describe result {:viewers (v/get-viewers ns (v/viewers result))})]
-    (prn :->result blob-id result)
     (merge {:nextjournal/viewer :clerk/result
             :nextjournal/value (cond-> (try {:nextjournal/edn (->edn described-result)}
                                             (catch Throwable _e
@@ -89,7 +88,7 @@
 (def resource->static-url
   {"/css/app.css" "https://storage.googleapis.com/nextjournal-cas-eu/data/8VxQBDwk3cvr1bt8YVL5m6bJGrFEmzrSbCrH1roypLjJr4AbbteCKh9Y6gQVYexdY85QA2HG5nQFLWpRp69zFSPDJ9"
    "/css/viewer.css" "https://storage.googleapis.com/nextjournal-cas-eu/data/8VxoxUgsBRs2yjjBBcfeCc8XigM7erXHmjJg2tjdGxNBxwTYuDonuYswXqRStaCA2b3rTEPCgPwixJmAVrea1qAHHU"
-   "/js/viewer.js" "https://storage.googleapis.com/nextjournal-cas-eu/data/8VvQsUZ6bR3VAdZzpqUSuV2QRWn3bpg3nxMn1Zf2FikwxTA3yku4orUucwZ87AetKrbVsJVJmF4QPaHNa2fFGhybNS"})
+   "/js/viewer.js" "https://storage.googleapis.com/nextjournal-cas-eu/data/8VtPHHm4km9f42PFm17NUPos3x4WNsFPk928DUfYYip6AkugxsKsjyWY6Kz7jpykxk77YvXHSgCMG4p28Ew1QF5Fd4"})
 
 (defn ->html [{:keys [conn-ws? live-js?] :or {conn-ws? true live-js? live-js?}} doc]
   (hiccup/html5
