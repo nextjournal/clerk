9319fa32858a7dfa130395a0c9ab44a4338ff6a5
Martin Kavalar <mk@katercalling.com>
2021-11-26 12:23:37 +0100

    Bump viewers with spacing fix between results and folded code cells
    https://github.com/nextjournal/viewers/commit/0ebf86bbb457f819a6f9e8b5a33f34fc62c5b2e5

--- src/nextjournal/clerk/view.clj
+++ src/nextjournal/clerk/view.clj
@@ -126,7 +126,7 @@
 
 (def resource->static-url
   {"/css/app.css" "https://storage.googleapis.com/nextjournal-cas-eu/data/8VxQBDwk3cvr1bt8YVL5m6bJGrFEmzrSbCrH1roypLjJr4AbbteCKh9Y6gQVYexdY85QA2HG5nQFLWpRp69zFSPDJ9"
-   "/css/viewer.css" "https://storage.googleapis.com/nextjournal-cas-eu/data/8Vx9d2KqsZ38vfuiaUYDCMhns2zK5YPJ4JDRHuekrb8udKgLEM7SLfzoBhtEAkHZwspwmaDm19ZUrPzwfQUtPswF1P"
+   "/css/viewer.css" "https://storage.googleapis.com/nextjournal-cas-eu/data/8VvykE47cdahchdt8fxwHyYwJ7YSmEFcMSyqf4UNs61izpuF1xXpKA4HeZQctDkkU11B5iLVSBjpCQrk5f5mWXS9xv"
    "/js/viewer.js" "https://storage.googleapis.com/nextjournal-cas-eu/data/8VwyMcfayNcoRj7RZhzLTBGyS2hAtk8Teieo3s5GyiNooj4R5JhZdgZ3GuNnSAye7STHZKzhUTfrSsVbEZx79JEFc2"})
 
 (defn ->html [{:keys [conn-ws? live-js?] :or {conn-ws? true live-js? live-js?}} doc]
