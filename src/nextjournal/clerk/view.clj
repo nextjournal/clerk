a349fe8c30586b64ed88ed52fd7b78d92dca30a6
Martin Kavalar <mk@katercalling.com>
2021-12-15 09:59:00 +0100

    Drop website.cljs

--- src/nextjournal/clerk/view.clj
+++ src/nextjournal/clerk/view.clj
@@ -161,7 +161,7 @@
 (def resource->static-url
   {"/css/app.css" "https://storage.googleapis.com/nextjournal-cas-eu/data/8VxQBDwk3cvr1bt8YVL5m6bJGrFEmzrSbCrH1roypLjJr4AbbteCKh9Y6gQVYexdY85QA2HG5nQFLWpRp69zFSPDJ9"
    "/css/viewer.css" "https://storage.googleapis.com/nextjournal-cas-eu/data/8VvykE47cdahchdt8fxwHyYwJ7YSmEFcMSyqf4UNs61izpuF1xXpKA4HeZQctDkkU11B5iLVSBjpCQrk5f5mWXS9xv"
-   "/js/viewer.js" "https://storage.googleapis.com/nextjournal-cas-eu/data/8Vw6v2sTz9kw13m4gPDmVb2xp21UdPgKsWLTpizVSS5JUP7Pi8qt1hZxavUAWuSPvBE3ugLtdjiU1TTnvkQm9EMK55"})
+   "/js/viewer.js" "https://storage.googleapis.com/nextjournal-cas-eu/data/8VvSMJBSzXtEZMB7YXPbqQMqyCDnduvmdn9zgKnVHFhXPJkFopGHSp5X44rHJDMW6nFwF5ug4n2CbqBGi2ABNkQhU4"})
 
 (defn ->html [{:keys [conn-ws? live-js?] :or {conn-ws? true live-js? live-js?}} state]
   (hiccup/html5
@@ -169,7 +169,8 @@
     [:meta {:charset "UTF-8"}]
     [:meta {:name "viewport" :content "width=device-width, initial-scale=1"}]
     (hiccup/include-css "https://cdn.jsdelivr.net/npm/katex@0.13.13/dist/katex.min.css")
-    (hiccup/include-css (cond-> "/css/app.css"    (not live-js?) resource->static-url))
+    (hiccup/include-js "https://cdn.tailwindcss.com/")
+    #_(hiccup/include-css (cond-> "/css/app.css"    (not live-js?) resource->static-url))
     (hiccup/include-css (cond-> "/css/viewer.css" (not live-js?) resource->static-url))
     (hiccup/include-js  (cond-> "/js/viewer.js"   (not live-js?) resource->static-url))]
    [:body
