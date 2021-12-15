ea47d86ad4980a8a3d9340ca402e2ed675714808
Andrea Amantini <lo.zampino@gmail.com>
2021-12-15 16:23:01 +0100

    Bump js

--- src/nextjournal/clerk/view.clj
+++ src/nextjournal/clerk/view.clj
@@ -161,7 +161,7 @@
 (def resource->static-url
   {"/css/app.css" "https://storage.googleapis.com/nextjournal-cas-eu/data/8VxQBDwk3cvr1bt8YVL5m6bJGrFEmzrSbCrH1roypLjJr4AbbteCKh9Y6gQVYexdY85QA2HG5nQFLWpRp69zFSPDJ9"
    "/css/viewer.css" "https://storage.googleapis.com/nextjournal-cas-eu/data/8VvykE47cdahchdt8fxwHyYwJ7YSmEFcMSyqf4UNs61izpuF1xXpKA4HeZQctDkkU11B5iLVSBjpCQrk5f5mWXS9xv"
-   "/js/viewer.js" "https://storage.googleapis.com/nextjournal-cas-eu/data/8VurV4Bq5fghN6heGyGriCMaFbWb3oTBm36Z9KSsdedq5p8eRumeGJZb48durKdyjVcr6dZRX7ECL71XCwNxfoWSd1"})
+   "/js/viewer.js" "https://storage.googleapis.com/nextjournal-cas-eu/data/8VviBLV3fGsusyA9jYmFoXieZoo9C6LcKVcq2VWZSGpsUC8p4LuMMbXDJVfDf2BckoTSpWnUQSJXUyU56XwVR9if7c"})
 
 (defn ->html [{:keys [conn-ws? live-js?] :or {conn-ws? true live-js? live-js?}} state]
   (hiccup/html5
