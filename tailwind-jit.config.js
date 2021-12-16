94966ce0679f27fcedb1c0e67639f7914b61302f
Andrea Amantini <lo.zampino@gmail.com>
2021-12-16 11:16:41 +0100

    Add a separate Tailwind JIT config and a new babashka task

---
+++ tailwind-jit.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  mode: 'jit',
+  purge: ["./public/build/index.html", "./public/build/**/*.html", "./build/viewer.js"],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
