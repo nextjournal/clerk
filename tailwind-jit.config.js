a0582324761c1ead53081877b2fff851f513d2b7
Andrea Amantini <lo.zampino@gmail.com>
2021-12-16 15:03:57 +0100

    Upgrade tailwind to v3

--- tailwind-jit.config.js
+++ tailwind-jit.config.js
@@ -1,6 +1,5 @@
 module.exports = {
-  mode: 'jit',
-  purge: ["./public/build/index.html", "./public/build/**/*.html", "./build/viewer.js"],
+  content: ["./public/build/index.html", "./public/build/**/*.html", "./build/viewer.js"],
   theme: {
     extend: {},
   },
