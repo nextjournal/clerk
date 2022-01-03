fe0c98ff8081fe6513e6fc88c10a76d02bea5adb
Andrea Amantini <lo.zampino@gmail.com>
2022-01-03 17:06:09 +0100

    Simplify assignment of closing parens
    By lifting opening and closing parens to the viewer map, see #45.

--- src/nextjournal/clerk/sci_viewer.cljs
+++ src/nextjournal/clerk/sci_viewer.cljs
@@ -185,8 +185,9 @@
   (map-indexed (fn [idx x]
                  (inspect (update opts :path conj idx) x))))
 
-(defn coll-viewer [{:keys [open close]} xs {:as opts :keys [path viewer !expanded-at]}]
-  (html (let [expanded? (@!expanded-at path)]
+(defn coll-viewer [xs {:as opts :keys [path viewer !expanded-at]}]
+  (html (let [expanded? (@!expanded-at path)
+              {:keys [opening-paren closing-paren]} viewer]
           [:span.inspected-value.whitespace-nowrap
            {:class (when expanded? "inline-flex")}
            [:span
@@ -194,12 +195,12 @@
              (when (< 1 (count xs))
                {:on-click (partial toggle-expanded !expanded-at path)
                 :class "cursor-pointer bg-indigo-50 hover:bg-indigo-100 hover:rounded-sm border-b border-gray-400 hover:border-gray-500"})
-             open]
+             opening-paren]
             (into [:<>]
                   (comp (inspect-children opts)
-                        (interpose (if expanded? [:<> [:br] nbsp (when (= 2 (count open)) nbsp)] " ")))
+                        (interpose (if expanded? [:<> [:br] nbsp (when (= 2 (count opening-paren)) nbsp)] " ")))
                   xs)
-            (into [:<>] (:closing-parens viewer close))]])))
+            (cond->> closing-paren (list? closing-paren) (into [:<>]))]])))
 
 (declare inspect-paginated)
 (dc/defcard coll-viewer
@@ -215,6 +216,20 @@
           [:div.mb-3.result-viewer
            [inspect-paginated coll]])))
 
+(dc/defcard coll-viewer-simple
+  "with a simple `inspect` and no `describe` we don't move closing parens to children"
+  (into [:div]
+        (for [coll [
+                    {:foo (into #{} (range 3))}
+                    {:foo {:bar (range 20)}}
+                    [1 [2]]
+                    [[1] 2]
+                    {:a "bar"  :c (range 10)}
+                    {:a "bar"  :c (range 10) :d 1}
+                    ]]
+          [:div.mb-3.result-viewer
+           [inspect coll]])))
+
 (defn elision-viewer [{:as fetch-opts :keys [remaining unbounded?]} _]
   (html [view-context/consume :fetch-fn
          (fn [fetch-fn]
@@ -227,7 +242,8 @@
                           (fetch-fn fetch-opts))} remaining (when unbounded? "+") (if (fn? fetch-fn) " more…" " more elided")])]))
 
 (defn map-viewer [xs {:as opts :keys [path viewer !expanded-at] :or {path []}}]
-  (html (let [expanded? (@!expanded-at path)]
+  (html (let [expanded? (@!expanded-at path)
+              {:keys [closing-paren]} viewer]
           [:span.inspected-value.whitespace-nowrap
            [:span.bg-opacity-70
             (when (expandable? xs)
@@ -238,7 +254,7 @@
                  (comp (inspect-children opts)
                        (interpose (if expanded? [:<> [:br] (repeat (inc (count path)) nbsp)] " ")))
                  xs)
-           (into [:<>] (:closing-parens viewer "}"))])))
+           (cond->> closing-paren (list? closing-paren) (into [:<>]))])))
 
 (defn string-viewer [s opts]
   (html (into [:span] (map #(cond->> % (not (string? %)) (inspect opts))) s)))
