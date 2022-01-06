cdf9b868be10718206d052b495d2c5bba9e6f303
Martin Kavalar <mk@katercalling.com>
2022-01-06 15:37:22 +0100

    Add deeply nested samples

---
+++ notebooks/deep.clj
@@ -0,0 +1,13 @@
+(ns ^:nextjournal.clerk/no-cache deep)
+
+(reduce (fn [acc i] (vector acc)) :fin (range 30 0 -1))
+
+(reduce (fn [acc i] (vector i acc)) (range 30 0 -1))
+
+(reduce (fn [acc i] (vector acc i)) (range 30 0 -1))
+
+(reduce (fn [acc i] (vector i [i (inc i)] acc)) (range 30 0 -1))
+
+(reduce (fn [acc i] (vector i acc (inc i))) :fin (range 30 0 -1))
+
+(reduce (fn [acc i] (vector #{i} i acc (inc i) #{(inc i)})) :fin (range 30 0 -1))
