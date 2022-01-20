8cbb055078eddc2a20a6c9ea5d78155b262a7988
Martin Kavalar <mk@katercalling.com>
2022-01-20 16:24:50 +0100

    Also allow to override css in static app

--- src/nextjournal/clerk/view.clj
+++ src/nextjournal/clerk/view.clj
@@ -170,22 +170,21 @@
 #_(nextjournal.clerk/show! "notebooks/test.clj")
 #_(nextjournal.clerk/show! "notebooks/visibility.clj")
 
-(defn include-tailwind-cdn []
-  (list
-   (hiccup/include-js "https://cdn.tailwindcss.com?plugins=typography")
-   [:script (-> (slurp (io/resource "stylesheets/tailwind.config.js"))
-                (str/replace  #"^module.exports" "tailwind.config")
-                (str/replace  #"require\(.*\)" ""))]
-   [:style {:type "text/tailwindcss"} (slurp (io/resource "stylesheets/viewer.css"))]))
+(defn include-viewer-css []
+  (if-let [css-url (@config/!resource->url "/css/viewer.css")]
+    (hiccup/include-css css-url)
+    (list (hiccup/include-js "https://cdn.tailwindcss.com?plugins=typography")
+          [:script (-> (slurp (io/resource "stylesheets/tailwind.config.js"))
+                       (str/replace  #"^module.exports" "tailwind.config")
+                       (str/replace  #"require\(.*\)" ""))]
+          [:style {:type "text/tailwindcss"} (slurp (io/resource "stylesheets/viewer.css"))])))
 
 (defn ->html [{:keys [conn-ws?] :or {conn-ws? true}} state]
   (hiccup/html5
    [:head
     [:meta {:charset "UTF-8"}]
     [:meta {:name "viewport" :content "width=device-width, initial-scale=1"}]
-    (if-let [css-url (@config/!resource->url "/css/viewer.css")]
-      (hiccup/include-css css-url)
-      (include-tailwind-cdn))
+    (include-viewer-css)
     (hiccup/include-css "https://cdn.jsdelivr.net/npm/katex@0.13.13/dist/katex.min.css")
     (hiccup/include-js (@config/!resource->url "/js/viewer.js"))
     (hiccup/include-css "https://cdn.jsdelivr.net/npm/katex@0.13.13/dist/katex.min.css")
@@ -207,7 +206,7 @@ window.ws_send = msg => ws.send(msg)")]]))
     [:title (or (and current-path (-> state :path->doc (get current-path) v/value :title)) "Clerk")]
     [:meta {:charset "UTF-8"}]
     [:meta {:name "viewport" :content "width=device-width, initial-scale=1"}]
-    (include-tailwind-cdn)
+    (include-viewer-css)
     (hiccup/include-js (@config/!resource->url "/js/viewer.js"))
     (hiccup/include-css "https://cdn.jsdelivr.net/npm/katex@0.13.13/dist/katex.min.css")
     (hiccup/include-css "https://fonts.googleapis.com/css2?family=Fira+Code:wght@400;700&family=Fira+Mono:wght@400;700&family=Fira+Sans+Condensed:ital,wght@0,700;1,700&family=Fira+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&family=PT+Serif:ital,wght@0,400;0,700;1,400;1,700&display=swap")]
