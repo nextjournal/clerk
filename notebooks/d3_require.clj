a09bb96fdde779355f5f69a3422df243634db1ef
Martin Kavalar <mk@katercalling.com>
2022-02-19 13:53:08 +0100

    Improve examaple

--- notebooks/d3_require.clj
+++ notebooks/d3_require.clj
@@ -1,14 +1,21 @@
+;; # Custom Viewers with d3-require
+^{:nextjournal.clerk/visibility #{:hide-ns}}
 (ns d3-require
   (:require [nextjournal.clerk :as clerk]))
 
-(clerk/with-viewer {:fetch-fn (fn [_ x] x)
-                    :render-fn '(fn [value]
-                                  (v/html
-                                   (when value
-                                     [d3-require/with {:package ["mermaid@8.14/dist/mermaid.js"]}
-                                      (fn [mermaid]
-                                        [:div {:ref (fn [el] (when el
-                                                               (.render mermaid (random-uuid) value (fn [svg] (set! (.-innerHTML el) svg)))))}])])))}
+;; This is a custom viewer for [Mermaid](https://mermaid-js.github.io/mermaid), a markdown-like syntax for creating diagrams from text. Note that this library isn't bundles with Clerk but we use a component based on [d3-require](https://github.com/d3/d3-require) to load it at runtime.
+(def mermaid {:pred string?
+              :fetch-fn (fn [_ x] x)
+              :render-fn '(fn [value]
+                            (v/html
+                             (when value
+                               [d3-require/with {:package ["mermaid@8.14/dist/mermaid.js"]}
+                                (fn [mermaid]
+                                  [:div {:ref (fn [el] (when el
+                                                         (.render mermaid (str (gensym)) value #(set! (.-innerHTML el) %))))}])])))})
+
+;; We can then use  the above viewer using `with-viewer`.
+(clerk/with-viewer mermaid
   "stateDiagram-v2
     [*] --> Still
     Still --> [*]
@@ -17,5 +24,3 @@
     Moving --> Still
     Moving --> Crash
     Crash --> [*]")
-
-#_(clerk/serve! {})
