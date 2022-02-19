a30e6cf71fa0c722c9e002e77e61b8d393d6da77
Martin Kavalar <mk@katercalling.com>
2022-02-19 13:31:31 +0100

    Change example to mermaid

--- notebooks/d3_require.clj
+++ notebooks/d3_require.clj
@@ -1,20 +1,21 @@
 (ns d3-require
   (:require [nextjournal.clerk :as clerk]))
 
-
 (clerk/with-viewer {:fetch-fn (fn [_ x] x)
                     :render-fn '(fn [value]
                                   (v/html
                                    (when value
-                                     [d3-require/with {:package ["vega-embed@6.11.1"]}
-                                      (fn [vega]
-                                        (let [embed (.-embed vega)]
-                                          [:div {:style {:overflow-x "auto"}} ;; TODO: figure out how to make options customizable
-                                           [:div.vega-lite {:ref #(when % (embed % (clj->js value) (clj->js {:actions false})))}]]))])))}
-  {:width 650 :height 400 :data {:url "https://vega.github.io/vega-datasets/data/us-10m.json"
-                                 :format {:type "topojson" :feature "counties"}}
-   :transform [{:lookup "id" :from {:data {:url "https://vega.github.io/vega-datasets/data/unemployment.tsv"}
-                                    :key "id" :fields ["rate"]}}]
-   :projection {:type "albersUsa"} :mark "geoshape" :encoding {:color {:field "rate" :type "quantitative"}}})
+                                     [d3-require/with {:package ["mermaid@8.14/dist/mermaid.js"]}
+                                      (fn [mermaid]
+                                        [:div {:ref (fn [el] (when el
+                                                               (.render mermaid (random-uuid) value (fn [svg] (set! (.-innerHTML el) svg)))))}])])))}
+  "stateDiagram-v2
+    [*] --> Still
+    Still --> [*]
+
+    Still --> Moving
+    Moving --> Still
+    Moving --> Crash
+    Crash --> [*]")
 
 #_(clerk/serve! {})
