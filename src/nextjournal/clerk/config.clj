6aa4d97270376a9bbbc6db444de81275b33b525b
Andrea Amantini <lo.zampino@gmail.com>
2022-02-22 16:41:37 +0100

    release js

--- src/nextjournal/clerk/config.clj
+++ src/nextjournal/clerk/config.clj
@@ -10,7 +10,7 @@
     (not= "false" prop)))
 
 (def default-resource-manifest
-  {"/js/viewer.js" "https://storage.googleapis.com/nextjournal-cas-eu/data/8VwKauX6JACEP3K6ahNmP5p1w7rWdhKzeGXCDrHMnJiVrUxHVxcm3Xj84K2r3fcAKWxMQKzqoFe92osgFEHCuKCtZC"})
+  {"/js/viewer.js" "https://storage.googleapis.com/nextjournal-cas-eu/data/8VtKKJsGSrxJXADLyQ6NLVfbXytpP4koTf3sfmxHtSoK4GtBjNu2qciFWJXPxc9H1oJJyS8gLJu9rDxM5Atj2mcU9y"})
 
 (def resource-manifest-from-props
   (when-let [prop (System/getProperty "clerk.resource_manifest")]
