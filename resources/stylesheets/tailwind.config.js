1a1aee07f1851847a0d6340ae6849e06649cc9f1
Martin Kavalar <mk@katercalling.com>
2022-03-02 15:26:36 +0100

    Fix tailwind darkmode toggle by adding dark class to safelist

--- resources/stylesheets/tailwind.config.js
+++ resources/stylesheets/tailwind.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   darkMode: "class",
   content: ["./public/build/index.html", "./public/build/**/*.html", "./build/viewer.js"],
+  safelist: ['dark'],
   theme: {
     extend: {},
     fontFamily: {
