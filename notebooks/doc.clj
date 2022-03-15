19c5059b3cd2709894bdeadbe327ef1fbaaa20b8
Martin Kavalar <mk@katercalling.com>
2022-03-15 15:42:37 +0100

    Simplify viewer notebook

--- notebooks/doc.clj
+++ notebooks/doc.clj
@@ -4,44 +4,38 @@
   (:require [clojure.string :as str]
             [nextjournal.clerk :as clerk]))
 
-(def var-meta-keys
-  #{:arglists :col :column :doc :end-col :end-row :file :line :name #_ :ns})
+#_(filter #(str/starts-with? % "nextjournal.clerk") (sort (map str (all-ns))))
 
+(defn var->doc-viewer
+  "Takes a clojure `var` and returns a Clerk viewer to display its documentation."
+  [var]
+  (let [{:keys [doc name arglists]} (meta var)]
+    (clerk/html
+     [:div.border-t-2.t-6.pt-3
+      [:h2.text-lg name]
+      (clerk/code (str/join "\n" (mapv (comp pr-str #(concat [name] %)) arglists)))
+      (when doc
+        (clerk/md doc))])))
 
-(defn var-meta->doc [{:keys [doc name arglists]}]
-  [:div.border-t-2.t-6.pt-3
-   [:h2.text-lg name]
-   (clerk/code (str/join "\n" (mapv (comp pr-str #(concat [name] %)) arglists)))
-   (when doc
-     (clerk/md doc))])
+(var->doc-viewer #'var->doc-viewer)
 
 (def var-doc-viewer {:pred ::clerk/var-from-def
-                     :transform-fn (comp clerk/html var-meta->doc meta ::clerk/var-from-def)})
+                     :transform-fn (comp var->doc-viewer ::clerk/var-from-def)})
 
-
-(defn namespace->doc [ns]
-  (clerk/html [:div.flex-auto.overflow-y-auto.p-6.text-sm
-               [:div.max-w-6xl.mx-auto
-                [:h1.text-2xl (ns-name ns)]
-                (when-let [doc (-> ns meta :doc)]
-                  [:div.mt-4.leading-normal.viewer-markdown.prose
-                   (clerk/md doc)])
-                (into [:<>]
-                      (map (comp var-meta->doc meta val))
-                      (into (sorted-map) (-> ns ns-publics)))]]))
-
-(defn hello "What a _wonderful_ world." []
-  :what?)
-
-(defn hello-again "What a _wonderful_ world, again."
-  ([] (hello-again "hi"))
-  ([s] s)
-  ([s a] s a))
-
-(clerk/html (var-meta->doc (meta #'hello-again)))
+(defn namespace->doc-viewer [ns]
+  (clerk/html
+   [:div.flex-auto.overflow-y-auto.p-6.text-sm
+    [:div.max-w-6xl.mx-auto
+     [:h1.text-2xl (ns-name ns)]
+     (when-let [doc (-> ns meta :doc)]
+       [:div.mt-4.leading-normal.viewer-markdown.prose
+        (clerk/md doc)])
+     (into [:<>]
+           (map (comp :nextjournal/value var->doc-viewer val))
+           (into (sorted-map) (-> ns ns-publics)))]]))
 
 (def ns-doc-viewer {:pred #(instance? clojure.lang.Namespace %)
-                :transform-fn namespace->doc})
+                    :transform-fn namespace->doc-viewer})
 
 (clerk/with-viewer ns-doc-viewer (find-ns 'nextjournal.clerk))
 
