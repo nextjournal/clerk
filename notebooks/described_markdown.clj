70c63057c5f3478157882cef11a6c53b50a1b5e9
Andrea Amantini <lo.zampino@gmail.com>
2022-03-23 13:05:31 +0100

    clearer separation of concerns between fetch-fn and transform-fn

--- notebooks/described_markdown.clj
+++ notebooks/described_markdown.clj
@@ -13,36 +13,43 @@
 (defn with-md-viewer [{:as node :keys [type]}]
   (v/wrap-value node (keyword "nextjournal.markdown" (name type))))
 
-;; We define a generic `fetch-fn` to drill down into the markdown structure. At each depth this produces the desired hiccup representation of the node and delegates to describe the representation of the children nodes:
+;; We define a generic `transform-fn` with 2 purposes:
+;; * produces the desired hiccup representation at each node and
+;; * delegates to describe the representation of the child nodes:
 
 (defn into-markup [mkup]
-  (fn [opts {:keys [text content]}]
-    (v/fetch-all opts (into mkup (if text [text] (map with-md-viewer content))))))
+  (fn [{:keys [text content]}] (into mkup (if text [text] (map with-md-viewer content)))))
 
 ^{::clerk/viewer :hide-result}
 (def md-viewers
   [{:name :nextjournal.markdown/doc
-    :fetch-fn (into-markup [:div.viewer-markdown])
+    :transform-fn (into-markup [:div.viewer-markdown])
+    :fetch-fn v/fetch-all
     :render-fn 'v/html}
 
    {:name :nextjournal.markdown/heading
-    :fetch-fn (into-markup [:h1.foo])
+    :transform-fn (into-markup [:h1.foo])
+    :fetch-fn v/fetch-all
     :render-fn 'v/html}
 
    {:name :nextjournal.markdown/paragraph
-    :fetch-fn (into-markup [:p])
+    :transform-fn (into-markup [:p])
+    :fetch-fn v/fetch-all
     :render-fn 'v/html}
 
    {:name :nextjournal.markdown/em
-    :fetch-fn (into-markup [:em])
+    :transform-fn (into-markup [:em])
+    :fetch-fn v/fetch-all
     :render-fn 'v/html}
 
    {:name :nextjournal.markdown/strong
-    :fetch-fn (into-markup [:strong])
+    :transform-fn (into-markup [:strong])
+    :fetch-fn v/fetch-all
     :render-fn 'v/html}
 
    {:name :nextjournal.markdown/text
-    :fetch-fn (into-markup [:span.text])
+    :transform-fn (into-markup [:span.text])
+    :fetch-fn v/fetch-all
     :render-fn 'v/html}
 
    {:name :nextjournal.markdown/formula
@@ -51,4 +58,4 @@
 
 ^{::clerk/viewers md-viewers}
 (with-md-viewer (md/parse "# Hello
-This is _really_ a **strong** formula $\\alpha$."))
+This is not _really_ a **strong** formula $\\alpha$."))
