9b3816dd96d389ce01086e4a9d885e30caa1533d
Andrea Amantini <lo.zampino@gmail.com>
2022-03-24 18:20:28 +0100

    Make custom eval a bit more interesting

--- notebooks/viewers/custom_markdown.clj
+++ notebooks/viewers/custom_markdown.clj
@@ -5,32 +5,39 @@
             [nextjournal.clerk.viewer :as v]
             [nextjournal.markdown.transform :as markdown.transform]))
 
-(defn slider [min max]
-  (clerk/with-viewer {:fetch-fn v/fetch-all
-                      :render-fn (list 'fn '[_]
-                                       (list 'v/html
-                                             [:input {:type :range
-                                                      :min min :max max
-                                                      :on-change '(fn [x] (js/console.log (.. x -target -value)))}]))}
-    nil))
+;; This notebook should demostrate how to override markdown nodes and to test how they render by default.
 
-;; Define custom inline `(clerk/tex "\\beta")` evaluation like to build inline controls `(slider 0 100)` to interact with.
+;; ## Custom Inline Eval (is this margin ok 👇?)
+
+(defonce slider-state (atom 42))
+
+(defn slider [min max]
+  (clerk/with-viewer
+    {:fetch-fn (fn [_ x] x)
+     :render-fn `(fn [val]
+                   (v/html [:input {:type :range
+                                    :min ~min
+                                    :max ~max
+                                    :value val
+                                    :on-change #(v/clerk-eval `(reset! slider-state (Integer/parseInt ~(.. % -target -value))))}]))}
+    @slider-state))
+
+;; With overridable markdown nodes we can get inline evaluation for free: `(clerk/tex "\\beta")`. We can build inline controls to interact with.
 ;;
-;; _**FIXME:** inline formulas!!!_
+;; Say you don't like the text color of your notebook: fix it with a slider `(slider 0 256)` and set blue values to `@slider-state`. Unfortunately dragging of the slider isn't smooth at all, because of recomputations.
 
 (clerk/set-viewers! [{:name :nextjournal.markdown/text
-                      :fetch-fn v/fetch-all
-                      :transform-fn (v/into-markup [:span {:style {:color "#f87171"}}])
-                      :render-fn 'v/html}
+                      :transform-fn
+                      (v/into-markup
+                       [:span {:style {:color (str "rgb(130, 130, " @slider-state ")")}}])}
 
                      {:name :nextjournal.markdown/monospace
                       :transform-fn (comp eval read-string markdown.transform/->text)}
 
                      {:name :nextjournal.markdown/ruler
-                      :transform-fn (v/into-markup [:hr {:style {:border "3px solid #fb923c"}}])
-                      :fetch-fn v/fetch-all
-                      :render-fn 'v/html}])
+                      :transform-fn (v/into-markup [:hr {:style {:border "3px solid #fb923c"}}])}])
 
+;; # Custom Ruler
 ;; ---
 ;; Margins in markdown code blocks are still to be fixed, both in fenced blocks
 ;; ```clojure
