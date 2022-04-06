346f9ead87923146a30be413df51c0838c5f97ec
Philippa Markovics <philippa@markovics.com>
2022-04-06 16:59:49 +0200

    More compactness WIP

--- notebooks/tracer.clj
+++ notebooks/tracer.clj
@@ -29,6 +29,7 @@
           quoted-expr)))
     @trace))
 
+^{::clerk/visibility :hide ::clerk/viewer clerk/hide-result}
 (defn icon [kind]
   [:div.text-slate-700
    {:style {:font-size 10}}
@@ -40,19 +41,23 @@
 ;; code with results (currently in a visually unappealing way, but it
 ;; should be enough to get you started. 😊)
 
+^{::clerk/visibility :hide ::clerk/viewer clerk/hide-result}
 (declare show-element)
 
+^{::clerk/visibility :hide ::clerk/viewer clerk/hide-result}
 (def exception-icon
   [:svg.h-4.w-4
    {:xmlns "http://www.w3.org/2000/svg" :viewBox "0 0 20 20" :fill "currentColor"}
    [:path {:fill-rule "evenodd" :d "M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" :clip-rule "evenodd"}]])
 
+^{::clerk/visibility :hide ::clerk/viewer clerk/hide-result}
 (defn show-exception [e]
   [:div.text-red-600.flex.text-xs
    exception-icon
    (when (<= (count e) 150)
      [:span.ml-1 e])])
 
+^{::clerk/visibility :hide ::clerk/viewer clerk/hide-result}
 (defn show-result [result]
   (let [res (str (pr-str result))]
     [:div.whitespace-nowrap.text-slate-500.text-xs
@@ -61,72 +66,82 @@
        "\uD835\uDC53"
        res)]))
 
+^{::clerk/visibility :hide ::clerk/viewer clerk/hide-result}
 (defn show-with-bindings
   ([lookup depth result-id elem]
    (show-with-bindings true lookup depth result-id elem))
   ([as-pairs? lookup depth result-id elem]
    (println elem)
-   (let [named? (-> elem second symbol?)]
-     [:div.pl-2.rounded.inline-block.shadow.border.border-slate-400.border-opacity-60
-      {:class (str "py-[2px] pr-[2px] " (if (even? depth) "bg-slate-300" "bg-slate-200"))}
-      (let [depth (inc depth)]
-        [:<>
-         [:div.flex
-          [:div.mr-2.font-bold
-           {:class "py-[2px] "}
-           (first elem)]
-          (when named?
-            [:div.mr-2.font-bold
-             {:class "py-[2px] "}
-             (second elem)])
-          (let [bindings (if named? (nth elem 2) (second elem))]
-            [:div
-             [:div.px-2.rounded.relative.shadow.border.border-slate-400.border-opacity-60.mb-1
-              {:class (str "py-[2px] " (if (even? depth) "bg-slate-300" "bg-slate-200"))}
-              [:div.flex
-               (icon "[]")
-               (when-not (empty? bindings)
-                 (into [:div.ml-2]
-                       (map
-                         (fn [[k v]]
-                           [:div.flex.mb-1.last:mb-0
-                            [:div.mr-2.flex-shrink-0
-                             {:class (when as-pairs? "font-bold")}
-                             (show-element lookup (inc depth) nil k)]
-                            [:div
-                             (show-element lookup (inc depth) nil v)]])
-                         (partition 2 bindings))))]]])]
-         (into [:div]
-               (map
-                 (fn [el]
-                   (show-element lookup (inc (inc depth)) nil el))
-                 (drop 2 elem)))])])))
-
-(defn show-coll [lookup depth result-id elem]
-  [:div.px-2.rounded.inline-flex.shadow.border.border-slate-400.border-opacity-60
-   {:class (if (even? depth) "bg-slate-300" "bg-slate-200")}
-   (icon (cond (set? elem) "#{}"
-                    (map? elem) "{}"
-                    (vector? elem) "[]"))
-   [:div.flex-auto.ml-2
-    (if (map? elem)
-      (into [:div]
+   (let [named? (-> elem second symbol?)
+         bindings (nth elem (if named? 2 1))
+         bindings-empty? (empty? bindings)
+         remainder (drop (if named? 3 2) elem)]
+     [:div.inline-block.relative
+      [:div.flex
+       [:div.relative
+        [:div.absolute.bg-white.border.border-r-0.left-0.top-0.bottom-0.rounded
+         {:style {:z-index -1 :right -20}}]
+        [:div.pl-2.font-bold (first elem)]]
+       (when named?
+         [:div.ml-2 (second elem)])
+       [:div.flex.ml-2.relative
+        [:div.absolute.bg-white.border.left-0.top-0.bottom-0.rounded
+         {#_#_:class (when-not bindings-empty? "border-r-0")
+          :style {:z-index -1 :width (if bindings-empty? 30 40)}}]
+        [:div.pl-2
+         (icon "[]")]
+        (when-not bindings-empty?
+          (if as-pairs?
+            (into [:div]
+                  (map
+                    (fn [[k v]]
+                      [:div
+                       [:div.inline-flex.px-2
+                        [:div.font-bold.relative.px-2
+                         [:div.absolute.bg-white.border.left-0.top-0.bottom-0.rounded
+                          {:style {:z-index -1 :right -5}}]
+                         (show-element lookup (inc depth) nil k)]
+                        (show-element lookup (inc depth) nil v)]])
+                    (partition 2 bindings)))
+            (into [:div.flex]
+                  (map
+                    (fn [b]
+                      [:div.mr-2 b])
+                    bindings))))]]
+      (into [:div.ml-4]
             (map
-              (fn [[k v]]
-                [:div.flex
-                 [:div.mr-2.font-bold.flex-shrink-0
-                  (show-element lookup (inc depth) nil k)]
-                 [:div (show-element lookup (inc depth) nil v)]])
-              elem))
-      (into [:div] (mapv (partial show-element lookup (inc depth) nil) elem)))]])
+              (fn [el]
+                (show-element lookup (inc (inc depth)) nil el))
+              remainder))])))
 
+^{::clerk/visibility :hide ::clerk/viewer clerk/hide-result}
+(defn show-coll [lookup depth result-id elem]
+  (let [empty? (empty? elem)]
+    [:div.flex.relative.pl-2
+     [:div.absolute.bg-white.border.left-0.top-0.bottom-0.rounded
+      {:class (when-not empty? "border-r-0")
+       :style {:z-index -1 :width (if empty? 30 40)}}]
+     (icon (cond (set? elem) "#{}"
+                 (map? elem) "{}"
+                 (vector? elem) "[]"))
+     [:div.flex-auto.ml-2
+      (if (map? elem)
+        (into [:div]
+              (map
+                (fn [[k v]]
+                  [:div.flex
+                   (show-element lookup (inc depth) nil k)
+                   (show-element lookup (inc depth) nil v)])
+                elem))
+        (into [:div]
+              (mapv (partial show-element lookup (inc depth) nil) elem)))]]))
+
+^{::clerk/visibility :hide ::clerk/viewer clerk/hide-result}
 (defn show-seq [lookup depth result-id elem]
   (let [remaining (rest elem)
         line? (< (count remaining) 4)]
-    [:div.px-2.rounded.inline-flex.shadow.border.border-slate-400.border-opacity-60.mr-2.last:mr-0.flex-wrap
-     {:class (str
-               (when line? "items-center ")
-               (if (even? depth) "bg-slate-300" "bg-slate-200 py-[2px]"))}
+    [:div.inline-flex
+     {:class (when line? "items-center ")}
      [:div.font-bold.mr-2 (show-element lookup (inc depth) nil (first elem))]
      [:div.flex-auto.items-center
       {:class (when line? "flex")}
@@ -145,7 +160,9 @@
             (show-exception e)
             (show-result result))]))]))
 
+^{::clerk/visibility :hide ::clerk/viewer clerk/hide-result}
 (defn show-element [lookup depth result-id elem]
+  (println elem)
   (let [list-elem? (list? elem)
         fn-elem? (and list-elem? (contains? #{'fn 'fn* 'defn} (first elem)))]
     (cond (and list-elem? (= (first elem) 'add-trace)) (show-element lookup depth (second (second elem)) (second (nth elem 2)))
@@ -153,7 +170,11 @@
           (and (not list-elem?) (coll? elem)) (show-coll lookup depth result-id elem)
           (sequential? elem) (show-seq lookup depth result-id elem)
           :else
-          [:span
+          [:div.relative
+           (when-not (symbol? elem) {:class "px-2"})
+           (when-not (symbol? elem)
+             [:div.absolute.bg-white.border.left-0.top-0.bottom-0.rounded
+              {:style {:z-index -1 :right -5}}])
            (str " "
                 (if (string? elem) "\"")
                 elem
@@ -191,10 +212,10 @@
        ;; the top level form itself
        (first (second (last t1)))))])
 
-^::clerk/no-cache
-(clerk/html
+
+#_(clerk/html
   {::clerk/width :wide}
-  [:div.text-sm {:class "font-mono"}
+  [:div.text-sm.relative {:class "font-mono"}
    (let [t2 (debug-expression '(defn wrapped-with-metadata [value visibility h]
                                  (cond-> {:nextjournal/value value
                                           ::visibility visibility}
