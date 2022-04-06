6a1785113dc193bbd67ab5b8e3a39ca068e4d6ab
Martin Kavalar <mk@katercalling.com>
2022-04-06 11:58:28 +0200

    Sketch a plan

---
+++ notebooks/slideshow.clj
@@ -0,0 +1,7 @@
+;; # Slideshow Mode
+
+;; ## TODO
+;; * [x] describe root notebook mode 
+;; * [ ] stop using fetch all in notebook viewer and describe blocks 
+;; * [ ] move notebook viewer to overridable named viewer
+;; * [ ] introduce custom notebook viewer here that performs slideshow transformation
