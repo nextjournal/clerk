11dbbe6fd202cfed856aa2cc53d5d17ed31ab883
Philippa Markovics <philippa@markovics.com>
2022-04-06 13:29:43 +0200

    More compact layout

--- notebooks/tracer.clj
+++ notebooks/tracer.clj
@@ -1,7 +1,8 @@
 ;; # 👩🏻‍💻 Show the code
 ^{:nextjournal.clerk/visibility #{:hide-ns}}
 (ns tracer
-  (:require [nextjournal.clerk :as clerk]))
+  (:require [nextjournal.clerk :as clerk]
+            [clojure.string :as str]))
 
 ;; ## Tracer
 
@@ -29,7 +30,7 @@
     @trace))
 
 (defn icon [kind]
-  [:div.bg-slate-500.rounded-full.text-white.text-xs.w-5.h-5.flex.items-center.justify-center.flex-shrink-0
+  [:div.text-slate-700
    {:style {:font-size 10}}
    kind])
 
@@ -41,36 +42,65 @@
 
 (declare show-element)
 
-(defn show-let [lookup depth result-id elem]
-  [:div.rounded-md.p-2.pt-0
-   {:class (if (even? depth) "bg-slate-200 " "bg-slate-300 ")}
-   [:span.font-bold.inline-block.mt-2 "let"]
-   (let [depth (inc depth)]
-     [:div.rounded-md.p-2.pt-0.mt-2
-      {:class (if (even? depth) "bg-slate-200 " "bg-slate-300 ")}
-      [:div.flex
-       [:div.mt-2 (icon "[]")]
-       (into [:div.ml-2 {:class "-mt-2"}]
+(def exception-icon
+  [:svg.h-4.w-4
+   {:xmlns "http://www.w3.org/2000/svg" :viewBox "0 0 20 20" :fill "currentColor"}
+   [:path {:fill-rule "evenodd" :d "M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" :clip-rule "evenodd"}]])
+
+(defn show-exception [e]
+  [:div.text-red-600.flex.text-xs
+   exception-icon
+   (when (<= (count e) 150)
+     [:span.ml-1 e])])
+
+(defn show-result [result]
+  (let [res (str (pr-str result))]
+    [:div.whitespace-nowrap.text-slate-500.text-xs
+     [:span.mx-1 "→"]
+     (if (str/starts-with? res "#function")
+       "\uD835\uDC53"
+       res)]))
+
+(defn show-with-bindings
+  ([lookup depth result-id elem]
+   (show-with-bindings true lookup depth result-id elem))
+  ([as-pairs? lookup depth result-id elem]
+   [:div.pl-2.rounded.inline-block.shadow.border.border-slate-400.border-opacity-60
+    {:class (str "py-[2px] pr-[2px] " (if (even? depth) "bg-slate-300" "bg-slate-200"))}
+    (let [depth (inc depth)]
+      [:<>
+       [:div.flex
+        [:div.mr-2.font-bold
+         {:class "py-[2px] "}
+         (first elem)]
+        [:div
+         [:div.px-2.rounded.relative.shadow.border.border-slate-400.border-opacity-60.mb-1
+          {:class (str "py-[2px] " (if (even? depth) "bg-slate-300" "bg-slate-200"))}
+          [:div.flex
+           (icon "[]")
+           (when-not (-> elem second empty?)
+             (into [:div.ml-2]
+                   (map
+                     (fn [[k v]]
+                       [:div.flex.mb-1.last:mb-0
+                        [:div.mr-2.flex-shrink-0
+                         {:class (when as-pairs? "font-bold")}
+                         (show-element lookup (inc depth) nil k)]
+                        [:div
+                         (show-element lookup (inc depth) nil v)]])
+                     (->> elem second (partition 2)))))]]]]
+       (into [:div]
              (map
-               (fn [[k v]]
-                 [:div.flex.mt-2
-                  [:div.mr-2.font-bold.flex-shrink-0
-                   (show-element lookup (inc depth) nil k)]
-                  [:div {:class (when (coll? v) "-mt-2")}
-                   (show-element lookup (inc depth) nil v)]])
-               (->> elem second (partition 2))))]])
-   (into [:div]
-         (map
-           (fn [el]
-             (show-element lookup (inc depth) nil el))
-           (drop 2 elem)))])
+               (fn [el]
+                 (show-element lookup (inc (inc depth)) nil el))
+               (drop 2 elem)))])]))
 
 (defn show-coll [lookup depth result-id elem]
-  [:div.rounded-md.p-2.pt-0.flex.mt-2
-   {:class (str (if (even? depth) "bg-slate-200 " "bg-slate-300 "))}
-   [:div.mt-2 (icon (cond (set? elem) "#{}"
-                          (map? elem) "{}"
-                          (vector? elem) "[]"))]
+  [:div.px-2.rounded.inline-flex.shadow.border.border-slate-400.border-opacity-60
+   {:class (if (even? depth) "bg-slate-300" "bg-slate-200")}
+   (icon (cond (set? elem) "#{}"
+                    (map? elem) "{}"
+                    (vector? elem) "[]"))
    [:div.flex-auto.ml-2
     (if (map? elem)
       (into [:div]
@@ -79,39 +109,52 @@
                 [:div.flex
                  [:div.mr-2.font-bold.flex-shrink-0
                   (show-element lookup (inc depth) nil k)]
-                 [:div {:class (when (coll? v) "-mt-2 -mb-2")}
-                  (show-element lookup (inc depth) nil v)]])
+                 [:div (show-element lookup (inc depth) nil v)]])
               elem))
       (into [:div] (mapv (partial show-element lookup (inc depth) nil) elem)))]])
 
 (defn show-seq [lookup depth result-id elem]
-  [:div.rounded-md.p-2.pt-0.flex.mt-2
-   {:class (str (if (even? depth) "bg-slate-200 " "bg-slate-300 "))}
-   [:div
-    [:span.font-bold (show-element lookup (inc depth) nil (first elem))]
-    (into [:<>] (mapv (partial show-element lookup (inc depth) nil) (rest elem)))
-    (when result-id
-      (let [result (get lookup result-id)]
-        (if-let [e (:exception result)]
-          [:div.rounded.border-2.border-red-500.bg-red-100.text-red-500.p-2.font-bold.text-xs.mt-2 e]
-          [:span.text-slate-500.float-right.ml-2.mt-2
-           (str "→ " (pr-str result))])))]])
+  (let [remaining (rest elem)
+        line? (< (count remaining) 4)]
+    [:div.px-2.rounded.inline-flex.shadow.border.border-slate-400.border-opacity-60.mr-2.last:mr-0.flex-wrap
+     {:class (str
+               (when line? "items-center ")
+               (if (even? depth) "bg-slate-300" "bg-slate-200 py-[2px]"))}
+     [:div.font-bold.mr-2 (show-element lookup (inc depth) nil (first elem))]
+     [:div.flex-auto.items-center
+      {:class (when line? "flex")}
+      (into [:<>] (mapv
+                    (fn [e]
+                      [:div
+                       {:class (if line? "mr-2 last:mr-0" "mb-1 last:mb-0")}
+                       (show-element lookup (inc depth) nil e)])
+                    remaining))]
+     (when result-id
+       (let [result (get lookup result-id)]
+         [:div.inline-flex.ml-2
+          {:class (if line? "items-center" "mt-[2px]")
+           :style {:max-width 300}}
+          (if-let [e (:exception result)]
+            (show-exception e)
+            (show-result result))]))]))
 
 (defn show-element [lookup depth result-id elem]
-  (println elem)
-  (cond (and (list? elem) (= (first elem) 'add-trace)) (show-element lookup depth (second (second elem)) (second (nth elem 2)))
-        (and (list? elem) (= (first elem) 'let)) (show-let lookup depth result-id elem)
-        (and (not (list? elem)) (coll? elem)) (show-coll lookup depth result-id elem)
-        (sequential? elem) (show-seq lookup depth result-id elem)
-        :else
-        [:span.inline-block.mt-2.mr-2
-         (str " "
-              (if (string? elem) "\"")
-              elem
-              (if (string? elem) "\""))]))
+  (let [list-elem? (list? elem)
+        fn-elem? (and list-elem? (contains? #{'fn 'fn*} (first elem)))]
+    (cond (and list-elem? (= (first elem) 'add-trace)) (show-element lookup depth (second (second elem)) (second (nth elem 2)))
+          (and list-elem? (or (= (first elem) 'let) fn-elem?)) (show-with-bindings (not fn-elem?) lookup depth result-id elem)
+          (and (not list-elem?) (coll? elem)) (show-coll lookup depth result-id elem)
+          (sequential? elem) (show-seq lookup depth result-id elem)
+          :else
+          [:span
+           (str " "
+                (if (string? elem) "\"")
+                elem
+                (if (string? elem) "\""))])))
 
 ^::clerk/no-cache
 (clerk/html
+  {::clerk/width :wide}
   [:div.text-sm {:class "font-mono"}
    ;; boring arithmetic example form
    (let [t (debug-expression '(let [x 10
