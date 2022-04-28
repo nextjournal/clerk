c8a7c10b9cb558df4a8687801d93f45e34199d55
Philippa Markovics <philippa@markovics.com>
2022-04-28 12:13:15 +0200

    Fix overflow when markdown contains superscript in first line

--- src/nextjournal/clerk/sci_viewer.cljs
+++ src/nextjournal/clerk/sci_viewer.cljs
@@ -192,7 +192,7 @@
                       (let [viewer (viewer/viewer x)
                             blob-id (:blob-id (viewer/value x))
                             inner-viewer-name (some-> x viewer/value viewer/viewer :name)]
-                        [:div {:class ["viewer" "overflow-x-auto"
+                        [:div {:class ["viewer" "overflow-x-auto" "overflow-y-hidden"
                                        (when (keyword? viewer) (str "viewer-" (name viewer)))
                                        (when inner-viewer-name (str "viewer-" (name inner-viewer-name)))
                                        (case (or (viewer/width x) (case viewer (:code :code-folded) :wide :prose))
