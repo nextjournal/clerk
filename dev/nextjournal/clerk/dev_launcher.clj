538b7d1fec18224aab0ac7ccd47af64583485d49
Martin Kavalar <mk@katercalling.com>
2022-05-07 20:37:56 +0200

    Use explicit require for nrepl

--- dev/nextjournal/clerk/dev_launcher.clj
+++ dev/nextjournal/clerk/dev_launcher.clj
@@ -1,10 +1,11 @@
 (ns nextjournal.clerk.dev-launcher
   "A dev launcher that launches nrepl, shadow-cljs and once the first cljs compliation completes, Clerk.
   
-  Avoiding ns requires here so the REPL comes up early.")
+  Avoiding other ns requires here so the REPL comes up early."
+  (:require [nrepl.cmdline :as nrepl]))
 
 (defn start [serve-opts]
-  (future ((requiring-resolve 'nrepl.cmdline/-main) "--middleware" "[cider.nrepl/cider-middleware]"))
+  (future (nrepl/dispatch-commands {:middleware '[cider.nrepl/cider-middleware]}))
   (require 'shadow.cljs.silence-default-loggers)
   ((requiring-resolve 'shadow.cljs.devtools.server/start!))
   ((requiring-resolve 'shadow.cljs.devtools.api/watch) :browser)
