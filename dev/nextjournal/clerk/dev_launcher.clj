de32491b390c43e8a66340447a983b888b7e289f
Martin Kavalar <mk@katercalling.com>
2022-05-07 17:39:04 +0200

    Simplify & silence logging
    Thanks @thheller!

--- dev/nextjournal/clerk/dev_launcher.clj
+++ dev/nextjournal/clerk/dev_launcher.clj
@@ -3,17 +3,9 @@
   
   Avoiding ns requires here so the REPL comes up early.")
 
-(defonce !start-clerk-delay
-  (atom nil))
-
-(defn cljs-flushed
-  {:shadow.build/stage :flush}
-  [build-state & _]
-  (some-> !start-clerk-delay deref deref)
-  build-state)
-
 (defn start [serve-opts]
-  (reset! !start-clerk-delay (delay ((requiring-resolve 'nextjournal.clerk/serve!) serve-opts)))
   (future ((requiring-resolve 'nrepl.cmdline/-main) "--middleware" "[cider.nrepl/cider-middleware]"))
+  (require 'shadow.cljs.silence-default-loggers)
   ((requiring-resolve 'shadow.cljs.devtools.server/start!))
-  ((requiring-resolve 'shadow.cljs.devtools.api/watch) :browser))
+  ((requiring-resolve 'shadow.cljs.devtools.api/watch) :browser)
+  ((requiring-resolve 'nextjournal.clerk/serve!) serve-opts))
