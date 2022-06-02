7d4f474d0490f7c15d7980f7ce83ce6cdc7a22e1
Martin Kavalar <martin@nextjournal.com>
2022-06-02 11:58:58 +0200

    Lazy loading &c working

--- src/nextjournal/clerk/examples.clj
+++ src/nextjournal/clerk/examples.clj
@@ -11,17 +11,19 @@
 
 ;; The example viewer shows the form and it's resulting value.
 (def example-viewer
-  {:transform-fn (fn [wrapped-value]
+  {:transform-fn (fn [{:as wrapped-value :nextjournal/keys [viewers] :keys [path current-path]}]
+                   (prn :path path :current-path current-path :keys (keys wrapped-value))
                    (-> wrapped-value
-                       clerk/mark-presented
-                       (assoc :nextjournal/viewer {:render-fn 'v/html})
+                       (assoc :nextjournal/viewer {:render-fn '(fn [[form val] opts]
+                                                                 (v/html [:div.flex.flex-wrap
+                                                                          {:class "py-[7px]"}
+                                                                          [:div [:div.bg-slate-100.px-2.rounded
+                                                                                 (v/inspect (update opts :path conj 0) form)]]
+                                                                          [:div.flex.mt-1
+                                                                           [:div.mx-2.font-sans.text-xs.text-slate-500 {:class "mt-[2px]"} "⇒"]
+                                                                           (v/inspect (update opts :path conj 0) val)]]))})
                        (update :nextjournal/value (fn [{:keys [form val]}]
-                                                    [:div.flex.flex-wrap
-                                                     {:class "py-[7px]"}
-                                                     [:div [:div.bg-slate-100.px-2.rounded (v/inspect-wrapped-value (clerk/code form))]]
-                                                     [:div.flex.mt-1
-                                                      [:div.mx-2.font-sans.text-xs.text-slate-500 {:class "mt-[2px]"} "⇒"]
-                                                      (v/inspect-wrapped-value (v/present val))]]))))})
+                                                    [(clerk/code form) val]))))})
 
 (clerk/with-viewer example-viewer
   {:form '(+ 1 2)
@@ -31,20 +33,22 @@
   {:form '(clerk/html [:h1 "👋"])
    :val (clerk/html [:h1 "👋"])})
 
-(clerk/with-viewer example-viewer
-  {:form '(vec (range 10))
-   :val [0 1 2 3 4 5 6 7 8 9]})
 
-(v/inspect-wrapped-value [0 1 2 3 4 5 6 7 8 9])
+(clerk/with-viewer example-viewer
+  {:form '(-> 42 range shuffle)
+   :val (-> 42 range shuffle)})
 
 ;; Outside of Clerk, the `example` macro evaluates to `nil`, just like `clojure.core/comment`. Try this in your editor!
 (defmacro example
   [& body]
   (when nextjournal.clerk.config/*in-clerk*
     `(clerk/with-viewer {:transform-fn (clerk/update-val (fn [ex#]
-                                                           (clerk/html (into [:div.border-l-2.border-slate-300.pl-4
-                                                                              [:div.uppercase.tracking-wider.text-xs.font-sans.text-slate-500.mt-4.mb-2 "Examples"]]
-                                                                             (mapv (partial clerk/with-viewer example-viewer) ex#)))))}
+                                                           (clerk/with-viewer {:render-fn '(fn [examples opts]
+                                                                                             (v/html (into [:div.border-l-2.border-slate-300.pl-4
+                                                                                                            [:div.uppercase.tracking-wider.text-xs.font-sans.text-slate-500.mt-4.mb-2 "Examples"]]
+                                                                                                           (v/inspect-children opts) examples)))}
+                                                             
+                                                             (mapv (partial clerk/with-viewer example-viewer) ex#))))}
        (mapv (fn [form# val#] {:form form# :val val#}) ~(mapv (fn [x#] `'~x#) body) ~(vec body)))))
 
 
@@ -56,6 +60,7 @@
  (-> 42 range shuffle)
  (macroexpand '(example (+ 1 2)))
  (clerk/html [:h1 "👋"])
+ (javax.imageio.ImageIO/read (java.net.URL. "https://etc.usf.edu/clipart/36600/36667/thermos_36667_sm.gif"))
  )
 
 
@@ -63,6 +68,6 @@
 ;; ## TODO
 ;; - [x] Show all code & results
 ;; - [x] Styling pass
-;; - [ ] Support using viewer api for example results
+;; - [x] Support using viewer api for example results
 ;; - [ ] Hide code cells for examples
 ;; - [ ] Combine with docstring ns rendering
