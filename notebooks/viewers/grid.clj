137acccb8b0d129a937c59f60488afbbcd93df2e
Philippa Markovics <philippa@markovics.com>
2022-06-06 15:17:51 +0200

    Separate into row and col viewers

--- notebooks/viewers/grid.clj
+++ notebooks/viewers/grid.clj
@@ -1,5 +1,5 @@
 ;; # 🔠 Grid Viewer
-(ns ^:nextjournal.clerk/no-cache grid
+(ns grid
   (:require [nextjournal.clerk :as clerk]
             [nextjournal.clerk.viewer :as v])
   (:import (java.net URL)
@@ -28,17 +28,17 @@
                        (update :nextjournal/viewers update-grid-viewers)
                        (assoc :nextjournal/value (map (partial v/with-viewer :grid/row) rows)))))})
 
-(clerk/with-viewer grid-viewer
+#_(clerk/with-viewer grid-viewer
   [[(ImageIO/read (URL. "https://etc.usf.edu/clippix/pix/allure-of-an-antique-empire_medium.jpg"))
     (ImageIO/read (URL. "https://etc.usf.edu/clippix/pix/allegorical-symbol-for-the-emperos-victories-and-conquests_medium.jpg"))
     (ImageIO/read (URL. "https://etc.usf.edu/clippix/pix/emperor-hadrian-getting-ready-for-a-day-of-hunting_medium.jpg"))]])
 
-(clerk/with-viewer grid-viewer
+#_(clerk/with-viewer grid-viewer
   [[(ImageIO/read (URL. "https://etc.usf.edu/clippix/pix/allure-of-an-antique-empire_medium.jpg"))
     (ImageIO/read (URL. "https://etc.usf.edu/clippix/pix/allegorical-symbol-for-the-emperos-victories-and-conquests_medium.jpg"))]
    [(ImageIO/read (URL. "https://etc.usf.edu/clippix/pix/emperor-hadrian-getting-ready-for-a-day-of-hunting_medium.jpg"))]])
 
-(clerk/with-viewer grid-viewer
+#_(clerk/with-viewer grid-viewer
   [[(clerk/with-viewer grid-viewer
       [[(ImageIO/read (URL. "https://etc.usf.edu/clippix/pix/allure-of-an-antique-empire_medium.jpg"))]
        [(ImageIO/read (URL. "https://etc.usf.edu/clippix/pix/allegorical-symbol-for-the-emperos-victories-and-conquests_medium.jpg"))]])
@@ -48,7 +48,7 @@
        [(ImageIO/read (URL. "https://etc.usf.edu/clippix/pix/fregio-profectio_medium.jpg"))
         (ImageIO/read (URL. "https://etc.usf.edu/clippix/pix/fourth-fregio-of-trajan_medium.jpg"))]])]])
 
-(clerk/with-viewer grid-viewer
+#_(clerk/with-viewer grid-viewer
   [[(clerk/with-viewer grid-viewer
       [[(ImageIO/read (URL. "https://etc.usf.edu/clippix/pix/allure-of-an-antique-empire_medium.jpg"))]
        [(ImageIO/read (URL. "https://etc.usf.edu/clippix/pix/allegorical-symbol-for-the-emperos-victories-and-conquests_medium.jpg"))]])
@@ -57,3 +57,55 @@
        [(ImageIO/read (URL. "https://etc.usf.edu/clippix/pix/fregio-ingressus_medium.jpg"))]
        [(ImageIO/read (URL. "https://etc.usf.edu/clippix/pix/fregio-profectio_medium.jpg"))]
        [(ImageIO/read (URL. "https://etc.usf.edu/clippix/pix/fourth-fregio-of-trajan_medium.jpg"))]])]])
+
+(def grid-row
+  {:name :grid/row
+   :render-fn '(fn [items opts]
+                 (let [item-count (count items)]
+                   (v/html (into [:div {:class "md:flex md:flex-row md:gap-4 not-prose"}]
+                             (map (fn [item]
+                                    [:div.flex.items-center.justify-center
+                                     {:class (str "md:w-[" (* 100 (float (/ 1 item-count))) "%]")}
+                                     (v/inspect opts item)])) items))))})
+
+(def grid-col
+  {:name :grid/col
+   :render-fn '(fn [items opts]
+                 (v/html (into [:div {:class "md:flex md:flex-col md:gap-4 clerk-grid not-prose"}]
+                           (map (fn [item]
+                                  [:div.flex.items-center.justify-center
+                                   (v/inspect opts item)])) items)))})
+
+(clerk/with-viewer grid-row
+  [(ImageIO/read (URL. "https://etc.usf.edu/clippix/pix/allure-of-an-antique-empire_medium.jpg"))
+   (ImageIO/read (URL. "https://etc.usf.edu/clippix/pix/allegorical-symbol-for-the-emperos-victories-and-conquests_medium.jpg"))
+   (ImageIO/read (URL. "https://etc.usf.edu/clippix/pix/emperor-hadrian-getting-ready-for-a-day-of-hunting_medium.jpg"))])
+
+(clerk/with-viewer grid-col
+  [(ImageIO/read (URL. "https://etc.usf.edu/clippix/pix/allure-of-an-antique-empire_medium.jpg"))
+   (ImageIO/read (URL. "https://etc.usf.edu/clippix/pix/allegorical-symbol-for-the-emperos-victories-and-conquests_medium.jpg"))
+   (ImageIO/read (URL. "https://etc.usf.edu/clippix/pix/emperor-hadrian-getting-ready-for-a-day-of-hunting_medium.jpg"))])
+
+(clerk/with-viewer grid-col
+  [(clerk/with-viewer grid-row [(ImageIO/read (URL. "https://etc.usf.edu/clippix/pix/allure-of-an-antique-empire_medium.jpg"))
+                                (ImageIO/read (URL. "https://etc.usf.edu/clippix/pix/allegorical-symbol-for-the-emperos-victories-and-conquests_medium.jpg"))
+                                (ImageIO/read (URL. "https://etc.usf.edu/clippix/pix/emperor-hadrian-getting-ready-for-a-day-of-hunting_medium.jpg"))])
+   (clerk/with-viewer grid-row [(ImageIO/read (URL. "https://etc.usf.edu/clippix/pix/fregio-ingressus_medium.jpg"))])
+   (clerk/with-viewer grid-row [(ImageIO/read (URL. "https://etc.usf.edu/clippix/pix/fregio-profectio_medium.jpg"))
+                                (ImageIO/read (URL. "https://etc.usf.edu/clippix/pix/fourth-fregio-of-trajan_medium.jpg"))])])
+
+(clerk/with-viewer grid-col
+  [(clerk/with-viewer grid-row [(ImageIO/read (URL. "https://etc.usf.edu/clippix/pix/allure-of-an-antique-empire_medium.jpg"))
+                                (ImageIO/read (URL. "https://etc.usf.edu/clippix/pix/allegorical-symbol-for-the-emperos-victories-and-conquests_medium.jpg"))
+                                (ImageIO/read (URL. "https://etc.usf.edu/clippix/pix/emperor-hadrian-getting-ready-for-a-day-of-hunting_medium.jpg"))
+                                (ImageIO/read (URL. "https://etc.usf.edu/clippix/pix/fregio-ingressus_medium.jpg"))])
+   (clerk/with-viewer grid-row [(ImageIO/read (URL. "https://etc.usf.edu/clippix/pix/fregio-profectio_medium.jpg"))
+                                (ImageIO/read (URL. "https://etc.usf.edu/clippix/pix/fourth-fregio-of-trajan_medium.jpg"))])])
+
+(clerk/with-viewer grid-row
+  [(clerk/with-viewer grid-col {:width "25%"} [(ImageIO/read (URL. "https://etc.usf.edu/clippix/pix/allure-of-an-antique-empire_medium.jpg"))
+                                               (ImageIO/read (URL. "https://etc.usf.edu/clippix/pix/allegorical-symbol-for-the-emperos-victories-and-conquests_medium.jpg"))
+                                               (ImageIO/read (URL. "https://etc.usf.edu/clippix/pix/emperor-hadrian-getting-ready-for-a-day-of-hunting_medium.jpg"))
+                                               (ImageIO/read (URL. "https://etc.usf.edu/clippix/pix/fregio-ingressus_medium.jpg"))])
+   (clerk/with-viewer grid-col [(ImageIO/read (URL. "https://etc.usf.edu/clippix/pix/fregio-profectio_medium.jpg"))
+                                (ImageIO/read (URL. "https://etc.usf.edu/clippix/pix/fourth-fregio-of-trajan_medium.jpg"))])])
