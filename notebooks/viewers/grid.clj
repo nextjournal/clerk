285edb8d29c30e1ca477fbcd313aad1a932a12a4
Philippa Markovics <philippa@markovics.com>
2022-06-07 09:34:52 +0200

    Update documentation

--- notebooks/viewers/grid.clj
+++ notebooks/viewers/grid.clj
@@ -1,14 +1,20 @@
-;; # 🔠 Grid Viewer
-(ns ^:nextjournal.clerk/no-cache grid
+;; # 🔠 Grid Layouts
+(ns grid
   (:require [nextjournal.clerk :as clerk]
             [nextjournal.clerk.viewer :as v])
   (:import (java.net URL)
            (javax.imageio ImageIO)))
 
-(v/row
-  (ImageIO/read (URL. "https://etc.usf.edu/clippix/pix/allure-of-an-antique-empire_medium.jpg"))
-  (ImageIO/read (URL. "https://etc.usf.edu/clippix/pix/allegorical-symbol-for-the-emperos-victories-and-conquests_medium.jpg"))
-  (ImageIO/read (URL. "https://etc.usf.edu/clippix/pix/emperor-hadrian-getting-ready-for-a-day-of-hunting_medium.jpg")))
+;; ## Layouts can be composed via `row`s and `col`s
+;;
+;; Passing `:width`, `:height` or any other style attributes to `::clerk/opts`
+;; will assign them on the row or col that contains your items. You can use
+;; this to size your containers accordingly.
+
+#(v/row
+   (ImageIO/read (URL. "https://etc.usf.edu/clippix/pix/allure-of-an-antique-empire_medium.jpg"))
+   (ImageIO/read (URL. "https://etc.usf.edu/clippix/pix/allegorical-symbol-for-the-emperos-victories-and-conquests_medium.jpg"))
+   (ImageIO/read (URL. "https://etc.usf.edu/clippix/pix/emperor-hadrian-getting-ready-for-a-day-of-hunting_medium.jpg")))
 
 (v/col
   {::clerk/opts {:width 150}}
@@ -51,6 +57,12 @@
     (ImageIO/read (URL. "https://etc.usf.edu/clippix/pix/fregio-profectio_medium.jpg"))
     (ImageIO/read (URL. "https://etc.usf.edu/clippix/pix/fourth-fregio-of-trajan_medium.jpg"))))
 
+;; ## Alternative notations
+;;
+;; By default, `row` and `col` operate on `& rest` so you can pass any number of items to the functions.
+;; But the viewers are smart enough to accept any sequential list of items so you can
+;; assign the viewers via metadata on your data structures too.
+
 ^{::clerk/viewer :row}
 [(ImageIO/read (URL. "https://etc.usf.edu/clippix/pix/allure-of-an-antique-empire_medium.jpg"))
  (ImageIO/read (URL. "https://etc.usf.edu/clippix/pix/allegorical-symbol-for-the-emperos-victories-and-conquests_medium.jpg"))
@@ -62,3 +74,9 @@
  (ImageIO/read (URL. "https://etc.usf.edu/clippix/pix/allegorical-symbol-for-the-emperos-victories-and-conquests_medium.jpg"))
  (ImageIO/read (URL. "https://etc.usf.edu/clippix/pix/emperor-hadrian-getting-ready-for-a-day-of-hunting_medium.jpg"))
  (ImageIO/read (URL. "https://etc.usf.edu/clippix/pix/fregio-ingressus_medium.jpg"))]
+
+(v/row
+  [(ImageIO/read (URL. "https://etc.usf.edu/clippix/pix/allure-of-an-antique-empire_medium.jpg"))
+   (ImageIO/read (URL. "https://etc.usf.edu/clippix/pix/allegorical-symbol-for-the-emperos-victories-and-conquests_medium.jpg"))
+   (ImageIO/read (URL. "https://etc.usf.edu/clippix/pix/emperor-hadrian-getting-ready-for-a-day-of-hunting_medium.jpg"))
+   (ImageIO/read (URL. "https://etc.usf.edu/clippix/pix/fregio-ingressus_medium.jpg"))])
