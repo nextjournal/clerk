cdf79a8b49cc66b58adc6a27f1d402f7350d8a8a
Andrea Amantini <lo.zampino@gmail.com>
2022-06-15 17:54:43 +0200

    Markdown Fixes (Inline Images + loose lists) (#172)
    * Fix rendering of inline images
    * Fix rendering markdown lists with more than one paragraph per list item
    * Reduce margin between prose and results

--- notebooks/viewers/table.clj
+++ notebooks/viewers/table.clj
@@ -6,12 +6,15 @@
             [next.jdbc :as jdbc]
             [nextjournal.clerk :as clerk]))
 
+;; ## SQL Queries
 (def query-results
   (let [_run-at #inst "2021-05-20T08:28:29.445-00:00"
         ds (jdbc/get-datasource {:dbtype "sqlite" :dbname "chinook.db"})]
     (with-open [conn (jdbc/get-connection ds)]
       (clerk/table (jdbc/execute! conn ["SELECT AlbumId, Bytes, Name, TrackID, UnitPrice FROM tracks"])))))
 
+;; ## Iris Data
+^{::clerk/visibility :hide}
 (clerk/table (clerk/use-headers (csv/read-csv (slurp "https://gist.githubusercontent.com/netj/8836201/raw/6f9306ad21398ea43cba4f7d537619d0e07d5ae3/iris.csv"))))
 
 (defn words-url []
@@ -19,13 +22,15 @@
     "/usr/share/dict/words"
     "https://gist.githubusercontent.com/wchargin/8927565/raw/d9783627c731268fb2935a731a618aa8e95cf465/words"))
 
+;; ## Words
 (clerk/table {:nextjournal/width :full}
              (->> (slurp (words-url))
                   str/split-lines
                   (group-by (comp keyword str/upper-case str first))
                   (into (sorted-map))))
 
-;; ;; The table viewer will perform normalization and show an error in case of failure:
+;; ## Table Errors
+;; The table viewer will perform normalization and show an error in case of failure:
 (clerk/table (set (range 30)))
 
 ;; Shows full column names when there are many long column names
@@ -37,8 +42,8 @@
                (-> (mapv (fn [char] (clojure.string/join "" (repeat 20 char)))
                          (map char (range 97 127))))]})
 
-;; Table with images
+;; ## Table with images
 (clerk/table [[1 2] [3 (javax.imageio.ImageIO/read (java.net.URL. "https://etc.usf.edu/clipart/36600/36667/thermos_36667_sm.gif"))]])
 
-;; Table with a table
+;; ## Table within tables
 (clerk/table [[1 2] [3 (clerk/table [[1 2] [3 4]])]])
