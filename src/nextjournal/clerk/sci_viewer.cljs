293b13604b6ce2ac8f1328957f88180ca2c7870a
Philippa Markovics <philippa@markovics.com>
2022-07-05 10:41:32 +0200

    Drag/drop with radial progress on expand indicators WIP

--- src/nextjournal/clerk/sci_viewer.cljs
+++ src/nextjournal/clerk/sci_viewer.cljs
@@ -334,6 +334,18 @@
                                      (swap! !expanded-at dissoc :prompt-multi-expand?)))
                      on-key-up (fn [event]
                                  (swap! !expanded-at dissoc :prompt-multi-expand?))
+                     on-mouse-down (fn [event]
+                                     (js/console.log (.-target event))
+                                     (when false
+                                       (.preventDefault event)
+                                       (.stopPropagation event)
+                                       (js/document.addEventListener "mouseup" on-mouse-up)
+                                       (js/document.addEventListener "mousemove" on-mouse-move)
+                                       (let [{:keys [x y width height]} (j/lookup (.. event -target getBoundingClientRect))
+                                             x (+ x (/ width 2))
+                                             y (+ y (/ height 2))]
+                                         (swap! !drag-state
+                                           assoc :visible? true :x1 x :y1 y :x2 x :y2 y))))
                      ref-fn (fn [el]
                               (if el
                                 (do
@@ -351,37 +363,8 @@
           [view-context/provide {:fetch-fn fetch-fn}
            [error-boundary
             !error
-            (let [drag-area-width "calc((100vw - 756px) / 2)"
-                  {:keys [visible? x1 y1 x2 y2]} @!drag-state]
+            (let [{:keys [visible? x1 y1 x2 y2]} @!drag-state]
               [:div.relative.inspector-container
-               [:div.absolute.left-0.top-0.bottom-0.group
-                {:style {:width drag-area-width :transform "translateX(-100%)"}}
-                (when-not visible?
-                  [:div.absolute.rounded-full.z-10.border-2.border-slate-300.opacity-0.group-hover:opacity-100.group-hover:animate-ping
-                   {:class "w-[16px] h-[16px] top-[5px] right-[8px]"}])
-                [:div.absolute.rounded-full.transition-all.z-10.border-2.border-slate-300.group
-                 {:class (str "w-[16px] h-[16px] top-[5px] right-[8px] cursor-grab opacity-0 group-hover:opacity-100 "
-                           "hover:right-[4px] hover:w-[24px] hover:h-[24px] hover:top-[1px] hover:bg-slate-100 hover:border-indigo-600 "
-                           (when visible? "opacity-0"))
-                  :on-mouse-down (fn [event]
-                                   (.preventDefault event)
-                                   (.stopPropagation event)
-                                   (js/document.addEventListener "mouseup" on-mouse-up)
-                                   (js/document.addEventListener "mousemove" on-mouse-move)
-                                   (let [{:keys [x y width height]} (j/lookup (.. event -target getBoundingClientRect))
-                                         x (+ x (/ width 2))
-                                         y (+ y (/ height 2))]
-                                     (swap! !drag-state
-                                       assoc :visible? true :x1 x :y1 y :x2 x :y2 y)))}]
-                [:div.opacity-0.group-hover:opacity-100.absolute.left-0.top-0
-                 {:class "w-[24px] h-[200px] rounded-full bg-slate-100.border-2.border-indigo-600"}]]
-               (when visible?
-                 [:svg.fixed.left-0.top-0.right-0.bottom-0.cursor-grabbing.text-indigo-600.pointer-events-none
-                  {:xmlns "http://www.w3.org/2000/svg" :fill "currentColor"
-                   :style {:z-index 1000}
-                   :ref #(when % (.setAttribute % "viewBox" (str "0 0 " js/innerWidth " " js/innerHeight)))}
-                  [:circle {:cx x1 :cy y1 :r 8 :fill "currentColor"}]
-                  [:line {:x1 x1 :y1 y1 :x2 x2 :y2 y2 :stroke "currentColor" :stroke-width "5" :stroke-linecap "round"}]])
                [:div.overflow-x-auto.overflow-y-hidden
                 {:ref ref-fn}
                 [inspect {:!expanded-at !expanded-at} @!desc]]])]])))
@@ -432,24 +415,66 @@
 
 (def triangle-spacer [:span {:class "inline-block w-[8px]"}])
 
+(defn expand-button [!expanded-at opening-paren path]
+  (r/with-let [on-mouse-move (fn on-mouse-move* [event]
+                               (when (:drag-state @!expanded-at)
+                                 (.preventDefault event)
+                                 (.stopPropagation event)
+                                 (let [{:keys [clientX clientY]} (j/lookup event)]
+                                   (when-not (zero? clientX)
+                                     (let [{:keys [drag-state]} @!expanded-at
+                                           {:keys [x1 y1]} drag-state
+                                           max-r 20
+                                           max-length 300
+                                           length (Math/sqrt (+ (Math/pow (- clientX x1) 2) (Math/pow (- clientY y1) 2)))
+                                           r (min 20 length)
+                                           c (* Math/PI (* 2 max-r))
+                                           progress (* 100 (/ (- (min max-length length) max-r) max-length))
+                                           expand-until (Math/ceil (/ r 10))
+                                           offset (* progress c)]
+                                       (swap! !expanded-at update :drag-state merge {:x2 clientX :y2 clientY :r r :offset offset})
+                                       #_(when (> expand-until 0)
+                                           (reset! !expanded-at (reduce #(assoc %1 %2 true) {} (take expand-until paths)))))))))
+               on-mouse-up (fn on-mouse-up* []
+                             (swap! !expanded-at dissoc :drag-state)
+                             (js/document.removeEventListener "mouseup" on-mouse-up*)
+                             (js/document.removeEventListener "mousemove" on-mouse-move))]
+    (let [expanded? (@!expanded-at path)
+          {:keys [drag-state hover-path prompt-multi-expand?]} @!expanded-at
+          multi-expand? (and hover-path prompt-multi-expand? (= (count path) (count hover-path)))]
+      [:span.group.hover:bg-indigo-100.rounded-sm.hover:shadow.cursor-pointer
+       {:class (when multi-expand? "bg-indigo-100 shadow ")
+        :on-click (partial toggle-expanded !expanded-at path)
+        :on-mouse-enter #(swap! !expanded-at assoc :hover-path path)
+        :on-mouse-leave #(swap! !expanded-at dissoc :hover-path)
+        :on-mouse-down (fn [event]
+                         (js/document.addEventListener "mouseup" on-mouse-up)
+                         (js/document.addEventListener "mousemove" on-mouse-move)
+                         (let [{:keys [x y width height]} (j/lookup (.. event -target getBoundingClientRect))
+                               x (+ x (/ width 2))
+                               y (+ y (/ height 2))]
+                           (swap! !expanded-at assoc :drag-state {:x1 x :y1 y})))}
+       (when (and (= path hover-path) drag-state (:r drag-state))
+         (let [{:keys [x1 y1 r offset]} drag-state]
+           [:svg.fixed.left-0.top-0.right-0.bottom-0.cursor-grabbing.text-indigo-600
+            {:xmlns "http://www.w3.org/2000/svg"
+             :style {:z-index 1000}
+             :ref #(when % (.setAttribute % "viewBox" (str "0 0 " js/innerWidth " " js/innerHeight)))}
+            [:circle.opacity-25 {:cx x1 :cy y1 :r r :fill "transparent" :stroke "currentColor" :stroke-width 5}]
+            [:circle.opacity-75 {:cx x1 :cy y1 :r r :fill "transparent" :stroke "currentColor" :stroke-width 5 :stroke-dasharray 565 :stroke-dashoffset offset}]]))
+       [:span.text-slate-400.group-hover:text-indigo-700
+        {:class (when multi-expand? "text-indigo-700 ")}
+        [triangle expanded?]]
+       [:span.group-hover:text-indigo-700 opening-paren]])))
+
 (defn coll-viewer [xs {:as opts :keys [path viewer !expanded-at] :or {path []}}]
   (html (let [expanded? (@!expanded-at path)
-              {:keys [hover-path prompt-multi-expand?]} @!expanded-at
-              multi-expand? (and hover-path prompt-multi-expand? (= (count path) (count hover-path)))
               {:keys [opening-paren closing-paren]} viewer]
           [:span.inspected-value.whitespace-nowrap
            {:class (when expanded? "inline-flex")}
            [:span
             (if (< 1 (count xs))
-              [:span.group.hover:bg-indigo-100.rounded-sm.hover:shadow.cursor-pointer
-               {:class (when multi-expand? "bg-indigo-100 shadow ")
-                :on-click (partial toggle-expanded !expanded-at path)
-                :on-mouse-enter #(swap! !expanded-at assoc :hover-path path)
-                :on-mouse-leave #(swap! !expanded-at dissoc :hover-path)}
-               [:span.text-slate-400.group-hover:text-indigo-700
-                {:class (when multi-expand? "text-indigo-700 ")}
-                [triangle expanded?]]
-               [:span.group-hover:text-indigo-700 opening-paren]]
+              [expand-button !expanded-at opening-paren path]
               [:span opening-paren])
             (into [:<>]
                   (comp (inspect-children opts)
@@ -498,22 +523,12 @@
 
 (defn map-viewer [xs {:as opts :keys [path viewer !expanded-at] :or {path []}}]
   (html (let [expanded? (@!expanded-at path)
-              {:keys [hover-path prompt-multi-expand?]} @!expanded-at
-              multi-expand? (and hover-path prompt-multi-expand? (= (count path) (count hover-path)))
               {:keys [closing-paren]} viewer]
           [:span.inspected-value.whitespace-nowrap
            {:class (when expanded? "inline-flex")}
            [:span
             (if (expandable? xs)
-              [:span.group.hover:bg-indigo-100.rounded-sm.hover:shadow.cursor-pointer
-               {:class (when multi-expand? "bg-indigo-100 shadow ")
-                :on-click (partial toggle-expanded !expanded-at path)
-                :on-mouse-enter #(swap! !expanded-at assoc :hover-path path)
-                :on-mouse-leave #(swap! !expanded-at dissoc :hover-path)}
-               [:span.text-slate-400.group-hover:text-indigo-700
-                {:class (when multi-expand? "text-indigo-700 ")}
-                [triangle expanded?]]
-               [:span.group-hover:text-indigo-700 "{"]]
+              [expand-button !expanded-at "{" path]
               [:span "{"])
             (into [:<>]
                   (comp (inspect-children opts)
