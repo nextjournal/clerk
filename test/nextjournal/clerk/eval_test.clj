nextjournal/clerk

commit 2bcfda3e2a6853e6d70b11332baca20f3c6e65a8

First doc pass on public viewer api, remove keyword indirection (#192)

src/nextjournal/clerk/viewer.cljc
@@ -134,6 +134,7 @@
 
 ;; TODO: Think of a better name
 (defn with-viewer-extracting-opts [viewer & opts+items]
+  ;; TODO: maybe support sequantial & viewer-opts?
   (cond
     (and (map? (first opts+items)) (not (wrapped-value? (first opts+items))))
     (with-viewer viewer (first opts+items) (rest opts+items))
@@ -1208,19 +1209,20 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; public convenience api
-(def html         (partial with-viewer :html))
-(def md           (partial with-viewer :markdown))
-(def plotly       (partial with-viewer :plotly))
-(def vl           (partial with-viewer :vega-lite))
-(def table        (partial with-viewer :table))
-(def row          (partial with-viewer-extracting-opts :row))
-(def col          (partial with-viewer-extracting-opts :col))
-(def tex          (partial with-viewer :latex))
-(def hide-result  (partial with-viewer :hide-result))
-(def notebook     (partial with-viewer :clerk/notebook))
+(def html         (partial with-viewer html-viewer))
+(def md           (partial with-viewer markdown-viewer))
+(def plotly       (partial with-viewer plotly-viewer))
+(def vl           (partial with-viewer vega-lite-viewer))
+(def table        (partial with-viewer table-viewer))
+(def row          (partial with-viewer-extracting-opts row-viewer))
+(def col          (partial with-viewer-extracting-opts col-viewer))
+(def tex          (partial with-viewer katex-viewer))
+(def hide-result  (partial with-viewer hide-result-viewer))
+(def notebook     (partial with-viewer notebook-viewer))
+(def code         (partial with-viewer code-viewer))
 (defn doc-url [path]
   (->viewer-eval (list 'v/doc-url path)))
-(def code (partial with-viewer :code))
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; examples

resources/viewer-js-hash
@@ -1,1 +1,1 @@
-37BqutqZj8NsE8ioSP3ECz3fTWWJ
+X9GQ2QnunBicdRmdwKapiZT4j2o

src/nextjournal/clerk.clj
@@ -76,20 +76,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; public viewer api
 
-;; these are refercing vars for convience when working at the REPL
-(def md             v/md)
-(def plotly         v/plotly)
-(def vl             v/vl)
-(def tex            v/tex)
-(def notebook       v/notebook)
-(def html           v/html)
-(def code           v/code)
-(def table          v/table)
-(def row            v/row)
-(def col            v/col)
-(def use-headers    v/use-headers)
-(def hide-result    v/hide-result)
-(def doc-url        v/doc-url)
+
 (def with-viewer    v/with-viewer)
 (def with-viewers   v/with-viewers)
 (def reset-viewers! v/reset-viewers!)
@@ -100,6 +87,138 @@
 (def update-val     v/update-val)
 (def mark-presented v/mark-presented)
 (def mark-preserve-keys v/mark-preserve-keys)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; public convenience api
+
+
+
+(defn html
+  "Displays `x` using the html-viewer. Supports HTML and SVG as strings or hiccup.
+
+  Supports an optional first `viewer-opts` map arg with the following optional keys:
+  * `:nextjournal.clerk/width`: set the width to `:full`, `:wide`, `:prose`
+  * `:nextjournal.clerk/viewers`: a seq of viewers to use for presentation of this value and its children
+  * `:nextjournal.clerk/opts`: a map argument that will be passed to the viewers `:render-fn`"
+  ([x] (html {} x))
+  ([viewer-opts x] (with-viewer v/html-viewer viewer-opts x)))
+
+(defn md
+  "Displays `x` with the markdown viewer.
+
+  Supports an optional first `viewer-opts` map arg with the following optional keys:
+  * `:nextjournal.clerk/width`: set the width to `:full`, `:wide`, `:prose`
+  * `:nextjournal.clerk/viewers`: a seq of viewers to use for presentation of this value and its children
+  * `:nextjournal.clerk/opts`: a map argument that will be passed to the viewers `:render-fn`"
+  ([x] (md {} x))
+  ([viewer-opts x] (with-viewer v/markdown-viewer viewer-opts x)))
+
+(defn plotly
+  "Displays `x` with the plotly viewer.
+
+  Supports an optional first `viewer-opts` map arg with the following optional keys:
+  * `:nextjournal.clerk/width`: set the width to `:full`, `:wide`, `:prose`
+  * `:nextjournal.clerk/viewers`: a seq of viewers to use for presentation of this value and its children
+  * `:nextjournal.clerk/opts`: a map argument that will be passed to the viewers `:render-fn`"
+  ([x] (plotly {} x))
+  ([viewer-opts x] (with-viewer v/plotly-viewer viewer-opts x)))
+
+(defn vl
+  "Displays `x` with the vega embed viewer, supporting both vega-lite and vega.
+
+  Supports an optional first `viewer-opts` map arg with the following optional keys:
+  * `:nextjournal.clerk/width`: set the width to `:full`, `:wide`, `:prose`
+  * `:nextjournal.clerk/viewers`: a seq of viewers to use for presentation of this value and its children
+  * `:nextjournal.clerk/opts`: a map argument that will be passed to the viewers `:render-fn`"
+  ([x] (vl {} x))
+  ([viewer-opts x] (with-viewer v/vega-lite-viewer viewer-opts x)))
+
+(defn use-headers
+  "Treats the first element of the seq `xs` as a header for the table.
+
+  Meant to be used in combination with `table`."
+  [xs]
+  (v/use-headers xs))
+
+(defn table
+  "Displays `xs` using the table viewer.
+
+  Performs normalization on the data, supporting:
+  * seqs of maps
+  * maps of seqs
+  * seqs of seqs
+
+  If you want a header for seqs of seqs use `use-headers`.
+
+  Supports an optional first `viewer-opts` map arg with the following optional keys:
+  * `:nextjournal.clerk/width`: set the width to `:full`, `:wide`, `:prose`
+  * `:nextjournal.clerk/viewers`: a seq of viewers to use for presentation of this value and its children
+  * `:nextjournal.clerk/opts`: a map argument that will be passed to the viewers `:render-fn`"
+  ([xs] (table {} xs))
+  ([viewer-opts xs] (with-viewer v/table-viewer viewer-opts xs)))
+
+(defn row
+  "Displays `xs` as rows.
+
+  Treats the first argument as `viewer-opts` if it is a map but not a `wrapped-value?`.
+
+  The `viewer-opts` map can contain the following optional keys:
+  * `:nextjournal.clerk/width`: set the width to `:full`, `:wide`, `:prose`
+  * `:nextjournal.clerk/viewers`: a seq of viewers to use for presentation of this value and its children
+  * `:nextjournal.clerk/opts`: a map argument that will be passed to the viewers `:render-fn`"
+  [& xs] (apply v/with-viewer-extracting-opts v/row-viewer xs))
+
+(defn col
+  "Displays `xs` as columns.
+
+  Treats the first argument as `viewer-opts` if it is a map but not a `wrapped-value?`.
+
+  The `viewer-opts` map can contain the following optional keys:
+  * `:nextjournal.clerk/width`: set the width to `:full`, `:wide`, `:prose`
+  * `:nextjournal.clerk/viewers`: a seq of viewers to use for presentation of this value and its children
+  * `:nextjournal.clerk/opts`: a map argument that will be passed to the viewers `:render-fn`"
+  [& xs] (apply v/with-viewer-extracting-opts v/col-viewer xs))
+
+(defn tex
+  "Displays `x` as LaTeX using KaTeX.
+
+  Supports an optional first `viewer-opts` map arg with the following optional keys:
+  * `:nextjournal.clerk/width`: set the width to `:full`, `:wide`, `:prose`
+  * `:nextjournal.clerk/viewers`: a seq of viewers to use for presentation of this value and its children
+  * `:nextjournal.clerk/opts`: a map argument that will be passed to the viewers `:render-fn"
+  ([x] (tex {} x))
+  ([viewer-opts x] (with-viewer v/katex-viewer viewer-opts x)))
+
+(defn hide-result
+  "Hides the result of evaluating `x`.
+
+  Supports an optional first `viewer-opts` map arg with the following optional keys:
+  * `:nextjournal.clerk/width`: set the width to `:full`, `:wide`, `:prose`
+  * `:nextjournal.clerk/viewers`: a seq of viewers to use for presentation of this value and its children
+  * `:nextjournal.clerk/opts`: a map argument that will be passed to the viewers `:render-fn"
+  ([x] (hide-result {} x))
+  ([viewer-opts x] (with-viewer v/hide-result-viewer viewer-opts x)))
+
+
+(defn code
+  "Displays `x` as syntax highlighted Clojure code.
+
+  A string is shown as-is, any other arg will be pretty-printed via `clojure.pprint/pprint`.
+
+  Supports an optional first `viewer-opts` map arg with the following optional keys:
+  * `:nextjournal.clerk/width`: set the width to `:full`, `:wide`, `:prose`
+  * `:nextjournal.clerk/viewers`: a seq of viewers to use for presentation of this value and its children
+  * `:nextjournal.clerk/opts`: a map argument that will be passed to the viewers `:render-fn"
+  ([code-string-or-form] (code {} code-string-or-form))
+  ([viewer-opts code-string-or-form] (with-viewer v/code-viewer viewer-opts code-string-or-form)))
+
+(def notebook
+  "Experimental notebook viewer. You probably should not use this."
+  (partial with-viewer :clerk/notebook))
+
+(defn doc-url [path]
+  (v/->viewer-eval (list 'v/doc-url path)))
 
 (defmacro example
   "Evaluates the expressions in `body` showing code next to results in Clerk.

commit 9c0e4c61e21b8e6941a1d356c4060c461f9b29e4

Fix regression in result hiding and add regression test
Was introduced with 2bcfda3e2a6853e6d70b11332baca20f3c6e65a8.

src/nextjournal/clerk/viewer.cljc
@@ -336,7 +336,11 @@
               ;; TODO: consider dropping this. Still needed by notebook-viewer fn to read :nextjournal/width option on result blocks
               opts-from-form-meta))))
 
-(defn result-hidden? [result] (= :hide-result (-> result ->value ->viewer)))
+(def hide-result-viewer
+  {:name :hide-result :transform-fn (fn [_] nil)})
+
+(defn result-hidden? [result]
+  (contains? #{:hide-result hide-result-viewer} (-> result ->value ->viewer)))
 
 (defn ->display [{:as code-cell :keys [result ns?]}]
   (let [{:nextjournal.clerk/keys [visibility]} result
@@ -718,10 +722,6 @@
                             (-> wrapped-value
                                 (update :nextjournal/value (partial process-blocks viewers))
                                 mark-presented)))})
-
-(def hide-result-viewer
-  {:name :hide-result :transform-fn (fn [_] nil)})
-
 
 (def default-viewers
   ;; maybe make this a sorted-map

test/nextjournal/clerk/eval_test.clj
@@ -161,4 +161,20 @@
                     eval/eval-string
                     view/doc->viewer
                     :nextjournal/value
-                    :blocks)))))
+                    :blocks))))
+
+  (testing "hides the result"
+    (is (= []
+           (-> "^{:nextjournal.clerk/viewer :hide-result
+  :nextjournal.clerk/visibility :hide}
+ {:some :map}
+^{:nextjournal.clerk/viewer nextjournal.clerk/hide-result
+  :nextjournal.clerk/visibility :hide}
+ {:another :map}
+^{:nextjournal.clerk/viewer nextjournal.clerk.viewer/hide-result-viewer
+  :nextjournal.clerk/visibility :hide}
+ {:a-third :map}"
+               eval/eval-string
+               view/doc->viewer
+               :nextjournal/value
+               :blocks)))))

test/nextjournal/clerk/viewer_test.clj
@@ -69,7 +69,6 @@
     (is (= :full
            (:nextjournal/width (v/apply-viewers (v/table {:nextjournal.clerk/width :full} {:a [1] :b [2] :c [3]})))))))
 
-(defn viewer-eval-inspect? [x] (= x (v/->viewer-eval 'v/inspect)))
 
 (deftest present
   (testing "only transform-fn can select viewer"
@@ -77,7 +76,7 @@
                                      [:p [:span "Hello "] [:em [:span "markdown"]] [:span "!"]]]
                  :nextjournal/viewer {:name :html-}}
                 (v/present (v/with-viewer {:transform-fn (comp v/md v/->value)}
-                              "Hello _markdown_!")))))
+                             "Hello _markdown_!")))))
 
   (testing "works with sorted-map which can throw on get & contains?"
     (v/present (into (sorted-map) {'foo 'bar})))
@@ -89,7 +88,7 @@
 (deftest assign-closing-parens
   (testing "closing parenthesis are moved to right-most children in the tree"
     (let [before (#'v/present* (assoc (v/ensure-wrapped-with-viewers {:a [1 '(2 3 #{4})]
-                                                                       :b '([5 6] 7 8)}) :path []))
+                                                                      :b '([5 6] 7 8)}) :path []))
           after (v/assign-closing-parens before)]
 
       (is (= "}"