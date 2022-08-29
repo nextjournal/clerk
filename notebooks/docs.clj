c3a7b965fcce783531fe75cef585760fcab59abb
Martin Kavalar <martin@nextjournal.com>
2022-08-29 13:08:19 +0200

    Add rationale

--- notebooks/docs.clj
+++ notebooks/docs.clj
@@ -12,13 +12,116 @@
 
 ;; ## ⚖️ Rationale
 
+;; Computational notebooks allow arguing from evidence by mixing prose with executable code. For a good overview of problems users encounter in traditional notebooks like Jupyter, see [I don't like notebooks](https://www.youtube.com/watch?v=7jiPeIFXb6U) and [What’s Wrong with Computational Notebooks? Pain Points, Needs, and Design Opportunities](https://web.eecs.utk.edu/\~azh/pubs/Chattopadhyay2020CHI_NotebookPainpoints.pdf).
+
+;; Specifically Clerk wants to address the following problems:
+
+;; * Less helpful than my editor
+;; * Notebook code being hard to reuse
+;; * Reproduction problems coming from out-of-order execution
+;; * Problems with archival and putting notebooks in source control
+
+;; Clerk is a notebook library for Clojure that aims to address these problems by doing less, namely:
+
+;; * no editing environment, folks can keep using the editors they know and love
+;; * no new format: Clerk notebooks are regular Clojure namespaces (interspersed with markdown comments). This also means Clerk notebooks are meant to be stored in source control.
+;; * no out-of-order execution: Clerk notebooks always evaluate from top to bottom. Clerk builds a dependency graph of Clojure vars and only recomputes the needed changes to keep the feedback loop fast.
+;; * no external process: Clerk runs inside your Clojure process, giving Clerk access to all code on the classpath.
+
 ;; ## 🚀 Getting Started
 
+;; ### 🤹 Clerk Demo
+
+;; When you're not yet familiar with Clerk, we recommend cloning and playing with the [nextjournal/clerk-demo](https://github.com/nextjournal/clerk-demo) repo.
+;; ```sh
+;; git clone git@github.com:nextjournal/clerk-demo.git
+;; cd clerk-demo
+;; ```
+
+;; Then open `dev/user.clj` from the project in your favorite editor start a REPL into the project, see
+;; * [Emacs & Cider](https://docs.cider.mx/cider/basics/up_and_running.html#launch-an-nrepl-server-from-emacs)
+;; * [Calva](https://calva.io/jack-in-guide/)
+;; * [Cursive](https://cursive-ide.com/userguide/repl.html)
+;; * [Vim & Neovim](https://github.com/clojure-vim/vim-jack-in)
+
+;; ### 🔌 In an Existing Project
+
+;; To use Clerk in your project, add the following dependency to your `deps.edn`:
+
+;; ```edn
+;; {:deps {io.github.nextjournal/clerk {:mvn/version "0.9.513"}}}
+;; ```
+
+;; Require and start Clerk as part of your system start, e.g. in `user.clj`:
+
+;; ```clojure
+;; (require '[nextjournal.clerk :as clerk])
+
+;; ;; start Clerk's built-in webserver on the default port 7777, opening the browser when done
+;; (clerk/serve! {:browse? true})
+
+;; ;; either call `clerk/show!` explicitly to show a given notebook.
+;; (clerk/show! "notebooks/rule_30.clj")
+;; ```
+
+;; You can then access Clerk at <http://localhost:7777>.
+
 ;; ### ⏱ File Watcher
+
+;; You can load, evaluate, and present a file with the clerk/show! function, but in most cases it's easier to start a file watcher with something like:
+
+;; ```clojure
+;; (clerk/serve! {:watch-paths ["notebooks" "src"]})
+;; ```
+;; ... which will automatically reload and re-eval any clj or md files that change, displaying the most recently changed one in your browser.
+
+;; To make this performant enough to feel good, Clerk caches the computations it performs while evaluating each file. Likewise, to make sure it doesn't send too much data to the browser at once, Clerk paginates data structures within an interactive viewer.
+
 
 ;; ### 🔪 Editor Integration
 
 ;; For even better flow states, we recommend you bind clerk/show! to a shortcut in your favorite editor:
+
+
+;; **Emacs**
+
+;; In Emacs, add the following to your config:
+
+;; ```elisp
+;; (defun clerk-show ()
+;;   (interactive)
+;;   (save-buffer)
+;;   (let
+;;       ((filename
+;;         (buffer-file-name)))
+;;     (when filename
+;;       (cider-interactive-eval
+;;        (concat "(nextjournal.clerk/show! \"" filename "\")")))))
+
+;; (define-key clojure-mode-map (kbd "<M-return>") 'clerk-show)
+;; ```
+
+;; **IntelliJ/Cursive**
+
+;; In IntelliJ/Cursive, you can [set up REPL commands](https://cursive-ide.com/userguide/repl.html#repl-commands) via:
+
+;; * going to `Tools→REPL→Add New REPL Command`, then
+;; * add the following command: `(show! "~file-path")`;
+;; * make sure the command is executed in the `nextjournal.clerk` namespace;
+;; * lastly assign a shortcut of your choice via `Settings→Keymap`
+
+;; **Neovim + Conjure**
+
+;; With [neovim](https://neovim.io/) + [conjure](https://github.com/Olical/conjure/) one can use the following vimscript function to save the file and show it with Clerk:
+
+;; ```
+;; function! ClerkShow()
+;; exe "w"
+;; exe "ConjureEval (nextjournal.clerk/show! \"" . expand("%:p") . "\")"
+;; endfunction
+
+;; nmap <silent> <localleader>cs :execute ClerkShow()<CR>
+;; ```
 
 
 
