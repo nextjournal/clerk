39d354d65b01429f3d7f70554924c77a556cf7f6
Andrea Amantini <lo.zampino@gmail.com>
2022-09-21 17:33:40 +0200

    Attempt at a better fix

--- notebooks/slideshow.clj
+++ notebooks/slideshow.clj
@@ -10,25 +10,24 @@
 ;;
 ;; `->slide` wraps a collection of blocks into markup suitable for rendering a slide.
 (defn ->slide [blocks]
+  ;; TODO: move to render-fn
   [:div.flex.flex-col.justify-center
    {:style {:min-block-size "100vh"}}
    (into [:div.text-xl.p-20 {:class ["prose max-w-none prose-h1:mb-0 prose-h2:mb-8 rose-h3:mb-8 prose-h4:mb-8"
                                      "prose-h1:text-6xl prose-h2:text-5xl prose-h3:text-3xl prose-h4:text-2xl"]}]
-         (map (fn [{:as block :keys [result]}]
-                (if result
-                  ;; TODO: ideally use (v/with-viewer :clerk/result-block block), currenlty messes wrapping during postwalk below
-                  (v/transform-result {:nextjournal/value block})
-                  ;; render code blocks with markdown for convenience
-                  (v/md block))))
          blocks)])
 
 ;; ---
 ;; The `doc->slides` helper function takes a Clerk notebook and partitions its blocks into slides by occurrences of markdown rulers.
 (defn doc->slides [{:as doc :keys [blocks]}]
   (sequence (comp (mapcat (partial v/with-block-viewer doc))
-                  (mapcat #(if (= :markdown (v/->viewer %)) (-> % v/->value :content) [(v/->value %)]))
-                  (partition-by (comp #{:ruler} :type))
-                  (remove (comp #{:ruler} :type first))
+                  (mapcat #(cond
+                             (= :markdown (v/->viewer %)) (map v/md (-> % v/->value :content))
+                             (= :clerk/code-block (v/->viewer %)) [(-> % v/->value v/md)]
+                             'else [%]))
+                  (partition-by (comp #{:ruler} :type v/->value))
+                  (remove (comp #{:ruler} :type v/->value first))
+                  (map (fn [bs] (map #(cond-> % (= :clerk/result-block (v/->viewer %)) v/apply-viewers) bs)))
                   (map ->slide))
             blocks))
 ;; ---
@@ -36,6 +35,9 @@
 (def slideshow-viewer
   {:name :clerk/notebook
    :transform-fn (comp v/mark-presented
+                       ;; TODO: drop mark-presented and the postwalk here in favour of a collection of slides as maps with preserved keys
+                       ;; {:as slide :keys [blocks]}
+                       ;; then use [v/inspect-presented block] in the viewer here
                        (v/update-val (comp (partial w/postwalk (v/when-wrapped v/inspect-wrapped-value))
                                            doc->slides)))
    :render-fn '(fn [slides]
