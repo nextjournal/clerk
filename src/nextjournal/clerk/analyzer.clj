nextjournal/clerk

commit 01875b21be921b8ca91611d9d06f0142a7a1e157

Fail eval if var is only present at runtime but not in file
This makes Clerk be more strict than the REPL and actually complain
when one still depends on a var no longer present in the file.

The check is currently only performed for the notebook being shown iff
it starts with an `ns` form.

src/nextjournal/clerk/analyzer.clj
@@ -218,6 +218,14 @@
                                  doc? (assoc :ns *ns*))]
                      (when ns-effect?
                        (eval form))
+                     (when-let [missing-dep (and (:ns? doc)
+                                                 (first (set/difference (into #{}
+                                                                              (filter #(and (symbol? %)
+                                                                                            (#{(-> state :ns ns-name name)} (namespace %))))
+                                                                              deps)
+                                                                        (-> state :->analysis-info keys set))))]
+                       (throw (ex-info (str "Could not resolve var: " (name missing-dep))
+                                       (merge {:var missing-dep} (select-keys analyzed [:form]) (select-keys doc [:file])))))
                      (if (seq deps)
                        (-> (reduce (partial analyze-deps analyzed) state deps)
                            (make-deps-inherit-no-cache analyzed))

test/nextjournal/clerk/analyzer_test.clj
@@ -175,7 +175,17 @@
   (testing "preserves *ns*"
     (with-ns-binding 'nextjournal.clerk.analyzer-test
       (is (= (find-ns 'nextjournal.clerk.analyzer-test)
-             (do (analyze-string "(ns example-notebook)") *ns*)))))
+             (do (analyze-string ";; boo\n\n (ns example-notebook)") *ns*)))))
+
+  (testing "should fail when var is only present at runtime but not in file"
+    (let [ns (create-ns 'missing-var)]
+      (intern (create-ns 'missing-var) 'foo :bar)
+      (is (thrown? Exception (analyze-string "(ns missing-var) foo")))))
+
+  (testing "should not fail on var present at runtime if there's no ns form"
+    (let [ns (create-ns 'existing-var)]
+      (intern (create-ns 'existing-var) 'foo :bar)
+      (analyze-string "(in-ns 'existing-var) foo")))
 
   (testing "defmulti has no deref deps"
     (is (empty? (-> "(defmulti foo :bar)" analyze-string :blocks first :deref-deps)))))

src/nextjournal/clerk/parser.clj
@@ -46,9 +46,11 @@
 
 (defn ->doc-settings [first-form]
   {:visibility (->doc-visibility first-form)
+   :ns? (ns? first-form)
    :toc (or (#{true :collapsed} (-> first-form meta :nextjournal.clerk/toc)) false)})
 
 #_(->doc-settings '(ns foo))
+#_(->doc-settings '(in-ns 'foo))
 #_(->doc-settings '^{:nextjournal.clerk/toc true} (ns foo))
 #_(->doc-settings '^{:nextjournal.clerk/toc :pin} (ns foo))
 #_(->doc-settings '^{:nextjournal.clerk/toc :boom} (ns foo)) ;; TODO: error
@@ -116,7 +118,7 @@
                 (-> state
                     (assoc :add-comment-on-line? false)
                     (update :nodes rest))))
-       (merge (select-keys state [:blocks :visibility])
+       (merge (select-keys state [:blocks :visibility :ns?])
               (when doc?
                 (-> {:content (into []
                                     (comp (filter (comp #{:markdown} :type))

test/nextjournal/clerk/parser_test.clj
@@ -40,6 +40,7 @@
                                     {:type :code, :text "{2 \"bar\" 1 \"foo\"}"}],
                            :visibility #{:show},
                            :title "📶 Sorting",
+                           :ns? true
                            :toc {:type :toc,
                                  :mode true,
                                  :children [{:type :toc :children [{:type :toc}

commit 24b00bbf483c5803735dabe0863cdb628b950936

Fix analyzer issues with clojure proxy (#221)
Fixes a regression in analyzer introduced with 01875b21be921b8ca91611d9d06f0142a7a1e157  which prevents notebooks with calls to `clojure.core/proxy` to be evaluated.

Fixes #222.

Co-authored-by: Martin Kavalar <martin@nextjournal.com>

resources/viewer-js-hash
@@ -1,1 +1,1 @@
-3rau7AxxNuqJe1GVYdUBMwnsJeZx
+273BuUNzWG5yHs4azyACePmHxPGM

src/nextjournal/clerk/analyzer.clj
@@ -160,6 +160,7 @@
                    ([s] (clojure.string/includes?
                          (rewrite-clj.parser/parse-string-all s) "hi")))))
 #_(type (first (:deps (analyze #'analyze-form))))
+#_(-> (analyze '(proxy [clojure.lang.ISeq] [])))
 
 
 (defn- circular-dependency-error? [e]
@@ -217,6 +218,21 @@
   (-> (reduce add-block-id (assoc analyzed-doc :blocks [] :id->count {} ) blocks)
       (dissoc :id->count)))
 
+(defn ^:private internal-proxy-name?
+  "Returns true if `sym` represents a var name interned by `clojure.core/proxy`."
+  [sym]
+  (str/includes? (name sym) ".proxy$"))
+
+(defn throw-if-dep-is-missing [doc state analyzed]
+  (when-let [missing-dep (and (first (set/difference (into #{}
+                                                           (comp (filter #(and (symbol? %)
+                                                                               (#{(-> state :ns ns-name name)} (namespace %))))
+                                                                 (remove internal-proxy-name?))
+                                                           (:deps analyzed))
+                                                     (-> state :->analysis-info keys set))))]
+    (throw (ex-info (str "The var `#'" missing-dep "` exists at runtime, but Clerk cannot find it in the namespace. Did you remove it?")
+                    (merge {:var-name missing-dep} (select-keys analyzed [:form]) (select-keys doc [:file]))))))
+
 (defn analyze-doc
   ([doc]
    (analyze-doc {:doc? true :graph (dep/graph)} doc))
@@ -241,14 +257,8 @@
                                                        (->ana-keys analyzed))
                                          doc? (update-in [:blocks i] merge (dissoc analyzed :deps :no-cache? :ns-effect?))
                                          (and doc? (not (contains? state :ns))) (merge (parser/->doc-settings form) {:ns *ns*}))]
-                             (when-let [missing-dep (and (:ns? state)
-                                                         (first (set/difference (into #{}
-                                                                                      (filter #(and (symbol? %)
-                                                                                                    (#{(-> state :ns ns-name name)} (namespace %))))
-                                                                                      deps)
-                                                                                (-> state :->analysis-info keys set))))]
-                               (throw (ex-info (str "Could not resolve var: " (name missing-dep))
-                                               (merge {:var missing-dep} (select-keys analyzed [:form]) (select-keys doc [:file])))))
+                             (when (:ns? state)
+                               (throw-if-dep-is-missing doc state analyzed))
                              (if (seq deps)
                                (-> (reduce (partial analyze-deps analyzed) state deps)
                                    (make-deps-inherit-no-cache analyzed))

test/nextjournal/clerk/analyzer_test.clj
@@ -210,6 +210,9 @@ my-uuid"
                    :->analysis-info
                    vals))))
 
+(deftest can-analyze-proxy-macro
+  (is (analyze-string "(ns proxy-example-notebook) (proxy [clojure.lang.ISeq][] (seq [] '(this is a test seq)))")))
+
 (deftest circular-dependency
   (is (match? {:graph {:dependencies {'(ns circular) any?
                                       'circular/b #{'clojure.core/str 'circular/a+circular/b}