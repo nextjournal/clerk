3e03bfb5f0b72ebcb92b3f5c236f48b9837e9c39
Philippa Markovics <philippa@markovics.com>
2022-10-03 16:17:38 +0200

    Make exec duration viz more comparable

--- src/nextjournal/clerk/builder_ui.clj
+++ src/nextjournal/clerk/builder_ui.clj
@@ -89,7 +89,7 @@
       
       (when-let [{:keys [code markdown code-executing]} (not-empty block-counts)]
         [:div
-         [:span.text-sm.mr-2
+         [:span.text-xs.mr-3
           (when code
             [:<>
              (when code-executing
@@ -97,17 +97,20 @@
              (str code " code")])
           (when markdown (str (when code " & ") markdown " markdown"))
           " blocks"]
-         (when duration [:span.text-sm.font-mono (int duration) "ms"])])]]
-    (when duration
-      [:div.rounded-full.bg-green-600
-       {:class "h-[4px] mt-[-5px]"
-        :style {:min-width 1 :width (str (int (* 100 (/ duration max-duration))) "%")}}])]
+         (when duration
+           [:div.inline-flex.items-center
+            [:div.bg-slate-300
+             {:class "h-[4px] w-[50px]"}
+             [:div.bg-green-600.border-r.border-white
+              {:class "h-[6px] -mt-[1px] min-w-[2px]"
+               :style {:width (str (int (* 100 (/ duration max-duration))) "%")}}]]
+            [:span.font-mono.ml-1
+             {:class "w-[40px] text-[10px]"}
+             (int duration) "ms"]])])]]]
    #_(when (= :executing state)
        (blocks-view doc))
    #_[:div.mx-auto.w-8.border.border-t-0.border-slate-300.bg-slate-50.rounded-b.text-slate-500.flex.justify-center.shadow.hover:bg-slate-100.cursor-pointer
-      [:svg.h-3.w-3
-       {:xmlns "http://www.w3.org/2000/svg" :fill "none" :viewBox "0 0 24 24" :stroke "currentColor" :stroke-width "2"}
-       [:path {:stroke-linecap "round" :stroke-linejoin "round" :d "M19 9l-7 7-7-7"}]]]])
+      ]])
 
 
 #_(doc-build-badge (-> @!build-state :docs (nth 3)))
