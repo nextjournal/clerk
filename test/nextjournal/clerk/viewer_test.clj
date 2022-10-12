ff96721163f1a2dd31966475764e90b69c6e2cd7
Andrea Amantini <lo.zampino@gmail.com>
2022-10-12 16:30:01 +0200

    Move view test to viewer test

--- test/nextjournal/clerk/viewer_test.clj
+++ test/nextjournal/clerk/viewer_test.clj
@@ -3,7 +3,8 @@
             [clojure.test :refer :all]
             [matcher-combinators.test :refer [match?]]
             [nextjournal.clerk.viewer :as v]
-            [nextjournal.clerk.eval :as eval]))
+            [nextjournal.clerk.eval :as eval]
+            [clojure.walk :as w]))
 
 (defn present+fetch
   ([value] (present+fetch {} value))
@@ -96,8 +97,7 @@
   (testing "opts are not propagated to children during presentation"
     (let [count-opts (fn [o]
                        (let [c (atom 0)]
-                         (clojure.walk/postwalk (fn [f] (when (= :nextjournal/opts f) (swap! c inc)) f)
-                                                o)
+                         (w/postwalk (fn [f] (when (= :nextjournal/opts f) (swap! c inc)) f) o)
                          @c))]
       (let [presented (v/present (v/col {:nextjournal.clerk/opts {:width 150}} 1 2 3))]
         (is (= {:width 150} (:nextjournal/opts presented)))
@@ -138,3 +138,18 @@
                  (get 1)
                  v/->viewer
                  :closing-paren))))))
+
+(deftest doc->viewer
+  (testing "Doc options are propagated to blob processing"
+    (let [doc->viewer (resolve 'nextjournal.clerk.view/doc->viewer)
+          test-doc (eval/eval-string "(java.awt.image.BufferedImage. 20 20 1)")
+          tree-re-find (fn [data re] (->> data
+                                          (tree-seq coll? seq)
+                                          (filter string?)
+                                          (filter (partial re-find re))))]
+
+      (is (not-empty (tree-re-find (doc->viewer {:inline-results? true :bundle? true} test-doc)
+                                   #"data:image/png;base64")))
+
+      (is (not-empty (tree-re-find (doc->viewer {:inline-results? true :bundle? false} test-doc)
+                                   #"\"_data/.+\.png\""))))))
