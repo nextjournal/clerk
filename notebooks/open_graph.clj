b972be682e9faf947cd6264c72f6695146acea58
Andrea Amantini <lo.zampino@gmail.com>
2022-10-24 14:21:45 +0200

    First notebook draft

---
+++ notebooks/open_graph.clj
@@ -0,0 +1,20 @@
+;; # 🗂 Open Graph Metadata
+(ns open-graph
+  {:nextjournal.clerk/open-graph
+   {:title "My title"
+    :type "article"
+    :url "https://clerk.vision"
+    :image "https://url-to-image"}}
+  (:require [nextjournal.clerk :as clerk])
+  (:import (javax.imageio ImageIO)
+           (java.net URL)))
+
+;; Clerk static page generator need to produce valid [Open Graph](https://ogp.me) metadata.
+
+;; A Specific result may be specified via metadata
+^::clerk/open-graph-image
+(clerk/col
+ (clerk/html [:h1 "This is my Card"]))
+
+;; By default the last result will serve the purpose.
+(ImageIO/read (URL. "https://etc.usf.edu/clipart/186600/186669/186669-trees-in-the-winter_sm.gif"))
