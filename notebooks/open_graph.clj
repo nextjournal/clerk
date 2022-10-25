15dc9f97523b0fc8b4084d137036fd051beacc1f
Andrea Amantini <lo.zampino@gmail.com>
2022-10-25 15:50:20 +0200

    Sketch of OG Card Preview viewer

--- notebooks/open_graph.clj
+++ notebooks/open_graph.clj
@@ -1,10 +1,11 @@
 ;; # 🗂 Open Graph Metadata
 (ns open-graph
   {:nextjournal.clerk/open-graph
-   {:title "My title"
-    :url "https://clerk.vision"
+   {:url "https://clerk.vision"
     :image "https://url-to-image"}}
-  (:require [nextjournal.clerk :as clerk])
+  (:require [nextjournal.clerk :as clerk]
+            [clojure.java.shell :as shell]
+            [babashka.fs :as fs])
   (:import (javax.imageio ImageIO)
            (java.net URL)))
 
@@ -14,18 +15,55 @@
 ;; A Specific result may be specified via metadata
 ^{::clerk/open-graph {:image :result}}
 (clerk/col
- (clerk/html [:h1 "This is my Card"]))
+ (clerk/html [:h1.pt-10 {:style {:font-size "3rem"}} "Stats"])
+ (clerk/plotly {:layout {:margin {:l 20 :r 0 :b 20 :t 0}
+                         :autosize false :width 300 :height 200}
+                :config {:displayModeBar false :displayLogo false}
+                :data [{:x ["giraffes" "orangutans" "monkeys"]
+                        :y [20 14 23]
+                        :type "bar"}]}))
 
 ;; By default the last result will serve the purpose.
 (ImageIO/read (URL. "https://etc.usf.edu/clipart/186600/186669/186669-trees-in-the-winter_sm.gif"))
 
-(defn render-open-graph-metadata [{:as doc :keys [open-graph]}]
-  (into [:<>]
-        (keep (fn [[prop content]]
-                (when (string? content)
-                  [:meta {:property (str "og" prop) :content content}])))
-        open-graph))
+(def og-card-preview
+  {:transform-fn (comp clerk/mark-presented (clerk/update-val :open-graph))
+   :render-fn
+   '(fn [{:as open-graph :keys [title description image]}]
+      [:div.flex.flex-col.items-center
+       [:div.border.border-gray-200 {:style {:width "10rem"}}
+        [:h2 title]
+        [:img {:src "build/foo/bar.png"}]
+        [:div description]]
+       [:div.viewer-code.border.border-grey-200
+        [nextjournal.clerk.render/inspect
+         (v/code
+          (into [:<>]
+                (keep (fn [[prop content]]
+                        (when (string? content)
+                          [:meta {:property (str "og" prop) :content content}])))
+                open-graph))]]])})
 
-(clerk/code
- (some-> @nextjournal.clerk.webserver/!doc
-         render-open-graph-metadata))
+(clerk/with-viewer og-card-preview
+  @nextjournal.clerk.webserver/!doc
+  #_ #_ use-this-if-the-above-hangs-show
+  {:open-graph {:type "article:clerk",
+                :title "My title",
+                :description "Clerk static page generator need to produce valid Open Graph metadata.",
+                :url "https://clerk.vision",
+                :image "https://url-to-image"}})
+
+^{:nextjournal.clerk/visibility {:code :hide :result :hide}}
+(comment
+
+  (do
+    (fs/delete-tree "ui_tests/screenshots")
+    (shell/sh "yarn" "nbb" "-m" "screenshots"
+              "--url" "localhost:7777"
+              "--out-dir" "screenshots"
+              :dir "ui_tests")
+    (as-> (fs/path "ui_tests/screenshots") dir
+      (when (fs/exists? dir)
+        (fs/list-dir dir))))
+
+  )
