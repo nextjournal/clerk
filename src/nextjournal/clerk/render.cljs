67e55579267d07a75952bcabfcbce47e50b2be18
Andrea Amantini <lo.zampino@gmail.com>
2022-10-31 18:06:01 +0100

    First take at hookifying code viewer inline in render ns

--- src/nextjournal/clerk/render.cljs
+++ src/nextjournal/clerk/render.cljs
@@ -3,6 +3,14 @@
             ["react" :as react]
             ["react-dom/client" :as react-client]
             ["use-sync-external-store/shim" :refer [useSyncExternalStore]]
+
+   ;; TODO: move to own ns
+            ["@codemirror/language" :refer [syntaxHighlighting HighlightStyle]]
+            ["@codemirror/state" :refer [EditorState]]
+            ["@codemirror/view" :refer [EditorView]]
+            ["@lezer/highlight" :refer [tags]]
+            [nextjournal.clojure-mode :as cm-clj]
+
             [applied-science.js-interop :as j]
             [cljs.reader]
             [clojure.string :as str]
@@ -765,7 +773,79 @@
         default-loading-view))))
 
 (def render-mathjax mathjax/viewer)
-(def render-code code/viewer)
+#_(def render-code code/viewer)
+
+;; code viewer
+(def cm6-theme
+  (.theme EditorView
+          (j/lit {"&.cm-focused" {:outline "none"}
+                  ".cm-line" {:padding "0"
+                              :line-height "1.6"
+                              :font-size "15px"
+                              :font-family "\"Fira Mono\", monospace"}
+                  ".cm-matchingBracket" {:border-bottom "1px solid var(--teal-color)"
+                                         :color "inherit"}
+
+                  ;; only show cursor when focused
+                  ".cm-cursor" {:visibility "hidden"}
+                  "&.cm-focused .cm-cursor" {:visibility "visible"
+                                             :animation "steps(1) cm-blink 1.2s infinite"}
+                  "&.cm-focused .cm-selectionBackground" {:background-color "Highlight"}
+                  ".cm-tooltip" {:border "1px solid rgba(0,0,0,.1)"
+                                 :border-radius "3px"
+                                 :overflow "hidden"}
+                  ".cm-tooltip > ul > li" {:padding "3px 10px 3px 0 !important"}
+                  ".cm-tooltip > ul > li:first-child" {:border-top-left-radius "3px"
+                                                       :border-top-right-radius "3px"}})))
+
+(def highlight-style
+  (.define HighlightStyle
+           (clj->js [{:tag (.-meta tags) :class "cmt-meta"}
+                     {:tag (.-link tags) :class "cmt-link"}
+                     {:tag (.-heading tags) :class "cmt-heading"}
+                     {:tag (.-emphasis tags) :class "cmt-italic"}
+                     {:tag (.-strong tags) :class "cmt-strong"}
+                     {:tag (.-strikethrough tags) :class "cmt-strikethrough"}
+                     {:tag (.-keyword tags) :class "cmt-keyword"}
+                     {:tag (.-atom tags) :class "cmt-atom"}
+                     {:tag (.-bool tags) :class "cmt-bool"}
+                     {:tag (.-url tags) :class "cmt-url"}
+                     {:tag (.-contentSeparator tags) :class "cmt-contentSeparator"}
+                     {:tag (.-labelName tags) :class "cmt-labelName"}
+                     {:tag (.-literal tags) :class "cmt-literal"}
+                     {:tag (.-inserted tags) :class "cmt-inserted"}
+                     {:tag (.-string tags) :class "cmt-string"}
+                     {:tag (.-deleted tags) :class "cmt-deleted"}
+                     {:tag (.-regexp tags) :class "cmt-regexp"}
+                     {:tag (.-escape tags) :class "cmt-escape"}
+                     {:tag (.. tags (special (.-string tags))) :class "cmt-string"}
+                     {:tag (.. tags (definition (.-variableName tags))) :class "cmt-variableName"}
+                     {:tag (.. tags (local (.-variableName tags))) :class "cmt-variableName"}
+                     {:tag (.-typeName tags) :class "cmt-typeName"}
+                     {:tag (.-namespace tags) :class "cmt-namespace"}
+                     {:tag (.-className tags) :class "cmt-className"}
+                     {:tag (.. tags (special (.-variableName tags))) :class "cmt-variableName"}
+                     {:tag (.-macroName tags) :class "cmt-macroName"}
+                     {:tag (.. tags (definition (.-propertyName tags))) :class "cmt-propertyName"}
+                     {:tag (.-comment tags) :class "cmt-comment"}
+                     {:tag (.-invalid tags) :class "cmt-invalid"}])))
+
+(def ext #js [cm-clj/default-extensions
+              (syntaxHighlighting highlight-style)
+              (.. EditorView -editable (of false))
+              cm6-theme])
+
+(defn render-code [value]
+  (let [ref (use-ref nil)]
+    (use-effect (fn []
+                  (js/console.log :mounting @ref)
+                  (let [^js editor-view
+                        (EditorView. #js {:state (.create EditorState #js {:doc value
+                                                                           :extensions ext})
+                                          :parent @ref})]
+                    (fn [] (js/console.log :unmounting editor-view)
+                      (.destroy editor-view)))))
+    [:div {:ref ref}]))
 
 (def expand-icon
   [:svg {:xmlns "http://www.w3.org/2000/svg" :viewBox "0 0 20 20" :fill "currentColor" :width 12 :height 12}
