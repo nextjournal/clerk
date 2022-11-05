3b8e275acf2ea0735355cfd409e318c4e2a1e5d5
Matthew Huebert <mhuebert@gmail.com>
2022-11-05 08:44:24 +0100

    fix release

--- viewer/src/nextjournal/clerk/dev_launcher.clj
+++ viewer/src/nextjournal/clerk/dev_launcher.clj
@@ -4,24 +4,34 @@
   Avoiding other ns requires here so the REPL comes up early."
   (:require [nrepl.cmdline :as nrepl]
             [clojure.java.io :as io]
-            [shadow.cljs.devtools.config :as config])
+            [shadow.cljs.devtools.config :as config]
+            [shadow.cljs.devtools.api :as shadow]
+            [shadow.cljs.devtools.server :as shadow.server]
+            [nextjournal.clerk :as clerk])
   (:import (java.lang.management ManagementFactory)
            (java.util Locale)))
 
-(defn get-build [opts]
+(defn get-config [opts]
   (-> (io/resource "nextjournal/clerk/shadow-cljs.edn")
       config/read-config
+      (assoc-in [:builds :viewer :output-dir] (str (:out-path opts) "/js"))
+      (assoc-in [:builds :viewer :release :output-dir] (str (:out-path opts) "/js"))
       config/normalize
-      (->> (merge config/default-config))
+      (->> (merge config/default-config))))
+
+(defn get-build [opts]
+  (-> (get-config opts)
       (config/get-build :viewer)
       (assoc :nextjournal.clerk/extra-namespaces (:extra-namespaces opts))))
 
 (defn start [serve-opts]
   (future (nrepl/dispatch-commands {:middleware '[cider.nrepl/cider-middleware]}))
   (require 'shadow.cljs.silence-default-loggers)
-  ((requiring-resolve 'shadow.cljs.devtools.server/start!))
-  ((requiring-resolve 'shadow.cljs.devtools.api/watch) (get-build serve-opts))
-  ((requiring-resolve 'nextjournal.clerk/serve!) serve-opts)
+
+  (shadow.server/start! (get-config serve-opts))
+  (shadow/watch (get-build serve-opts))
+  (clerk/serve! serve-opts)
+
   (set! *print-namespace-maps* false)
   (println "Clerk dev system ready in"
            (String/format (Locale. "en-US")
@@ -37,8 +47,11 @@
                  :shadow.build/config
                  :nextjournal.clerk/extra-namespaces)))
 
-(defn release [opts]
-  ((requiring-resolve 'shadow.cljs.devtools.api/release) (get-build opts)))
+(defn release [clerk-opts shadow-opts]
+  (shadow/with-runtime
+   (shadow/release*
+    (get-build clerk-opts)
+    shadow-opts)))
 
 (comment
 
