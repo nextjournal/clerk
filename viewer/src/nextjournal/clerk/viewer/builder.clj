f18afd7e6165748765ea43c23604cc1965f20e35
Matthew Huebert <mhuebert@gmail.com>
2022-11-07 19:33:33 +0100

    release stops/starts

--- viewer/src/nextjournal/clerk/viewer/builder.clj
+++ viewer/src/nextjournal/clerk/viewer/builder.clj
@@ -2,7 +2,8 @@
   (:require [clojure.java.io :as io]
             [shadow.cljs.devtools.api :as shadow]
             [shadow.cljs.devtools.config :as config]
-            [shadow.cljs.devtools.server :as shadow.server]))
+            [shadow.cljs.devtools.server :as shadow.server]
+            [shadow.cljs.devtools.server.runtime :as runtime]))
 
 (defn get-config [opts]
   (-> (io/resource "nextjournal/clerk/shadow-cljs.edn")
@@ -21,11 +22,13 @@
   "Builds a release of Clerk viewer, returns asset path."
   [opts shadow-opts]
   (require 'shadow.cljs.silence-default-loggers)
-  (shadow.server/start! (get-config opts))
-  (let [state (shadow/with-runtime
+  (let [server-running? (runtime/get-instance)
+        _ (when-not server-running? (shadow.server/start! (get-config opts)))
+        state (shadow/with-runtime
                (shadow/release*
                 (get-build opts)
                 shadow-opts))
+        _ (when-not server-running? (shadow.server/stop!))
         output-name (->> (or (:shadow.build.closure/modules state)
                              (:build-modules state))
                          (filter (comp #{:viewer} :module-id))
