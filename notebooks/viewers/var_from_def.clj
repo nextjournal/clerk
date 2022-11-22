ab4b8975a4849075f1a4cd74dbd7ba2a4bb3b932
Andrea Amantini <lo.zampino@gmail.com>
2022-11-22 12:00:55 +0100

    Clarify examples

--- notebooks/viewers/var_from_def.clj
+++ notebooks/viewers/var_from_def.clj
@@ -3,8 +3,7 @@
   {:nextjournal.clerk/no-cache true}
   (:require [nextjournal.clerk :as clerk]
             [nextjournal.clerk.viewer :as v]))
-
-;; By default, for def-like expressions we're showing the underlying var value.
+;; By default, for def-like expressions we're showing the value of the underlying variable.
 (def d1 "def1")
 
 (defonce d2 "def2")
@@ -12,12 +11,14 @@
 ^{::clerk/viewer {:render-fn '(fn [x] [:pre (pr-str x)])}}
 (def d3 "def3")
 
-;; We can opt-out of the default behavour and obtain a var wraapped in a map with: `{::clerk/var-from-def my-var}` like so:
+;; We can opt out of the default behavour and obtain a var wraapped in a map like: `{::clerk/var-from-def my-var}` by setting a truthy `:var-from-def?` on the viewer map:
 
-^{::clerk/viewer {:var-from-def? true
-                  :render-fn '(fn [x] [:pre (pr-str x)])}}
-(def ^{:foo 'bar} d4 "def4")
+(def var-wrapping-viewer
+  {:var-from-def? true
+   :render-fn '(fn [x] [:pre (pr-str x)])})
 
-^{::clerk/viewer {:var-from-def? true
-                  :transform-fn (v/update-val (comp meta ::clerk/var-from-def))}}
+^{::clerk/viewer var-wrapping-viewer}
+(def d4 "def4")
+
+^{::clerk/viewer (assoc var-wrapping-viewer :transform-fn (v/update-val (comp meta ::clerk/var-from-def)))}
 (def ^{:foo 'bar} d5 "def5")
