12da826b61bbfddc4c975c7aaef0cfedfd6efe89
Martin Kavalar <martin@nextjournal.com>
2022-12-05 15:39:45 +0100

    Add simple quoted render-fn example

--- notebooks/jump_to_definition.clj
+++ notebooks/jump_to_definition.clj
@@ -5,25 +5,25 @@
    [nextjournal.clerk :as clerk]
    [nextjournal.clerk.render :as-alias render]
    [nextjournal.clerk.render.hooks :as-alias hooks]
-   [nextjournal.clerk.viewer :as viewer]))
+   [nextjournal.clerk.viewer :as v]))
 
 ;; Trying various ways to support jump to definition
-(clerk/code (slurp ".clj-kondo/hooks.clj"))
+(def my-code-viewer
+  {:render-fn 'nextjournal.clerk.render/render-code :transform-fn clerk/mark-presented})
 
-(viewer/->viewer-fn
- '(fn [spec] (let [plotly (hooks/use-d3-require "plotly.js-dist@2.15.1")
-                   ref-fn (hooks/use-callback #(when % (.newPlot plotly % (clj->js spec ))) [spec plotly])]
-               (js/console.log "dude")
-               (when spec
-                 (if plotly
-                   [:div.overflow-x-auto [:div.plotly {:ref ref-fn}]]
-                   render/default-loading-view)))))
 
-(viewer/->viewer-eval
- '(fn [spec] (let [plotly (hooks/use-d3-require "plotly.js-dist@2.15.1")
-                   ref-fn (hooks/use-callback #(when % (.newPlot plotly % (clj->js spec))) [spec plotly])]
-               (js/console.log "dude")
-               (when spec
-                 (if plotly
-                   [:div.overflow-x-auto [:div.plotly {:ref ref-fn}]]
-                   render/default-loading-view)))))
+(v/->viewer-fn '(fn [spec] (let [plotly (hooks/use-d3-require "plotly.js-dist@2.15.1")
+                                 ref-fn (hooks/use-callback #(when % (.newPlot plotly % (clj->js spec ))) [spec plotly])]
+                             (js/console.log "dude")
+                             (when spec
+                               (if plotly
+                                 [:div.overflow-x-auto [:div.plotly {:ref ref-fn}]]
+                                 render/default-loading-view)))))
+
+(v/->viewer-eval '(fn [spec] (let [plotly (hooks/use-d3-require "plotly.js-dist@2.15.1")
+                                   ref-fn (hooks/use-callback #(when % (.newPlot plotly % (clj->js spec))) [spec plotly])]
+                               (js/console.log "dude")
+                               (when spec
+                                 (if plotly
+                                   [:div.overflow-x-auto [:div.plotly {:ref ref-fn}]]
+                                   render/default-loading-view)))))
