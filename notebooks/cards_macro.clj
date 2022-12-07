476736df2808263f9c1dfab09af7118fe1c0dbf1
Martin Kavalar <martin@nextjournal.com>
2022-12-07 15:21:28 +0100

    Simpler macro

--- notebooks/cards_macro.clj
+++ notebooks/cards_macro.clj
@@ -1,6 +1,4 @@
 (ns cards-macro
   (:require [nextjournal.clerk.viewer :as v]))
 
-(defmacro card
-  ([body] `(v/->viewer-eval '~body))
-  ([opts body] (v/with-viewer {:tranform-fn identity} opts `(v/->viewer-eval '~body))))
+(defmacro card [body] `(v/->viewer-eval '~body))
