4363fba732a14aa0ac1f5c1342f1b8e5f504ec07
Andrea Amantini <lo.zampino@gmail.com>
2022-12-14 19:28:50 +0100

    Reduce diff

--- notebooks/viewers/code.clj
+++ notebooks/viewers/code.clj
@@ -1,16 +1,16 @@
 ;; # 👔 Code Viewer
 (ns viewers.code
-  {:nextjournal.clerk/toc true}
+  {:nextjournal.clerk/no-cache true}
   (:require [nextjournal.clerk :as clerk]
             [nextjournal.clerk.viewer :as viewer]))
 
-;; ## Code as data
+;; Code as data
 (clerk/code '(def fib (lazy-cat [0 1] (map + fib (rest fib)))))
-;; ## Code as string
+;; Code as string
 (clerk/code "(def fib (lazy-cat [0 1] (map + fib (rest fib))))")
 
-;; ## Clojure Syntax Highlight
-;; strings with whitespace
+;; Stings with line-breaks and whitespace
+
 (def ex
   (identity "1000
 2000
@@ -27,6 +27,7 @@
 
 10000"))
 
+;; strings with whitespace
 (do "    this is    a    string
 
 with     quite
@@ -35,7 +36,7 @@ with     quite
                 some
    whitespace      ")
 
-;; ## Editable code viewer
+;; Editable code viewer
 (clerk/with-viewer
   '(fn [code-str _] [:div.viewer-code [nextjournal.clerk.render.code/editor (reagent/atom code-str)]])
   "(def fib
@@ -60,10 +61,9 @@ with     quite
   (lazy-cat [0 1]
             (map + fib (rest fib))))")
 
-;; ### Synced Editor
 (def editor-sync-viewer
-  (assoc viewer/viewer-eval-viewer
-         :render-fn '(fn [!code _] [:div.bg-neutral-100 [nextjournal.clerk.render.code/editor !code]])))
+  (assoc viewer/viewer-eval-viewer :render-fn '(fn [!code _]
+                                                 [:div.bg-neutral-50 [nextjournal.clerk.render.code/editor !code]])))
 
 ^{::clerk/sync true ::clerk/viewer editor-sync-viewer}
 (defonce editable-code (atom "(def fib
@@ -72,9 +72,7 @@ with     quite
 
 @editable-code
 
-^{::clerk/visibility {:code :hide}}
 (comment
-  @editable-code
   (do
     (reset! editable-code "(def fib
   (lazy-cat [0 1]
