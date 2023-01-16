bda36a8bedc6d6c658679adc3e9a0b4d59739f1b
Martin Kavalar <martin@nextjournal.com>
2023-01-16 19:57:07 +0100

    Fixup graph visualizer and use Clerk instead of arrowic window

--- src/nextjournal/clerk/graph_visualizer.clj
+++ src/nextjournal/clerk/graph_visualizer.clj
@@ -1,21 +1,30 @@
 (ns nextjournal.clerk.graph-visualizer
   {:no-doc true}
   (:require [arrowic.core :as arrowic]
+            [nextjournal.clerk :as clerk]
+            [nextjournal.clerk.analyzer :as analyzer]
             [weavejester.dependency :as dep]))
 
-(defonce viewer
-  (arrowic/create-viewer (arrowic/create-graph)))
+(defn dep-svg [{:keys [graph ->analysis-info]}]
+  (clerk/html
+   (arrowic/as-svg
+    (arrowic/with-graph (arrowic/create-graph)
+      (let [vars->verticies (into {} (map (juxt identity arrowic/insert-vertex!)) (keys ->analysis-info))]
+        (doseq [var (keys ->analysis-info)]
+          (doseq [dep (dep/immediate-dependencies graph var)]
+            (when (and (vars->verticies var)
+                       (vars->verticies dep))
+              (arrowic/insert-edge! (vars->verticies var) (vars->verticies dep))))))))))
 
 
-(defn show-graph [{:keys [graph var->hash]}]
-  (arrowic/view viewer
-                (arrowic/with-graph (arrowic/create-graph)
-                  (let [vars->verticies (into {} (map (juxt identity arrowic/insert-vertex!)) (keys var->hash))]
-                    (doseq [var (keys var->hash)]
-                      (doseq [dep (dep/immediate-dependencies graph var)]
-                        (when (and (vars->verticies var)
-                                   (vars->verticies dep))
-                          (arrowic/insert-edge! (vars->verticies var) (vars->verticies dep)))))))))
+(defn graph-file [file]
+  (-> (analyzer/analyze-file {:graph (dep/graph)} file )
+      analyzer/build-graph
+      dep-svg))
 
-#_(-> "notebooks/elements.clj" nextjournal.clerk.analyzer/build-graph show-graph)
-#_(-> "src/nextjournal/clerk/analyzer.clj" nextjournal.clerk.analyzer/build-graph show-graph)
+^{::clerk/width :full}
+(graph-file "src/nextjournal/clerk/classpath.clj")
+
+
+^{::clerk/width :full}
+(graph-file "src/nextjournal/clerk/parser.cljc")
