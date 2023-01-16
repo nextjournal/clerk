83d4e18d43a46dfbd04aaf8c83a7c249be837a83
Andrea Amantini <lo.zampino@gmail.com>
2023-01-16 10:55:55 +0100

    Fix tests

--- test/nextjournal/clerk/viewer_test.clj
+++ test/nextjournal/clerk/viewer_test.clj
@@ -121,7 +121,7 @@
 (deftest present
   (testing "only transform-fn can select viewer"
     (is (match? {:nextjournal/value [:div.viewer-markdown
-                                     ["h1" {:id "hello-markdown!"} [:span "👋 Hello "] [:em [:span "markdown"]] [:span "!"]]]
+                                     ["h1" {:id "hello-markdown!"} [:<> "👋 Hello "] [:em [:<> "markdown"]] [:<> "!"]]]
                  :nextjournal/viewer {:name :html-}}
                 (v/present (v/with-viewer {:transform-fn (comp v/md v/->value)}
                              "# 👋 Hello _markdown_!")))))
