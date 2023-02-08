ff4a7e574ffecfc6f94eb80192406eadbbc5ce7c
Martin Kavalar <martin@nextjournal.com>
2023-02-08 17:17:25 +0100

    More fixes

--- notebooks/dice.clj
+++ notebooks/dice.clj
@@ -11,11 +11,11 @@
 
 ;; Here, we define a viewer using hiccup that will the dice as well as a button. Note that this button has an `:on-click` event handler that uses `v/clerk-eval` to tell Clerk to evaluate the argument, in this cases `(roll!)` when clicked.
 ^{::clerk/viewer '(fn [side]
-                    (v/html [:div.text-center
-                             (when side
-                               [:div.mt-2 {:style {:font-size "6em"}} side])
-                             [:button.bg-blue-500.hover:bg-blue-700.text-white.font-bold.py-2.px-4.rounded
-                              {:on-click (fn [e] (v/clerk-eval '(roll!)))} "Roll 🎲!"]]))}
+                    [:div.text-center
+                     (when side
+                       [:div.mt-2 {:style {:font-size "6em"}} side])
+                     [:button.bg-blue-500.hover:bg-blue-700.text-white.font-bold.py-2.px-4.rounded
+                      {:on-click (fn [e] (nextjournal.clerk.render/clerk-eval '(roll!)))} "Roll 🎲!"]])}
 @dice
 
 ;; Our roll! function `resets!` our `dice` with a random side and prints and says the result. Finally it updates the notebook.
