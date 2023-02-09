nextjournal/clerk

commit 3988bda0a20a3abd2261c0d7185bbffb8b890877

Make `build-graph` recur until all transitive deps are analyzed (#381)
Until now Clerk did not analyze the full transitive dependency graph which could lead to Clerk not detecting a change properly. Analysis is now recursive which means it's taking a bit longer initially. We cache analysis results per file in memory so subsequent analysis should be fast. We will follow up with visualizing the progress of analysis & execution.

Also discovered cases where classes instead of symbols could end up in the dependency graph and introduced normalization to symbols.

This also gets rid of the `->hash must be ifn?` warning which fixes #375.

Co-authored-by: Martin Kavalar <martin@nextjournal.com>
Co-authored-by: Michiel Borkent <michielborkent@gmail.com>

src/nextjournal/clerk.clj
@@ -484,6 +484,7 @@
   * the form of an anonymous expression"
   ([]
    (swap! webserver/!doc dissoc :blob->result)
+   (reset! analyzer/!file->analysis-cache {})
    (if (fs/exists? config/cache-dir)
      (do (fs/delete-tree config/cache-dir)
          (prn :cache-dir/deleted config/cache-dir))

src/nextjournal/clerk/analyzer.clj
@@ -20,6 +20,8 @@
             [taoensso.nippy :as nippy]
             [weavejester.dependency :as dep]))
 
+(set! *warn-on-reflection* true)
+
 (defn deref? [form]
   (and (seq? form)
        (= (first form) `deref)
@@ -48,13 +50,24 @@
 
 #_(sha1-base58 "hello")
 
+(defn ^:private ensure-symbol [class-or-sym]
+  (cond
+    (symbol? class-or-sym) class-or-sym
+    (class? class-or-sym) (symbol (pr-str class-or-sym))
+    :else (throw (ex-info "not a symbol or a class" {:class-or-sym class-or-sym} (IllegalArgumentException.)))))
+
 (defn class-deps [analyzed]
-  (set/union (into #{} (comp (keep :class)
-                             (filter class?)
-                             (map (comp symbol pr-str))) (ana-ast/nodes analyzed))
-             (into #{} (comp (filter (comp #{:const} :op))
-                             (filter (comp #{:class} :type))
-                             (keep :form)) (ana-ast/nodes analyzed))))
+  (set/union (into #{}
+                   (comp (keep :class)
+                         (filter class?)
+                         (map ensure-symbol))
+                   (ana-ast/nodes analyzed))
+             (into #{}
+                   (comp (filter (comp #{:const} :op))
+                         (filter (comp #{:class} :type))
+                         (keep :form)
+                         (map ensure-symbol))
+                   (ana-ast/nodes analyzed))))
 
 #_(map type (:deps (analyze '(+ 1 2))))
 
@@ -235,6 +248,7 @@
            (analyze-circular-dependency state vars form dep (ex-data e))
            (throw e)))))
 
+
 (defn make-deps-inherit-no-cache [state {:as analyzed :keys [no-cache? vars deps ns-effect?]}]
   (if no-cache?
     state
@@ -322,7 +336,7 @@
                                            doc? (assoc-in [:blocks i :text-without-meta]
                                                           (parser/text-with-clerk-metadata-removed text (ns-resolver notebook-ns)))
                                            (and doc? (not (contains? state :ns))) (merge (parser/->doc-settings form) {:ns *ns*}))]
-                               (if (seq deps)
+                               (if (and (:graph state) (seq deps))
                                  (-> (reduce (partial analyze-deps analyzed) state deps)
                                      (make-deps-inherit-no-cache analyzed))
                                  state)))))
@@ -335,12 +349,26 @@
                   parser/add-css-class
                   filter-code-blocks-without-form))))))
 
+
 #_(let [parsed (nextjournal.clerk.parser/parse-clojure-string "clojure.core/dec")]
     (build-graph (analyze-doc parsed)))
 
+(defonce !file->analysis-cache
+  (atom {}))
+
+#_(reset! !file->analysis-cache {})
+
 (defn analyze-file
-  ([file] (analyze-file {:graph (dep/graph)} file))
-  ([state file] (analyze-doc state (parser/parse-file {} file))))
+  ([file]
+   (let [current-file-sha (sha1-base58 (fs/read-all-bytes file))]
+            (or (when-let [{:as cached-analysis :keys [file-sha]} (@!file->analysis-cache file)]
+                  (when (= file-sha current-file-sha)
+                    cached-analysis))
+                (let [analysis (analyze-doc {:file-sha current-file-sha} (parser/parse-file {} file))]
+                  (swap! !file->analysis-cache assoc file analysis)
+                  analysis))))
+  ([state file]
+   (analyze-doc state (parser/parse-file {} file))))
 
 #_(:graph (analyze-file {:graph (dep/graph)} "notebooks/elements.clj"))
 #_(analyze-file {:graph (dep/graph)} "notebooks/rule_30.clj")
@@ -348,15 +376,16 @@
 #_(analyze-file {:graph (dep/graph)} "notebooks/hello.clj")
 
 (defn unhashed-deps [->analysis-info]
-  (set/difference (into #{}
-                        (mapcat :deps)
-                        (vals ->analysis-info))
-                  (-> ->analysis-info keys set)))
+  (remove deref? (set/difference (into #{}
+                                       (mapcat :deps)
+                                       (vals ->analysis-info))
+                                 (-> ->analysis-info keys set))))
 
 #_(unhashed-deps {#'elements/fix-case {:deps #{#'rewrite-clj.node/tag}}})
 
-(defn ns->path [ns]
-  (str/replace (namespace-munge ns) "." fs/file-separator))
+(defn ns->path
+  ([ns] (ns->path fs/file-separator ns))
+  ([separator ns] (str/replace (namespace-munge ns) "." separator)))
 
 (defn ns->file [ns]
   (some (fn [dir]
@@ -367,11 +396,17 @@
                 [".clj" ".cljc"]))
         (cp/classpath-directories)))
 
+(defn normalize-filename [f]
+  (if (fs/windows?)
+    (-> f fs/normalize fs/unixify)
+    f))
+
 (defn ns->jar [ns]
-  (let [path (ns->path ns)]
-    (some #(when (or (.getJarEntry % (str path ".clj"))
-                     (.getJarEntry % (str path ".cljc")))
-             (.getName %))
+  (let [path (ns->path "/" ns)]
+    (some (fn [^java.util.jar.JarFile jar-file]
+            (when (or (.getJarEntry jar-file (str path ".clj"))
+                      (.getJarEntry jar-file (str path ".cljc")))
+              (normalize-filename (.getName jar-file))))
           (cp/classpath-jarfiles))))
 
 #_(ns->jar (find-ns 'weavejester.dependency))
@@ -383,13 +418,14 @@
   (some-> (if (qualified-symbol? sym)
             (-> sym namespace symbol)
             sym)
-          resolve
+          ^Class resolve
           .getProtectionDomain
           .getCodeSource
           .getLocation
-          (guard #(= "file" (.getProtocol %)))
+          ^java.net.URL (guard #(= "file" (.getProtocol ^java.net.URL %)))
           .getFile
-          (guard #(str/ends-with? % ".jar"))))
+          (guard #(str/ends-with? % ".jar"))
+          normalize-filename))
 
 #_(symbol->jar 'io.methvin.watcher.PathUtils)
 #_(symbol->jar 'io.methvin.watcher.PathUtils/cast)
@@ -404,7 +440,7 @@
             (symbol->jar sym))))
 
 #_(find-location `inc)
-#_(find-location #'inc)
+#_(find-location '@nextjournal.clerk.webserver/!doc)
 #_(find-location `dep/depend)
 #_(find-location 'java.util.UUID)
 #_(find-location 'java.util.UUID/randomUUID)
@@ -416,6 +452,17 @@
   (memoize (fn [f]
              {:jar f :hash (sha1-base58 (io/input-stream f))})))
 
+(defn ^:private merge-analysis-info [state {:as analyzed-doc :keys [->analysis-info]}]
+  (reduce (fn [s {:as analyzed :keys [deps]}]
+            (if (seq deps)
+              (-> (reduce (partial analyze-deps analyzed) s deps)
+                  (make-deps-inherit-no-cache analyzed))
+              s))
+          (update state :->analysis-info merge ->analysis-info)
+          (vals ->analysis-info)))
+
+#_(merge-analysis-info {:->analysis-info {:a :b}} {:->analysis-info {:c :d}})
+
 #_(hash-jar (find-location `dep/depend))
 
 (defn build-graph
@@ -424,15 +471,47 @@
   Recursively decends into dependency vars as well as given they can be found in the classpath.
   "
   [doc]
-  (let [{:as graph :keys [->analysis-info]} (analyze-doc doc)]
-    (reduce (fn [g [source symbols]]
-              (if (or (nil? source)
-                      (str/ends-with? source ".jar"))
-                (update g :->analysis-info merge (into {} (map (juxt identity (constantly (if source (hash-jar source) {})))) symbols))
-                (analyze-file g source)))
-            graph
-            (group-by find-location (unhashed-deps ->analysis-info)))))
+  (loop [{:as state :keys [->analysis-info analyzed-file-set counter]}
 
+         (-> (cond-> doc
+               (not (:graph doc)) analyze-doc)
+             (assoc :analyzed-file-set (cond-> #{} (:file doc) (conj (:file doc))))
+             (assoc :counter 0))]
+    (let [unhashed (unhashed-deps ->analysis-info)
+          loc->syms (apply dissoc
+                           (group-by find-location unhashed)
+                           analyzed-file-set)]
+      (if (and (seq loc->syms) (< counter 10))
+        (recur (-> (reduce (fn [g [source symbols]]
+                             (if (or (nil? source)
+                                     (str/ends-with? source ".jar"))
+                               (update g :->analysis-info merge (into {} (map (juxt identity (constantly (if source (hash-jar source) {})))) symbols))
+                               (-> g
+                                   (update :analyzed-file-set conj source)
+                                   (merge-analysis-info (analyze-file source)))))
+                           state
+                           loc->syms)
+                   (update :counter inc)))
+        (do (when (seq unhashed)
+              (binding [*out* *err*]
+                (println "`build-graph` could not hash all deps:" {:unhashed-deps unhashed})))
+            (dissoc state :analyzed-file-set :counter))))))
+
+
+
+(comment
+  (def parsed (parser/parse-file {:doc? true} "src/nextjournal/clerk/webserver.clj"))
+  (def analysis (time (-> parsed analyze-doc build-graph)))
+  (-> analysis :->analysis-info keys set)
+  (let [{:keys [->analysis-info]} analysis]
+    (dissoc (group-by find-location (unhashed-deps ->analysis-info)) nil))
+  (nextjournal.clerk/clear-cache!))
+
+#_(do (time (build-graph (parser/parse-clojure-string (slurp "notebooks/how_clerk_works.clj")))) :done)
+#_(do (time (build-graph (parser/parse-clojure-string (slurp "notebooks/viewer_api.clj")))) :done)
+
+#_(analyze-file "notebooks/how_clerk_works.clj")
+#_(nextjournal.clerk/show! "notebooks/how_clerk_works.clj")
 
 #_(build-graph (parser/parse-clojure-string (slurp "notebooks/hello.clj")))
 #_(keys (:->analysis-info (build-graph "notebooks/elements.clj")))
@@ -445,12 +524,11 @@
 #_(dep/transitive-dependencies (:graph (build-graph "src/nextjournal/clerk/analyzer.clj"))  #'nextjournal.clerk.analyzer/long-thing)
 
 (defn hash-codeblock [->hash {:as codeblock :keys [hash form id deps vars]}]
-  (let [->hash' (if (and (not (ifn? ->hash)) (seq deps))
-                  (binding [*out* *err*]
-                    (println "->hash must be `ifn?`" {:->hash ->hash :codeblock codeblock})
-                    identity)
-                  ->hash)
-        hashed-deps (into #{} (map ->hash') deps)]
+  (when (and (seq deps) (not (ifn? ->hash)))
+    (throw (ex-info "`->hash` must be `ifn?`" {:->hash ->hash :codeblock codeblock})))
+  (let [hashed-deps (into #{} (map ->hash) deps)]
+    (when (contains? hashed-deps nil)
+      (binding [*out* *err*] (prn :hash-codeblock/unhashed-warning (remove ->hash deps))))
     (sha1-base58 (binding [*print-length* nil]
                    (pr-str (set/union (conj hashed-deps (if form form hash))
                                       vars))))))

test/nextjournal/clerk/analyzer_test.clj
@@ -1,5 +1,6 @@
 (ns nextjournal.clerk.analyzer-test
   (:require [babashka.fs :as fs]
+            [clojure.string :as str]
             [clojure.test :refer [deftest is testing]]
             [matcher-combinators.matchers :as m]
             [matcher-combinators.test :refer [match?]]
@@ -68,7 +69,13 @@
                 (with-ns-binding 'nextjournal.clerk.analyzer-test
                   (intern *ns* 'foo :foo)
                   (intern *ns* 'bar :bar)
-                  (:deps (ana/analyze '{:k-1 foo :k-2 #{bar}})))))))
+                  (:deps (ana/analyze '{:k-1 foo :k-2 #{bar}}))))))
+
+  (testing "deps should all be symbols"
+    (is (every? symbol? (:deps (ana/analyze '(.hashCode clojure.lang.Compiler)))))
+
+    (is (every? symbol? (:deps (ana/analyze '(defprotocol MyProtocol
+                                               (-check [_]))))))))
 
 (deftest read-string-tests
   (testing "read-string should read regex's such that value equalility is preserved"
@@ -132,7 +139,7 @@
               (ana/analyze '(def my-inc inc))))
 
   (ana/analyze '(do (def my-inc inc) (def my-dec dec)))
-  
+
   (is (match? {:ns-effect? false
                :vars '#{nextjournal.clerk.analyzer-test/!state}
                :deps       #{'clojure.lang.Var
@@ -180,9 +187,25 @@
   (testing "does not resolve jdk builtins"
     (is (not (ana/symbol->jar 'java.net.http.HttpClient/newHttpClient)))))
 
+(deftest find-location
+  (testing "clojure.core/inc"
+    (is (re-find #"clojure-1\..*\.jar" (ana/find-location 'clojure.core/inc))))
+
+  (testing "weavejester.dependency/graph"
+    (is (re-find #"dependency-.*\.jar" (ana/find-location 'weavejester.dependency/graph)))))
+
 (defn analyze-string [s]
   (-> (parser/parse-clojure-string {:doc? true} s)
-      ana/analyze-doc))
+      ana/analyze-doc
+      ana/build-graph))
+
+(deftest hash-test
+  (testing "The hash of weavejester/dependency is the same across OSes"
+    (is (match?
+         {:jar
+          #"repository/weavejester/dependency/0.2.1/dependency-0.2.1.jar",
+          :hash "5dsZiMRBpbMfWTafMoHEaNdGfEYxpx"}
+         (ana/hash-jar (ana/find-location 'weavejester.dependency/graph))))))
 
 (deftest analyze-doc
   (testing "reading a bad block shows block and file info in raised exception"
@@ -192,9 +215,11 @@
                        (-> (parser/parse-clojure-string {:doc? true} "(ns some-ns (:require []))")
                            (update-in [:blocks 0 :text] (constantly "##boom"))
                            ana/analyze-doc))))
-  (is (match? #{{:form '(ns example-notebook),
+  (is (match? #{{}
+                {:form '(ns example-notebook),
                  :deps set?}
-                {:form '#{1 3 2}}}
+                {:form '#{1 3 2}}
+                {:jar string? :hash string?}}
               (-> "^:nextjournal.clerk/no-cache (ns example-notebook)
 #{3 1 2}"
                   analyze-string :->analysis-info vals set)))
@@ -203,6 +228,9 @@
     (with-ns-binding 'nextjournal.clerk.analyzer-test
       (is (= (find-ns 'nextjournal.clerk.analyzer-test)
              (do (analyze-string ";; boo\n\n (ns example-notebook)") *ns*)))))
+
+  (testing "has empty analysis info for JDK built-in"
+    (is (= {} (get-in (analyze-string "(do (Thread/sleep 1) 42)") [:->analysis-info 'java.lang.Thread]))))
 
   (testing "defmulti has no deref deps"
     (is (empty? (-> "(defmulti foo :bar)" analyze-string :blocks first :deref-deps))))
@@ -219,7 +247,7 @@
 
 (deftest analyze-file
   (testing "should analyze depedencies"
-    (is (-> (ana/analyze-file "src/nextjournal/clerk/classpath.clj") :graph :dependencies not-empty))))
+    (is (-> (ana/analyze-file "src/nextjournal/clerk/classpath.clj") :->analysis-info not-empty))))
 
 (deftest add-block-ids
   (testing "assigns block ids"
@@ -233,14 +261,11 @@
 
 (deftest no-cache-dep
   (is (match? [{:no-cache? true} {:no-cache? true} {:no-cache? true}]
-              (->> "(def ^:nextjournal.clerk/no-cache my-uuid
+              (let [{:keys [blocks ->analysis-info]} (analyze-string "(def ^:nextjournal.clerk/no-cache my-uuid
   (java.util.UUID/randomUUID))
 (str my-uuid)
-my-uuid"
-                   analyze-string
-                   ana/analyze-doc
-                   :->analysis-info
-                   vals))))
+my-uuid")]
+                (mapv (comp ->analysis-info :id) blocks)))))
 
 (deftest can-analyze-proxy-macro
   (is (analyze-string "(ns proxy-example-notebook) (proxy [clojure.lang.ISeq][] (seq [] '(this is a test seq)))")))
@@ -256,6 +281,32 @@ my-uuid"
 (declare a)
 (def b (str a \" boom\"))
 (def a (str \"boom \" b))"))))
+
+
+(deftest build-graph
+  (testing "should have no unhashed deps for clojure.set"
+    (is (empty? (-> "(ns foo (:require [clojure.set :as set])) (set/union #{1} #{2})" analyze-string :->analysis-info ana/unhashed-deps))))
+
+  (testing "should have analysis info and no unhashed deps for `dep/graph`"
+    (prn :find-location (ana/find-location 'weavejester.dependency/graph))
+    (let [{:keys [->analysis-info]} (analyze-string "(ns foo (:require [weavejester.dependency :as dep])) (dep/graph)")]
+      (is (empty? (ana/unhashed-deps ->analysis-info)))
+      (is (match? {:jar string?} (->analysis-info 'weavejester.dependency/graph))))))
+
+
+(deftest ->hash
+  (testing "notices change in depedency namespace"
+    (let [test-var 'nextjournal.clerk.fixtures.my-test-ns/hello
+          test-string "(ns test (:require [nextjournal.clerk.fixtures.my-test-ns :as my-test-ns])) (str my-test-ns/hello)"
+          spit-with-value #(spit (format "test%s%s.clj" fs/file-separator (str/replace (namespace-munge (namespace test-var)) "." fs/file-separator ))
+                                 (format "(ns nextjournal.clerk.fixtures.my-test-ns) (def hello %s)" %))
+          _ (spit-with-value :hello)
+          analyzed-before (ana/hash (analyze-string test-string))
+          _ (spit-with-value :world)
+          analyzed-after (ana/hash (analyze-string test-string))]
+      (is (not= (get-in analyzed-before [:->hash test-var])
+                (get-in analyzed-after [:->hash test-var]))))))
+
 
 (deftest hash-deref-deps
   (testing "transitive dep gets new hash"

.gitignore
@@ -10,8 +10,9 @@ notebooks/scratch*
 /target/
 .idea/
 clerk.iml
-public/girouette.css
-public/images/
+/public/girouette.css
+/public/images/
+/test/nextjournal/clerk/fixtures/
 *~
 .work
 /build

deps.edn
@@ -3,7 +3,7 @@
         org.clojure/java.classpath {:mvn/version "1.0.0"}
         org.clojure/tools.analyzer {:mvn/version "1.1.0"}
         org.clojure/tools.analyzer.jvm {:mvn/version "1.1.0"}
-        babashka/fs {:mvn/version "0.1.11"}
+        babashka/fs {:mvn/version "0.2.14"}
         borkdude/edamame {:mvn/version "1.0.0"}
         weavejester/dependency {:mvn/version "0.2.1"}
         com.nextjournal/beholder {:mvn/version "1.0.0"}
@@ -50,10 +50,15 @@
 
            :test {:extra-deps {nubank/matcher-combinators {:mvn/version "3.5.1"}
                                org.clojure/test.check {:mvn/version "1.1.1"}
-                               lambdaisland/kaocha {:mvn/version "1.66.1034"}}
+                               lambdaisland/kaocha {:mvn/version "1.66.1034"}
+                               io.github.cognitect-labs/test-runner
+                               {:git/tag "v0.5.1" :git/sha "dfb30dd"}}
                   :extra-paths ["test"]
                   :jvm-opts ["-Dclerk.resource_manifest={\"/js/viewer.js\" \"/js/viewer.js\"}"]
-                  :exec-fn kaocha.runner/exec-fn}
+                  :exec-fn kaocha.runner/exec-fn
+                  ;; Run a single test:
+                  ;; clj -M:test -v nextjournal.clerk.analyzer-test/analyze-doc
+                  :main-opts ["-m" "cognitect.test-runner"]}
 
            :demo {:extra-deps {com.github.seancorfield/next.jdbc {:mvn/version "1.2.659"}
                                org.xerial/sqlite-jdbc {:mvn/version "3.34.0"}

notebooks/viewers/custom_markdown.clj
@@ -20,7 +20,7 @@
    {:name :nextjournal.markdown/ruler
     :transform-fn (constantly
                    (v/html [:div {:style {:width "100%" :height "80px" :background-position "center" :background-size "cover"
-                                          :background-image "url(https://www.maxpixel.net/static/photo/1x/Ornamental-Separator-Decorative-Line-Art-Divider-4715969.png)"}}]))}])
+                                          :background-image "url(https://cdn.pixabay.com/photo/2019/12/24/04/49/divider-4715969_960_720.png)"}}]))}])
 
 (def viewers-with-pretty-markdown
   (v/update-viewers (v/get-default-viewers) {(comp #{:markdown} :name)

src/nextjournal/clerk/graph_visualizer.clj
@@ -1,21 +1,30 @@
 (ns nextjournal.clerk.graph-visualizer
   {:no-doc true}
   (:require [arrowic.core :as arrowic]
+            [nextjournal.clerk :as clerk]
+            [nextjournal.clerk.analyzer :as analyzer]
             [weavejester.dependency :as dep]))
 
-(defonce viewer
-  (arrowic/create-viewer (arrowic/create-graph)))
+(defn dep-svg [{:keys [graph ->analysis-info]}]
+  (clerk/html
+   (arrowic/as-svg
+    (arrowic/with-graph (arrowic/create-graph)
+      (let [vars->verticies (into {} (map (juxt identity arrowic/insert-vertex!)) (keys ->analysis-info))]
+        (doseq [var (keys ->analysis-info)]
+          (doseq [dep (dep/immediate-dependencies graph var)]
+            (when (and (vars->verticies var)
+                       (vars->verticies dep))
+              (arrowic/insert-edge! (vars->verticies var) (vars->verticies dep))))))))))
 
 
-(defn show-graph [{:keys [graph var->hash]}]
-  (arrowic/view viewer
-                (arrowic/with-graph (arrowic/create-graph)
-                  (let [vars->verticies (into {} (map (juxt identity arrowic/insert-vertex!)) (keys var->hash))]
-                    (doseq [var (keys var->hash)]
-                      (doseq [dep (dep/immediate-dependencies graph var)]
-                        (when (and (vars->verticies var)
-                                   (vars->verticies dep))
-                          (arrowic/insert-edge! (vars->verticies var) (vars->verticies dep)))))))))
+(defn graph-file [file]
+  (-> (analyzer/analyze-file {:graph (dep/graph)} file )
+      analyzer/build-graph
+      dep-svg))
 
-#_(-> "notebooks/elements.clj" nextjournal.clerk.analyzer/build-graph show-graph)
-#_(-> "src/nextjournal/clerk/analyzer.clj" nextjournal.clerk.analyzer/build-graph show-graph)
+^{::clerk/width :full}
+(graph-file "src/nextjournal/clerk/classpath.clj")
+
+
+^{::clerk/width :full}
+(graph-file "src/nextjournal/clerk/parser.cljc")

commit 6c710b552f7b3591e34c93ea635c435a7e35231e

Speed up analysis using `loc->sym` cache
Partially fixes performance regressions introduced in 3988bda0a20a3abd2261c0d7185bbffb8b890877.

src/nextjournal/clerk.clj
@@ -492,6 +492,7 @@
   ([]
    (swap! webserver/!doc dissoc :blob->result)
    (reset! analyzer/!file->analysis-cache {})
+   (reset! analyzer/!ns->loc-cache {})
    (if (fs/exists? config/cache-dir)
      (do (fs/delete-tree config/cache-dir)
          (prn :cache-dir/deleted config/cache-dir))

src/nextjournal/clerk/analyzer.clj
@@ -436,13 +436,14 @@
 #_(symbol->jar 'io.methvin.watcher.PathUtils/cast)
 #_(symbol->jar 'java.net.http.HttpClient/newHttpClient)
 
+
 (defn find-location [sym]
-  (cond
-    (deref? sym) (find-location (second sym))
-    :else (if-let [ns (and (qualified-symbol? sym) (-> sym namespace symbol find-ns))]
-            (or (ns->file ns)
-                (ns->jar ns))
-            (symbol->jar sym))))
+  (if (deref? sym)
+    (find-location (second sym))
+    (if-let [ns (and (qualified-symbol? sym) (-> sym namespace symbol find-ns))]
+      (or (ns->file ns)
+          (ns->jar ns))
+      (symbol->jar sym))))
 
 #_(find-location `inc)
 #_(find-location '@nextjournal.clerk.webserver/!doc)
@@ -452,6 +453,25 @@
 #_(find-location 'io.methvin.watcher.PathUtils)
 #_(find-location 'io.methvin.watcher.hashing.FileHasher/DEFAULT_FILE_HASHER)
 #_(find-location 'String)
+
+(defn find-location+cache [!ns->loc sym]
+  (if (deref? sym)
+    (find-location+cache !ns->loc (second sym))
+    (if-let [ns-sym (and (qualified-symbol? sym) (-> sym namespace symbol))]
+      (or (@!ns->loc ns-sym)
+          (when-let [loc (find-location sym)]
+            (swap! !ns->loc assoc ns-sym loc)
+            loc))
+      (find-location sym))))
+
+(def !ns->loc-cache (atom {}))
+
+#_(reset! !ns->loc-cache {})
+
+(defn find-location-cached [sym]
+  (find-location+cache !ns->loc-cache sym))
+
+#_(find-location-cached `inc)
 
 (def hash-jar
   (memoize (fn [f]
@@ -484,7 +504,7 @@
              (assoc :counter 0))]
     (let [unhashed (unhashed-deps ->analysis-info)
           loc->syms (apply dissoc
-                           (group-by find-location unhashed)
+                           (group-by find-location-cached unhashed)
                            analyzed-file-set)]
       (if (and (seq loc->syms) (< counter 10))
         (recur (-> (reduce (fn [g [source symbols]]

test/nextjournal/clerk/analyzer_test.clj
@@ -194,6 +194,16 @@
   (testing "weavejester.dependency/graph"
     (is (re-find #"dependency-.*\.jar" (ana/find-location 'weavejester.dependency/graph)))))
 
+(deftest find-location+cache
+  (let [!ns->loc (atom {})]
+    (testing "populates the cache"
+      (ana/find-location+cache !ns->loc 'clojure.core/inc)
+      (is (= (@!ns->loc 'clojure.core) (ana/find-location 'clojure.core/inc))))
+
+    (testing "doesn't call `find-location` with cache populated"
+      (with-redefs [ana/find-location (fn [_] :bogus/value)]
+        (is (re-find #"clojure-1\..*\.jar" (ana/find-location+cache !ns->loc 'clojure.core/inc)))))))
+
 (defn analyze-string [s]
   (-> (parser/parse-clojure-string {:doc? true} s)
       ana/analyze-doc