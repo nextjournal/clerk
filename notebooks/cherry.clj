2acebbcc5c93f08a096005d8aa810dfc24c72c12
Michiel Borkent <michielborkent@gmail.com>
2023-02-13 12:32:57 +0100

    wip

---
+++ notebooks/cherry.clj
@@ -0,0 +1,11 @@
+;; # Test Cases for Auto-Expanding Data Structure Viewer
+(ns notebooks.cherry
+  #_{:nextjournal.clerk/visibility {:code :hide}
+   :nextjournal.clerk/auto-expand-results? true}
+  (:require [nextjournal.clerk :as clerk]))
+
+;; - [ ] TODO: compile :render-fn using cherry
+
+(clerk/with-viewer {:render-fn '(fn [value]
+                                  [:pre value])}
+  (+ 1 2 3))
