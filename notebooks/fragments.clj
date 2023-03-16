9a25f8825814c4d05e927b9dede3f3fa058ec8df
Andrea Amantini <lo.zampino@gmail.com>
2023-03-16 18:40:36 +0100

    Use fragments in Tap Inspector
    and demo fragments in its own notebook

--- notebooks/fragments.clj
+++ notebooks/fragments.clj
@@ -1,70 +1,10 @@
-;; # 🚰 Tap Inspector via Fragments
-^{:nextjournal.clerk/visibility {:code :hide}}
+;; # 🧩Fragments
 (ns fragments
-  {:nextjournal.clerk/visibility {:code :hide :result :hide}
-   :nextjournal.clerk/no-cache true}
-  (:require [nextjournal.clerk :as clerk]
-            [nextjournal.clerk.viewer :as v])
-  (:import (java.time Instant LocalTime ZoneId)))
+  (:require [nextjournal.clerk :as clerk]))
 
-(defn inst->local-time-str [inst] (str (LocalTime/ofInstant inst (ZoneId/systemDefault))))
-(defn timestamped [x] {:val x :tapped-at (Instant/now) :key (str (gensym))})
+;; With `clerk/fragment` it's possible to embed a sequence of values into the document as if they were results of individual cells.
 
-(def switch-view
-  (assoc v/viewer-eval-viewer
-         :render-fn
-         '(fn [!view]
-            (let [choices [:stream :latest]]
-              [:div.flex.justify-between.items-center
-               (into [:div.flex.items-center.font-sans.text-xs.mb-3 [:span.text-slate-500.mr-2 "View-as:"]]
-                     (map (fn [choice]
-                            [:button.px-3.py-1.font-medium.hover:bg-indigo-50.rounded-full.hover:text-indigo-600.transition
-                             {:class (if (= @!view choice) "bg-indigo-100 text-indigo-600" "text-slate-500")
-                              :on-click #(reset! !view choice)}
-                             choice]) choices))
-               [:button.text-xs.rounded-full.px-3.py-1.border-2.font-sans.hover:bg-slate-100.cursor-pointer
-                {:on-click #(nextjournal.clerk.render/clerk-eval `(reset-taps!))} "Clear"]]))))
-
-^{::clerk/sync true ::clerk/viewer switch-view ::clerk/visibility {:result :show}}
-(defonce !view (atom :stream))
-
-
-(defonce !taps (atom (mapv timestamped
-                           [(clerk/html {::clerk/width :wide} [:h1.w-full.border-2.border-amber-500.bg-amber-500.h-10])
-                            (clerk/plotly {::clerk/width :full} {:data [{:y [3 1 2]}]})])))
-
-(defn reset-taps! []
-  (reset! !taps [])
-  (clerk/recompute!))
-
-(defn tapped [x]
-  (swap! !taps conj (timestamped x))
-  (clerk/recompute!))
-
-(defonce tap-setup (add-tap (fn [x] ((resolve `tapped) x))))
-
-(def tap-separator
-  {:render-fn '(fn [{:keys [tapped-at key]} opts]
-                 (with-meta
-                   [:div.border-t.relative.py-3.mt-2
-                    [:span.absolute.rounded-full.px-2.bg-gray-300.font-mono.top-0
-                     {:class "left-1/2 -translate-x-1/2 -translate-y-1/2 py-[1px] text-[9px]"} tapped-at]]
-                   {:key key}))
-   :transform-fn (comp clerk/mark-presented (clerk/update-val update :tapped-at inst->local-time-str))})
-
-^{::clerk/visibility {:result :show}}
-(clerk/fragment (mapcat (fn [{:as x :keys [val]}]
-                          [(clerk/with-viewer tap-separator
-                             {::clerk/css-class [:w-full :max-w-prose]}
-                             (dissoc x :val))
-                           val])
-                        (cond->> (reverse @!taps) (= :latest @!view) (take 1))))
-
-(comment
-  (doseq [t @@#'clojure.core/tapset] (remove-tap t))
-  (tap> 1)
-  (tap> (clerk/html  {::clerk/width :wide} [:h1.w-full.border-2.border-amber-500.bg-amber-500.h-10]))
-  (tap> (clerk/table {::clerk/width :full} [[1 2] [3 4]]))
-  (tap> (clerk/plotly {::clerk/width :full} {:data [{:y [3 1 2]}]}))
-  (tap> (clerk/html {::clerk/width :full} [:h1 "Fin. 👋"]))
-  (reset-taps!))
+(clerk/fragment
+ (clerk/table  [[1 2] [3 4]])
+ (clerk/image {::clerk/width :full} "https://images.freeimages.com/images/large-previews/773/koldalen-4-1384902.jpg")
+ (clerk/plotly {::clerk/width :full} {:data [{:y [1 3 2]}]}))
