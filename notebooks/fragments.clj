c2456e4240b5ee84ac686d0245acbda1c1dae9e0
Andrea Amantini <lo.zampino@gmail.com>
2023-03-23 11:17:50 +0100

    Improve example notebook

--- notebooks/fragments.clj
+++ notebooks/fragments.clj
@@ -5,12 +5,8 @@
 ;; With `clerk/fragment` we allow to embed a sequence of values into the document as if they were results of individual cells, nesting is allowed.
 
 (clerk/fragment
- 1
- (clerk/table  [[1 2] [3 4]])
- 2
+ (clerk/table [[1 2] [3 4]])
  (clerk/plotly {::clerk/width :full} {:data [{:y [1 3 2]}]})
- (clerk/fragment 3
-                 (clerk/html {::clerk/width :full} [:hr.h-20.bg-amber-200])
-                 (clerk/html {::clerk/width :full} [:hr.h-20.bg-amber-300])
-                 (clerk/html {::clerk/width :full} [:hr.h-20.bg-amber-400])
-                 4))
+ (clerk/html {::clerk/width :full} [:div.h-20.bg-amber-200])
+ (clerk/fragment (clerk/html {::clerk/width :full} [:div.h-20.bg-amber-300])
+                 (clerk/html {::clerk/width :full} [:div.h-20.bg-amber-400])))
