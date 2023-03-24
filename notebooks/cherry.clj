99fd65ee803ed8d5b611bb3635ef86bf9e790a33
Michiel Borkent <michielborkent@gmail.com>
2023-03-24 11:02:10 +0100

    wip

--- notebooks/cherry.clj
+++ notebooks/cherry.clj
@@ -1,12 +1,12 @@
 ;; # Compile viewer functions using cherry
 (ns notebooks.cherry
   #_{:nextjournal.clerk/visibility {:code :hide}
-   :nextjournal.clerk/auto-expand-results? true}
-  (:require [nextjournal.clerk :as clerk]))
+     :nextjournal.clerk/auto-expand-results? true}
+  (:require [nextjournal.clerk :as clerk]
+            [nextjournal.clerk.viewer :as viewer]))
 
 (comment
-  (clerk/clear-cache!)
-  )
+  (clerk/clear-cache!))
 
 (clerk/with-viewer
   {:render-fn
@@ -45,3 +45,15 @@
       [nextjournal.clerk.render/inspect {:a (range 30)}])
    :evaluator :cherry}
   nil)
+
+;; cherry vega viewer!
+
+(def cherry-vega-viewer (assoc viewer/vega-lite-viewer :evaluator :cherry))
+
+(clerk/with-viewer
+  cherry-vega-viewer
+  {:width 700 :height 400 :data {:url "https://vega.github.io/vega-datasets/data/us-10m.json"
+                                 :format {:type "topojson" :feature "counties"}}
+   :transform [{:lookup "id" :from {:data {:url "https://vega.github.io/vega-datasets/data/unemployment.tsv"}
+                                    :key "id" :fields ["rate"]}}]
+   :projection {:type "albersUsa"} :mark "geoshape" :encoding {:color {:field "rate" :type "quantitative"}}})
