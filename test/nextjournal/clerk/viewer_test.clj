9c22fa45654d942b564ed34b2319a64241b2bb83
Andrea Amantini <lo.zampino@gmail.com>
2023-04-11 12:42:04 +0200

    Add a test against custom budget regressions

--- test/nextjournal/clerk/viewer_test.clj
+++ test/nextjournal/clerk/viewer_test.clj
@@ -312,7 +312,15 @@
             (->> (eval/eval-string "1\n(nextjournal.clerk/fragment 2 3 (nextjournal.clerk/fragment 4 5))\n6")
                  view/doc->viewer v/->value :blocks
                  (tree-seq coll? seq)
-                 (filter (every-pred map? :nextjournal/presented :nextjournal/blob-id))))))))
+                 (filter (every-pred map? :nextjournal/presented :nextjournal/blob-id)))))))
+
+  (testing "customizing budget, user-facing"
+    (is (= 5
+           (count
+            (->> (eval/eval-string "^{:nextjournal.clerk/budget 5}(reduce (fn [acc _i] (vector acc)) :fin (range 100 0 -1))")
+                 view/doc->viewer v/->value :blocks
+                 (tree-seq coll? seq)
+                 (filter (every-pred map? (comp #{'nextjournal.clerk.render/render-coll} :form :render-fn)))))))))
 
 (deftest ->edn
   (testing "normal symbols and keywords"
