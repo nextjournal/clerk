bd1373084ddc996b592b800af149e9ed06aa833a
Philippa Markovics <philippa@markovics.com>
2023-04-25 10:56:10 +0200

    Remove unused code example

--- src/nextjournal/clerk/index.clj
+++ src/nextjournal/clerk/index.clj
@@ -44,24 +44,3 @@
 
 (clerk/with-viewer index-viewer @!paths)
 
-#_[:div.bg-gray-100.dark:bg-gray-900.flex.justify-center.overflow-y-auto.w-screen.h-screen.p-4.md:p-0
-   {:ref ref-fn}
-   [:div.fixed.top-2.left-2.md:left-auto.md:right-2.z-10
-    [render/dark-mode-toggle !state]]
-   [:div.md:my-12.w-full.md:max-w-lg
-    [:div.bg-white.dark:bg-gray-800.shadow-lg.rounded-lg.border.dark:border-gray-800.dark:text-white
-     [:div.px-4.md:px-8.py-3
-      [:h1.text-xl "Clerk"]]
-     (into [:ul]
-           (map (fn [path]
-                  [:li.border-t.dark:border-gray-900
-                   [:a.pl-4.md:pl-8.pr-4.py-2.flex.w-full.items-center.justify-between.hover:bg-indigo-50.dark:hover:bg-gray-700
-                    {:href (doc-url view-data path)}
-                    [:span.text-sm.md:text-md.monospace.flex-auto.block.truncate path]
-                    [:svg.h-4.w-4.flex-shrink-0 {:xmlns "http://www.w3.org/2000/svg" :fill "none" :viewBox "0 0 24 24" :stroke "currentColor"}
-                     [:path {:stroke-linecap "round" :stroke-linejoin "round" :stroke-width "2" :d "M9 5l7 7-7 7"}]]]]))
-           (sort paths))]
-    [:div.my-4.md:mb-0.text-xs.text-gray-400.sans-serif.px-4.md:px-8
-     [:a.hover:text-indigo-600.dark:hover:text-white
-      {:href "https://github.com/nextjournal/clerk"}
-      "Generated with Clerk."]]]]
