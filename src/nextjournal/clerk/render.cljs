388f1636e3956120c3835658529f4c0d584797ce
Martin Kavalar <martin@nextjournal.com>
2023-04-28 16:22:18 +0200

    Fix regression where expanding a single path would expand all paths
    Introduced by c4f5fe11f7b4f1141654c7082f04d737f7192f1d.

--- src/nextjournal/clerk/render.cljs
+++ src/nextjournal/clerk/render.cljs
@@ -391,9 +391,9 @@
   (< 1 (count xs)))
 
 (defn inspect-children [opts]
-  (map (fn [x] (cond-> [inspect-presented opts x]
-                 (get-in x [:nextjournal/opts :id])
-                 (with-meta {:key (str (get-in x [:nextjournal/opts :id]) "@" @!eval-counter)})))))
+  (map-indexed (fn [idx x] (cond-> [inspect-presented (update opts :path (fnil conj []) idx) x]
+                             (get-in x [:nextjournal/opts :id])
+                             (with-meta {:key (str (get-in x [:nextjournal/opts :id]) "@" @!eval-counter)})))))
 
 (def expand-style
   ["cursor-pointer"
