478f6771c4200e81234bdb78260a12d2635fb7cb
Philippa Markovics <philippa@markovics.com>
2023-05-05 10:35:42 +0200

    Move filtering and listing to home, reinstate static index listing

--- src/nextjournal/clerk/home.clj
+++ src/nextjournal/clerk/home.clj
@@ -3,7 +3,8 @@
   (:require [clojure.string :as str]
             [babashka.fs :as fs]
             [nextjournal.clerk :as clerk]
-            [nextjournal.clerk.index :as index]))
+            [nextjournal.clerk.index :as index]
+            [nextjournal.clerk.viewer :as v]))
 
 (defn glob-notebooks []
   (let [ignored-paths (into #{} (map fs/path) #{"node_modules" "target"})]
@@ -19,6 +20,16 @@
 
 ^::clerk/sync
 (defonce !filter (atom {}))
+
+(defn filtered+sorted-paths [{:keys [paths query]}]
+  (vec
+   (mapcat
+    (fn [[dir files]]
+      (sort files))
+    (sort-by key
+             (group-by (fn [path]
+                         (str/join fs/file-separator (butlast (fs/path path))))
+                       (filter (partial query-fn query) paths))))))
 
 (defn select-path [move]
   (let [{:as filter :keys [selected-path]} @!filter
@@ -36,6 +47,95 @@
 (defn show-path []
   (when-let [path (:selected-path @!filter)]
     (clerk/show! path)))
+
+(defn query-fn [query path]
+  (str/includes? (str/lower-case path) (str/lower-case (or query ""))))
+
+(def index-viewer
+  {:render-fn '(fn [{:keys [directories query selected-path]} opts]
+                 [:div.not-prose.font-sans
+                  (into [:div]
+                        (map
+                         (fn [[dir files]]
+                           [:div.mb-6
+                            (when (seq dir)
+                              [:div.flex.items-center.gap-2.mb-1
+                               [:svg {:xmlns "http://www.w3.org/2000/svg" :fill "none" :viewBox "0 0 24 24" :stroke-width "1.5" :stroke "currentColor" :class "w-4 h-4"}
+                                [:path {:stroke-linecap "round" :stroke-linejoin "round" :d "M3.75 9.776c.112-.017.227-.026.344-.026h15.812c.117 0 .232.009.344.026m-16.5 0a2.25 2.25 0 00-1.883 2.542l.857 6a2.25 2.25 0 002.227 1.932H19.05a2.25 2.25 0 002.227-1.932l.857-6a2.25 2.25 0 00-1.883-2.542m-16.5 0V6A2.25 2.25 0 016 3.75h3.879a1.5 1.5 0 011.06.44l2.122 2.12a1.5 1.5 0 001.06.44H18A2.25 2.25 0 0120.25 9v.776"}]]
+                               [:span.font-bold dir]])
+                            (into [:ul
+                                   {:class (when (seq dir) "ml-[20px]")}]
+                                  (map (fn [file]
+                                         [:li.mb-1
+                                          [:a.hover:underline.inline-flex.items-center
+                                           {:class (str
+                                                    "px-[7px] py-[2px] "
+                                                    (if (= selected-path file)
+                                                      "bg-blue-700 dark:bg-slate-600 rounded-md text-white"
+                                                      "text-blue-600 dark:text-white"))
+                                            :href (nextjournal.clerk.viewer/doc-url file)}
+                                           file
+                                           (when (= selected-path file)
+                                             [:span.font-inter.opacity-60.relative.ml-1 {:class "top-[2px]"} "↩︎"])]]))
+                                  (sort files))]))
+                        directories)])
+   :transform-fn (comp
+                  clerk/mark-presented
+                  (clerk/update-val (fn [{:as index :keys [paths]}]
+                                      (assoc index :directories (sort-by
+                                                                 key
+                                                                 (group-by (fn [path]
+                                                                             (str/join fs/file-separator (butlast (fs/path path)))) paths))))))})
+
+(def filter-input-viewer
+  (assoc v/viewer-eval-viewer
+         :var-from-def true
+         :render-fn '(fn [!state]
+                       (let [!input-el (nextjournal.clerk.render.hooks/use-ref nil)]
+                         (nextjournal.clerk.render.hooks/use-effect
+                          (fn []
+                            (let [keydown-handler (fn [e]
+                                                    (when @!input-el
+                                                      (let [native-scroll-modifier? (or (.-metaKey e) (.-altKey e) (.-ctrlKey e))]
+                                                        (cond
+                                                          (and (.-metaKey e) (= (.-key e) "j"))
+                                                          (.focus @!input-el)
+                                                          (and (= @!input-el js/document.activeElement) (= (.-key e) "Escape"))
+                                                          (.blur @!input-el)
+                                                          (and (= (.-key e) "ArrowDown") (not native-scroll-modifier?))
+                                                          (do
+                                                            (.preventDefault e)
+                                                            (nextjournal.clerk.render/clerk-eval '(select-path inc)))
+                                                          (and (= (.-key e) "ArrowUp") (not native-scroll-modifier?))
+                                                          (do
+                                                            (.preventDefault e)
+                                                            (nextjournal.clerk.render/clerk-eval '(select-path dec)))
+                                                          (= (.-key e) "Enter")
+                                                          (do
+                                                            (.preventDefault e)
+                                                            (nextjournal.clerk.render/clerk-eval '(show-path)))))))]
+                              (js/document.addEventListener "keydown" keydown-handler)
+                              #(js/document.removeEventListener "keydown" keydown-handler)))
+                          [!input-el])
+                         [:div.relative
+                          [:input.pl-8.py-2.bg-white.dark:bg-slate-800.rounded-lg.font-medium.font-sans.border.dark:border-slate-700.w-full.shadow-inner.dark:focus:outline-none.dark:focus:border-white.dark:text-white.transition-all
+                           {:class "pr-[60px]"
+                            :type :text
+                            :placeholder "Type to filter…"
+                            :value (:query @!state "")
+                            :on-input (fn [e] (swap! !state #(-> %
+                                                                (assoc :query (.. e -target -value))
+                                                                (dissoc :selected-path))))
+                            :ref !input-el}]
+                          [:div.text-slate-400.absolute
+                           {:class "left-[10px] top-[11px]"}
+                           [:svg
+                            {:xmlns "http://www.w3.org/2000/svg" :fill "none" :viewBox "0 0 24 24"
+                             :stroke-width "1.5" :stroke "currentColor" :class "w-[20px] h-[20px]"}
+                            [:path {:stroke-linecap "round" :stroke-linejoin "round" :d "M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"}]]]
+                          [:div.absolute.font-inter.text-slate-400.dark:bg-slate-900.text-sm.font-medium.border.dark:border-slate-700.rounded-md.tracking-wide.px-1.shadow-sm.dark:shadow.pointer-events-none
+                           {:class "right-[10px] top-[9px]"}
+                           "⌘J"]]))))
 
 {::clerk/visibility {:result :show}}
 
@@ -67,13 +167,13 @@
     [:a.ml-3 {:href "#"} "🔍 Viewers"]
     [:a.ml-3 {:href "#"} "🙈 Controlling Visibility"]]]
   [:div.mt-6
-   (clerk/with-viewer index/filter-input-viewer `!filter)]
+   (clerk/with-viewer filter-input-viewer `!filter)]
   [:div.flex.mt-6.border-t.font-sans
    [:div {:class (str "w-1/2 pt-6 " (when-not (seq @!filter) "pr-6 border-r"))}
     [:h4.text-lg "All Notebooks"]
     (let [{:keys [query selected-path]} @!filter]
-      (clerk/with-viewer index/index-viewer {:paths (filter (partial index/query-fn query) @!notebooks)
-                                             :selected-path selected-path}))]
+      (clerk/with-viewer index-viewer {:paths (filter (partial query-fn query) @!notebooks)
+                                       :selected-path selected-path}))]
    (when-not (seq (:query @!filter))
      [:div {:class "w-1/2 pt-6 pl-6"}
       [:h4.text-lg "Static Build Index"]
@@ -81,4 +181,4 @@
         (cond
           error (clerk/md {::clerk/css-class [:m-0]} error)
           paths (let [{:keys [query]} @!filter]
-                  (clerk/with-viewer index/index-viewer {:paths (filter (partial index/query-fn query) paths)}))))])]])
+                  (clerk/with-viewer index-viewer {:paths (filter (partial query-fn query) paths)}))))])]])
--- src/nextjournal/clerk/index.clj
+++ src/nextjournal/clerk/index.clj
@@ -8,140 +8,35 @@
 
 (def !paths (delay (builder/index-paths)))
 
+(def index-item-viewer
+  {:pred string?
+   :transform-fn (clerk/update-val (fn [path]
+                                     (clerk/html
+                                      [:li.border-t.first:border-t-0.dark:border-gray-800.odd:bg-slate-50.dark:odd:bg-white
+                                       {:class "dark:odd:bg-opacity-[0.03]"}
+                                       [:a.pl-4.pr-4.py-2.flex.w-full.items-center.justify-between.hover:bg-indigo-50.dark:hover:bg-gray-700
+                                        {:href (clerk/doc-url path)}
+                                        [:span.text-sm.md:text-md.monospace.flex-auto.block.truncate path]
+                                        [:svg.h-4.w-4.flex-shrink-0 {:xmlns "http://www.w3.org/2000/svg" :fill "none" :viewBox "0 0 24 24" :stroke "currentColor"}
+                                         [:path {:stroke-linecap "round" :stroke-linejoin "round" :stroke-width "2" :d "M9 5l7 7-7 7"}]]]])))})
+
 (def index-viewer
-  {:render-fn '(fn [{:keys [directories query selected-path]} opts]
-                 [:div.not-prose.font-sans
-                  (into [:div]
-                        (map
-                         (fn [[dir files]]
-                           [:div.mb-6
-                            (when (seq dir)
-                              [:div.flex.items-center.gap-2.mb-1
-                               [:svg {:xmlns "http://www.w3.org/2000/svg" :fill "none" :viewBox "0 0 24 24" :stroke-width "1.5" :stroke "currentColor" :class "w-4 h-4"}
-                                [:path {:stroke-linecap "round" :stroke-linejoin "round" :d "M3.75 9.776c.112-.017.227-.026.344-.026h15.812c.117 0 .232.009.344.026m-16.5 0a2.25 2.25 0 00-1.883 2.542l.857 6a2.25 2.25 0 002.227 1.932H19.05a2.25 2.25 0 002.227-1.932l.857-6a2.25 2.25 0 00-1.883-2.542m-16.5 0V6A2.25 2.25 0 016 3.75h3.879a1.5 1.5 0 011.06.44l2.122 2.12a1.5 1.5 0 001.06.44H18A2.25 2.25 0 0120.25 9v.776"}]]
-                               [:span.font-bold dir]])
-                            (into [:ul
-                                   {:class (when (seq dir) "ml-[20px]")}]
-                                  (map (fn [file]
-                                         [:li.mb-1
-                                          [:a.hover:underline.inline-flex.items-center
-                                           {:class (str
-                                                    "px-[7px] py-[2px] "
-                                                    (if (= selected-path file)
-                                                      "bg-blue-700 dark:bg-slate-600 rounded-md text-white"
-                                                      "text-blue-600 dark:text-white"))
-                                            :href (nextjournal.clerk.viewer/doc-url file)}
-                                           file
-                                           (when (= selected-path file)
-                                             [:span.font-inter.opacity-60.relative.ml-1 {:class "top-[2px]"} "↩︎"])]]))
-                                  (sort files))]))
-                        directories)])
-   :transform-fn (comp
-                  clerk/mark-presented
-                  (clerk/update-val (fn [{:as index :keys [paths]}]
-                                      (assoc index :directories (sort-by
-                                                                 key
-                                                                 (group-by (fn [path]
-                                                                             (str/join fs/file-separator (butlast (fs/path path)))) paths))))))})
-
-(def filter-input-viewer
-  (assoc v/viewer-eval-viewer
-         :var-from-def true
-         :render-fn '(fn [!state]
-                       (let [!input-el (nextjournal.clerk.render.hooks/use-ref nil)]
-                         (nextjournal.clerk.render.hooks/use-effect
-                          (fn []
-                            (let [keydown-handler (fn [e]
-                                                    (when @!input-el
-                                                      (let [native-scroll-modifier? (or (.-metaKey e) (.-altKey e) (.-ctrlKey e))]
-                                                        (cond
-                                                          (and (.-metaKey e) (= (.-key e) "j"))
-                                                          (.focus @!input-el)
-                                                          (and (= @!input-el js/document.activeElement) (= (.-key e) "Escape"))
-                                                          (.blur @!input-el)
-                                                          (and (= (.-key e) "ArrowDown") (not native-scroll-modifier?))
-                                                          (do
-                                                            (.preventDefault e)
-                                                            (nextjournal.clerk.render/clerk-eval '(select-path inc)))
-                                                          (and (= (.-key e) "ArrowUp") (not native-scroll-modifier?))
-                                                          (do
-                                                            (.preventDefault e)
-                                                            (nextjournal.clerk.render/clerk-eval '(select-path dec)))
-                                                          (= (.-key e) "Enter")
-                                                          (do
-                                                            (.preventDefault e)
-                                                            (nextjournal.clerk.render/clerk-eval '(show-path)))))))]
-                              (js/document.addEventListener "keydown" keydown-handler)
-                              #(js/document.removeEventListener "keydown" keydown-handler)))
-                          [!input-el])
-                         [:div.relative
-                          [:input.pl-8.py-2.bg-white.dark:bg-slate-800.rounded-lg.font-medium.font-sans.border.dark:border-slate-700.w-full.shadow-inner.dark:focus:outline-none.dark:focus:border-white.dark:text-white.transition-all
-                           {:class "pr-[60px]"
-                            :type :text
-                            :placeholder "Type to filter…"
-                            :value (:query @!state "")
-                            :on-input (fn [e] (swap! !state #(-> %
-                                                                 (assoc :query (.. e -target -value))
-                                                                 (dissoc :selected-path))))
-                            :ref !input-el}]
-                          [:div.text-slate-400.absolute
-                           {:class "left-[10px] top-[11px]"}
-                           [:svg
-                            {:xmlns "http://www.w3.org/2000/svg" :fill "none" :viewBox "0 0 24 24"
-                             :stroke-width "1.5" :stroke "currentColor" :class "w-[20px] h-[20px]"}
-                            [:path {:stroke-linecap "round" :stroke-linejoin "round" :d "M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"}]]]
-                          [:div.absolute.font-inter.text-slate-400.dark:bg-slate-900.text-sm.font-medium.border.dark:border-slate-700.rounded-md.tracking-wide.px-1.shadow-sm.dark:shadow.pointer-events-none
-                           {:class "right-[10px] top-[9px]"}
-                           "⌘J"]]))))
-
-(defn query-fn [query path]
-  (str/includes? (str/lower-case path) (str/lower-case (or query ""))))
+  {:render-fn '(fn [xs opts]
+                 [:div.not-prose
+                  (into [:ul.border.dark:border-slate-800.rounded-md.overflow-hidden]
+                        (nextjournal.clerk.render/inspect-children opts)
+                        xs)])
+   :transform-fn (fn [wrapped-value]
+                   (update wrapped-value :nextjournal/viewers v/add-viewers [index-item-viewer]))})
 
 {::clerk/visibility {:result :show}}
 
-(clerk/html [:h2 {:style {:margin-bottom "-0.5rem"}} (str (last (fs/cwd)))])
-
-^{::clerk/sync true ::clerk/viewer filter-input-viewer}
-(defonce !filter (atom {}))
-
-
-(defn filtered+sorted-paths [{:keys [paths query]}]
-  (vec
-   (mapcat
-    (fn [[dir files]]
-      (sort files))
-    (sort-by key
-             (group-by (fn [path]
-                         (str/join fs/file-separator (butlast (fs/path path))))
-                       (filter (partial query-fn query) paths))))))
-
-(defn select-path [move]
-  (let [{:as filter :keys [selected-path]} @!filter
-        paths (filtered+sorted-paths (merge @!paths filter))
-        index (.indexOf paths selected-path)
-        next-index (move index)]
-    (when (contains? paths next-index)
-      (swap! !filter assoc :selected-path (get paths next-index)))))
-
-(defn show-path []
-  (when-let [path (:selected-path @!filter)]
-    (clerk/show! path)))
-
-
-^{::clerk/visibility {:result :hide} ::clerk/no-cache true}
-(add-watch !filter :empty-selected-path
-           (fn [_ _ old-filter {:as filter :keys [selected-path]}]
-             (when-not (contains? filter :selected-path)
-               (swap! !filter assoc :selected-path (first (filtered+sorted-paths (merge @!paths filter)))))))
+(clerk/html [:h1 {:style {:margin-bottom "-0.45rem"}} (str (last (fs/cwd)))])
 
 (let [{:keys [paths error]} @!paths]
   (cond
     error (clerk/md {::clerk/css-class [:font-sans :max-w-prose :w-full]} error)
-    paths (let [{:keys [query selected-path]} @!filter]
-            (clerk/html
-             [:div.py-1
-              (clerk/with-viewer index-viewer {:paths (filter (partial query-fn query) paths)
-                                               :selected-path selected-path})]))))
+    paths (clerk/with-viewer index-viewer paths)))
 
 (clerk/html
  [:div.text-xs.text-slate-400.font-sans.mb-8.not-prose
