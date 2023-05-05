d9f250bd6263756ec50e38ef5906339d7724ad45
Andrea Amantini <lo.zampino@gmail.com>
2023-05-05 15:20:12 +0200

    Allow customizing the css class of the notebook viewer

--- notebooks/viewer_classes.clj
+++ notebooks/viewer_classes.clj
@@ -1,7 +1,8 @@
-(ns notebooks.viewer-classes
+(ns viewer-classes
   {:nextjournal.clerk/visibility {:code :hide}
-   :nextjournal.clerk/css-class [:justify-center :bg-slate-200 :dark:bg-slate-900 :py-8 :min-h-screen]}
-  (:require [nextjournal.clerk :as clerk]
+   :nextjournal.clerk/doc-css-class [:justify-center :bg-slate-200 :dark:bg-slate-900 :py-8 :min-h-screen]}
+  (:require [babashka.fs :as fs]
+            [nextjournal.clerk :as clerk]
             [clojure.string :as str]))
 
 (clerk/html
@@ -16,8 +17,9 @@
 ^{::clerk/viewer clerk/table
   ::clerk/css-class [:max-w-2xl :mx-auto :bg-white :p-4 :rounded-lg :shadow-lg :mt-4]}
 (def dataset
-  (->> (slurp "/usr/share/dict/words")
+  (->> (slurp (if (fs/exists? "/usr/share/dict/words")
+                "/usr/share/dict/words"
+                "https://gist.githubusercontent.com/wchargin/8927565/raw/d9783627c731268fb2935a731a618aa8e95cf465/words"))
        str/split-lines
        (group-by (comp keyword str/upper-case str first))
        (into (sorted-map))))
-
