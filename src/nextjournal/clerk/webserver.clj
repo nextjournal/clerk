086dbfca46ba57acdede9e1992f63fe1ca337c41
Martin Kavalar <martin@nextjournal.com>
2023-05-31 14:53:53 +0200

    Check that extension matches #"\.(cljc?|md)$" before attempting to show

--- src/nextjournal/clerk/webserver.clj
+++ src/nextjournal/clerk/webserver.clj
@@ -192,7 +192,7 @@
 
 (defn ->file-or-ns [nav-path]
   (cond (str/starts-with? nav-path "'") (symbol (subs nav-path 1))
-        :else nav-path))
+        (re-find #"\.(cljc?|md)$" nav-path) nav-path))
 
 (defn show! [opts file-or-ns]
   ((resolve 'nextjournal.clerk/show!) opts file-or-ns))
@@ -213,14 +213,17 @@
        :headers {"Location" (or (:nav-path @!doc)
                                 (->nav-path 'nextjournal.clerk.home))}}
       :else
-      (do
-        (try (show! {:skip-history? true} (->file-or-ns nav-path))
-             (catch Exception _))
-        {:status 200
-         :headers {"Content-Type" "text/html" "Cache-Control" "no-store"}
-         :body (view/->html {:doc (view/doc->viewer @!doc)
-                             :resource->url @config/!resource->url
-                             :conn-ws? true})}))))
+      (if-let [file-or-ns (->file-or-ns nav-path)]
+        (do (try (show! {:skip-history? true} file-or-ns)
+                 (catch Exception _))
+            {:status 200
+             :headers {"Content-Type" "text/html" "Cache-Control" "no-store"}
+             :body (view/->html {:doc (view/doc->viewer @!doc)
+                                 :resource->url @config/!resource->url
+                                 :conn-ws? true})})
+        {:status 404
+         :headers {"Content-Type" "text/plain"}
+         :body (format "Could not find notebook at %s." (pr-str nav-path))}))))
 
 (defn app [{:as req :keys [uri]}]
   (if (:websocket? req)
