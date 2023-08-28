de50d1ec03c5631fc68b62e0f7852bca26e431f2
Philippa Markovics <philippa@markovics.com>
2023-08-28 16:31:07 +0200

    Sidebar nav experiments

--- notebooks/doc.clj
+++ notebooks/doc.clj
@@ -15,13 +15,17 @@
                :value @!query
                :class "px-2 py-1 relative bg-white bg-white rounded border border-slate-200 shadow-inner outline-none focus:outline-none focus:ring w-full text-sm font-sans"
                :on-input #(reset! !query (.. % -target -value))}]
-      [:button.absolute.right-2.text-xl.cursor-pointer
-       {:class "top-1/2 -translate-y-1/2"
-        :on-click #(reset! !query (clojure.string/join "." (drop-last (clojure.string/split @!query #"\."))))} "⏮"]]))
+      #_[:button.absolute.right-2.text-xl.cursor-pointer
+         {:class "top-1/2 -translate-y-1/2"
+          :on-click #(reset! !query (clojure.string/join "." (drop-last (clojure.string/split @!query #"\."))))} "⏮"]]))
 
 ^{::clerk/sync true}
-(defonce !ns-query (atom "nextjournal.clerk"))
-#_(reset! !ns-query "nextjournal.clerk")
+(defonce !ns-query (atom ""))
+#_(reset! !ns-query "")
+
+^{::clerk/sync true}
+(defonce !active-ns (atom "nextjournal.clerk.viewer"))
+#_(reset! !active-ns "nextjournal.clerk.viewer")
 
 (defn escape-pattern-str [s]
   (let [esc-chars "[](){}<>*&^%$#!\\|? "]
@@ -30,8 +34,11 @@
          (reduce str)
          str)))
 
+(defn match-nss [s]
+  (filter (partial re-find (re-pattern (escape-pattern-str s))) (sort (map str (all-ns)))))
+
 (def ns-matches
-  (filter (partial re-find (re-pattern (escape-pattern-str @!ns-query))) (sort (map str (all-ns)))))
+  (match-nss @!ns-query))
 
 (defn var->doc-viewer
   "Takes a clojure `var` and returns a Clerk viewer to display its documentation."
@@ -47,21 +54,20 @@
         [:div.mt-4.viewer-markdown.prose
          (clerk/md doc)])])))
 
-(defn render-ns [{:keys [name nss vars]}]
+(defn render-ns [{:keys [name nss]}]
   [:div.mt-1
-   [:div name]
-   (when vars
-     (into [:div.ml-3] (map (fn [var]
-                              [:div.mt-1.text-xs.italic var])) vars))
+   [:div.hover:underline.cursor-pointer.hover:text-indigo-600
+    {:class (when (= @!active-ns name) "font-bold")
+     :on-click (viewer/->viewer-eval `(fn []
+                                        (reset! !active-ns ~name)
+                                        (reset! !ns-query "")))} name]
    (when nss
      (into [:div.ml-3] (map render-ns) nss))])
 
 (defn ns-node-with-branches [nss-map ns-name]
-  (let [sub-nss (get nss-map ns-name)
-        vars (some-> ns-name symbol find-ns ns-publics not-empty vals vec)]
+  (let [sub-nss (get nss-map ns-name)]
     (cond-> {:name ns-name}
-      sub-nss (assoc :nss (mapv (partial ns-node-with-branches nss-map) sub-nss))
-      vars (assoc :vars vars))))
+      sub-nss (assoc :nss (mapv (partial ns-node-with-branches nss-map) sub-nss)))))
 
 (defn ns-tree
   ([ns-matches]
@@ -80,31 +86,82 @@
 #_(ns-tree ns-matches)
 #_(ns-tree ())
 
+(defn parent-ns [ns-str]
+  (when (str/includes? ns-str ".")
+    (str/join "." (butlast (str/split ns-str #"\.")))))
+
+(defn prepend-parent [nss]
+  (when-let [parent (parent-ns (first nss))]
+    (cons parent nss)))
+
+(defn path-to-ns [ns-str]
+  (last (take-while some? (iterate prepend-parent [ns-str]))))
+
 ^{::clerk/visibility {:result :show}}
 (clerk/html
- (let [ns-str (first ns-matches)]
-   [:<>
-    [:style ".markdown-viewer { padding: 0 !important; }"]
-    [:div.w-screen.h-screen.flex.fixed.left-0.top-0.bg-white.dark:bg-slate-950
-     [:div.border-r.py-3.flex-shrink-0.overflow-y-auto {:class "w-[300px]"}
-      [:div.px-3
-       (clerk/with-viewer {:render-fn render-input
-                           :transform-fn clerk/mark-presented} (viewer/->viewer-eval `!ns-query))]
-      (when ns-str
-        (into [:div.text-sm.font-sans.px-5.mt-3]
-              (map render-ns)
-              (ns-tree ns-matches)))]
-     [:div.flex-auto.max-h-screen.overflow-y-auto.px-8.py-5
-      (if ns-str
-        (let [ns (find-ns (symbol ns-str))]
-          [:<>
-           [:div.font-bold.font-sans.text-xl {:style {:margin 0}} (ns-name ns)]
-           (when-let [doc (-> ns meta :doc)]
-             [:div.mt-4.leading-normal.viewer-markdown.prose
-              (clerk/md doc)])
-           (into [:<>]
-                 (map (comp :nextjournal/value var->doc-viewer val))
-                 (into (sorted-map) (-> ns ns-publics)))])
-        [:div "No namespaces found."])]]]))
+ [:<>
+  [:style ".markdown-viewer { padding: 0 !important; }"]
+  [:div.w-screen.h-screen.flex.fixed.left-0.top-0.bg-white.dark:bg-slate-950
+   [:div.border-r.py-3.flex-shrink-0.overflow-y-auto {:class "w-[300px]"}
+    [:div.px-3
+     (clerk/with-viewer {:render-fn render-input
+                         :transform-fn clerk/mark-presented} (viewer/->viewer-eval `!ns-query))]
+    (cond (not (str/blank? @!ns-query))
+          [:div
+           [:div.tracking-wider.uppercase.text-slate-400.px-5.font-sans.text-xs.mt-5 "Search results"]
+           (into [:div.text-sm.font-sans.px-5.mt-3]
+                 (map render-ns)
+                 (ns-tree ns-matches))]
+          (= @!active-ns :all)
+          [:div
+           [:div.tracking-wider.uppercase.text-slate-400.px-5.font-sans.text-xs.mt-5 "All namespaces"]
+           (into [:div.text-sm.font-sans.px-5.mt-2]
+                 (map render-ns)
+                 (ns-tree (sort (map (comp str ns-name) (all-ns)))))]
+          :else
+          (let [path (path-to-ns @!active-ns)]
+            [:<>
+             [:div
+              [:div.text-slate-500.px-5.font-sans.text-xs.mt-3
+               {:on-click (viewer/->viewer-eval `(fn []
+                                                   (reset! !active-ns :all)
+                                                   (reset! !ns-query "")))}
+               "Show all namespaces"]
+              (into [:div.text-sm.font-sans.px-5.mt-2]
+                    (map render-ns)
+                    (ns-tree path))]
+             (when-let [vars (some-> @!active-ns symbol find-ns ns-publics not-empty vals vec)]
+               (into [:div.text-xs.font-sans.px-5.mt-1]
+                     (map (fn [var] [:div.mt-1 {:class (str "ml-" (* (count path) 3))} var]))
+                     vars))]))]
+   [:div.flex-auto.max-h-screen.overflow-y-auto.px-8.py-5
+    (let [ns (some-> @!active-ns symbol find-ns)]
+      (cond
+        ns [:<>
+            [:div.font-bold.font-sans.text-xl {:style {:margin 0}} (ns-name ns)]
+            (when-let [doc (-> ns meta :doc)]
+              [:div.mt-4.leading-normal.viewer-markdown.prose
+               (clerk/md doc)])
+            (into [:<>]
+                  (map (comp :nextjournal/value var->doc-viewer val))
+                  (into (sorted-map) (-> ns ns-publics)))]
+        @!active-ns [:<>
+                     [:div.font-bold.font-sans.text-xl {:style {:margin 0}} (if (= @!active-ns :all)
+                                                                              "All namespaces in classpath"
+                                                                              @!active-ns)]
+                     (into [:div]
+                           (map (fn [ns-str]
+                                  [:div.pt-3.mt-3
+                                   [:div.font-sans.text-base
+                                    {:style {:margin 0}
+                                     :on-click (viewer/->viewer-eval `(fn []
+                                                                        (reset! !active-ns ~ns-str)
+                                                                        (reset! !ns-query "")))}
+                                    ns-str]]))
+                           (if (= :all @!active-ns)
+                             (sort (map :name (ns-tree (map (comp str ns-name) (all-ns)))))
+                             (match-nss @!active-ns)))]
+        :else [:div "No namespaces found."]))]]])
 
 #_(deref nextjournal.clerk.webserver/!doc)
+
