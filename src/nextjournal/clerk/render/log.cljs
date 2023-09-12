a8d5ad085b3e133bfe008b1d1d15cfd0922e1b2f
Philippa Markovics <philippa@markovics.com>
2023-09-12 15:48:14 +0200

    Split views, render inspect

--- src/nextjournal/clerk/render/log.cljs
+++ src/nextjournal/clerk/render/log.cljs
@@ -12,10 +12,32 @@
   (localstorage/set-item! "log-visible" true))
 
 (defn log [event-name & pairs]
-  (js/console.log "log" event-name pairs)
   (swap! !history conj {:event-name event-name
                         :timestamp (js/Date.)
                         :pairs pairs}))
+
+(defn inspect-fn []
+  @(resolve 'nextjournal.clerk.render/inspect))
+
+(defn log-line [{:keys [event-name timestamp pairs]}]
+  [:div.flex.gap-3.p-1.border-b {:class "min-h-[32px]"}
+   [:div
+    [:div.text-white.rounded-sm.font-mono.flex-shrink-0.whitespace-nowrap.leading-none
+     {:class "text-[10px] px-[5px] py-[3px] bg-blue-600 mt-[3px]"}
+     event-name]]
+   (into [:div.flex-auto.text-xs.font-mono]
+         (map (fn [[k v]]
+                [:div.flex.gap-3
+                 [:div.whitespace-nowrap [(inspect-fn) k]]
+                 [:div [(inspect-fn) v]]]))
+         (partition 2 pairs))
+   [:div
+    [:div.font-mono.flex-shrink-0.whitespace-nowrap.pr-1
+     {:class "text-[10px] mt-[4px]"}
+     (.toLocaleTimeString timestamp "en-US")]]])
+
+(defn sci-repl []
+  [:textarea.w-full.h-full.appearance-none.font-mono.text-xs.rounded-md.border.border-slate-300.p-2])
 
 (defn panel []
   [:div.fixed.left-0.right-0.bottom-0.z-30.border-t.border-slate-300.flex.flex-col.bg-white
@@ -29,22 +51,7 @@
       [:path {:stroke-linecap "round", :stroke-linejoin "round", :d "M6 18L18 6M6 6l12 12"}]]]]
    [:div.bg-white.flex-auto.overflow-y-auto
     (into [:div]
-          (map (fn [{:keys [event-name timestamp pairs]}]
-                 [:div.flex.gap-3.p-1.border-b
-                  [:div
-                   [:div.text-white.rounded-sm.font-mono.flex-shrink-0.whitespace-nowrap.leading-none
-                    {:class "text-[10px] px-[5px] py-[3px] bg-blue-600"}
-                    event-name]]
-                  (into [:div.flex-auto.text-xs.font-mono]
-                        (map (fn [[a b]]
-                               [:div.flex.gap-3
-                                [:div.whitespace-nowrap a]
-                                [:div b]]))
-                        (partition 2 pairs))
-                  [:div
-                   [:div.font-mono.flex-shrink-0.whitespace-nowrap.pr-1
-                    {:class "text-[10px]"}
-                    (.toLocaleTimeString timestamp "en-US")]]]))
+          (map (fn [log] [log-line log]))
           @!history)]
    [:div.bg-slate-100.p-1.border-t.border-slate-300.flex.items-center
-    [:textarea.w-full.h-full.appearance-none.font-mono.text-xs.rounded-md.border.border-slate-300.p-2]]])
+    [sci-repl]]])
