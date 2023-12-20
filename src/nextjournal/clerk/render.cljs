nextjournal/clerk

commit f8377d6116ccecee00ecf1dcc2fa843555cac9d8

Static Build: new `:package` option (#592)
for controlling `clerk/build!` artefacts and deprecate `:bundle?` option. Clerk static apps now use client-side navigation by default for a smoother cross-document transition.

src/nextjournal/clerk/render.cljs
@@ -89,7 +89,7 @@
      status]]
    (when cell-progress
      [:div.w-full.bg-sky-100.dark:bg-purple-900.rounded.z-20 {:class "h-[2px] mt-[0.5px]"}
-      [:div.bg-sky-500.dark:bg-purple-400 {:class "h-[2px]" :style {:width (str (* cell-progress 100) "%")}}]])])5
+      [:div.bg-sky-500.dark:bg-purple-400 {:class "h-[2px]" :style {:width (str (* cell-progress 100) "%")}}]])])
 
 (defn connection-status [status]
   [:div.absolute.text-red-600.dark:text-white.text-xs.font-sans.ml-1.bg-white.dark:bg-red-800.rounded-full.shadow.z-30.font-bold.px-2.border.border-red-400
@@ -107,12 +107,12 @@
 
 (declare clerk-eval)
 
-(defn render-notebook [{:as doc xs :blocks :keys [bundle? doc-css-class sidenotes? toc toc-visibility header footer]}
+(defn render-notebook [{:as doc xs :blocks :keys [package doc-css-class sidenotes? toc toc-visibility header footer]}
                        {:as render-opts :keys [!expanded-at expandable-toc?]}]
   (r/with-let [root-ref-fn (fn [el]
                              (when (and el (exists? js/document))
                                (code/setup-dark-mode!)
-                               (when-some [heading (when (and (exists? js/location) (not bundle?))
+                               (when-some [heading (when (and (exists? js/location) (= :directory package))
                                                      (try (some-> js/location .-hash not-empty js/decodeURI (subs 1) js/document.getElementById)
                                                           (catch js/Error _
                                                             (js/console.warn (str "Clerk render-notebook, invalid hash: "
@@ -124,7 +124,7 @@
      [:div.fixed.top-2.left-2.md:left-auto.md:right-2.z-10
       [dark-mode-toggle]]
      (when (and toc toc-visibility)
-       [navbar/view toc (assoc render-opts :set-hash? (not bundle?) :toc-visibility toc-visibility)])
+       [navbar/view toc (assoc render-opts :set-hash? (= :directory package) :toc-visibility toc-visibility)])
      [:div.flex-auto.w-screen.scroll-container
       (into
        [:> (.-div motion)
@@ -555,7 +555,6 @@
 
 (defn root []
   [:<>
-   [inspect-presented @!doc]
    [:div.fixed.w-full.z-20.top-0.left-0.w-full
     (when-let [status (:nextjournal.clerk.sci-env/connection-status @!doc)]
       [connection-status status])
@@ -564,6 +563,8 @@
    (when-let [error (get-in @!doc [:nextjournal/value :error])]
      [:div.fixed.top-0.left-0.w-full.h-full
       [inspect-presented error]])
+   (when (:nextjournal/value @!doc)
+     [inspect-presented @!doc])
    (into [:<>]
          (map (fn [[id state]]
                 ^{:key id}
@@ -698,9 +699,6 @@
 (defn path-from-url-hash [url]
   (-> url ->URL .-hash (subs 2)))
 
-(defn static-app? [state]
-  (contains? state :path->doc))
-
 (defn ->doc-url [url]
   (let [path (js/decodeURI (.-pathname url))
         doc-path (js/decodeURI (.-pathname (.-location js/document)))]
@@ -745,25 +743,66 @@
 (defn handle-initial-load [^js _e]
   (history-push-state {:path (subs js/location.pathname 1) :replace? true}))
 
-(defn click->xhr-request [e]
-  (when-some [url (some-> e .-target closest-anchor-parent .-href ->URL)]
+(defn utf8-decode [bytes]
+  (.decode (js/TextDecoder. "utf-8") bytes))
+
+(defn delay-resolve [v] (new js/Promise (fn [res] (js/setTimeout #(res v) 100))))
+
+(defn read-response+show-progress [{:as state :keys [reader buffer content-length]}]
+  (swap! !doc assoc :status {:progress (if (zero? (count buffer)) 0.2 (/ (count buffer) content-length))})
+  (.. reader read
+      ;; delay a bit for progress bar to be visible
+      (then delay-resolve)
+      (then (fn [ret]
+              (if (.-done ret)
+                buffer
+                (read-response+show-progress (update state :buffer str (utf8-decode (.-value ret)))))))))
+
+(defn fetch+set-state [edn-path]
+  (.. ^js (js/fetch edn-path)
+      (then (fn handle-response [r]
+              (if (.-ok r)
+                {:buffer ""
+                 :reader (.. r -body getReader)
+                 :content-length (js/Number. (.. r -headers (get "content-length")))}
+                (throw (ex-info (.-statusText r) {:url (.-url r)
+                                                  :status (.-status r)
+                                                  :headers (.-headers r)})))))
+      (then read-response+show-progress)
+      (then (fn [edn] (set-state! {:doc (read-string edn)}) {:ok true}))
+      (catch (fn [e] (js/console.error "Fetch failed" e)
+               (set-state! {:doc {:nextjournal/viewer {:render-fn (constantly [:<>])} ;; FIXME: make :error top level on state
+                                  :nextjournal/value {:error (viewer/present e)}}})
+               {:ok false :error e}))))
+
+(defn click->fetch [e]
+  (when-some [url (some-> ^js e .-target closest-anchor-parent .-href ->URL)]
     (when-not (ignore-anchor-click? e url)
       (.preventDefault e)
       (let [path (.-pathname url)
             edn-path (str path (when (str/ends-with? path "/") "index") ".edn")]
-        (js/console.log "fetch EDN" edn-path )
-        (-> (js/fetch edn-path)
-            (.then (fn [r]
-                     (if (.-ok r)
-                       (.text r)
-                       (throw (ex-info "Not Found" {:response r})))))
-            (.then (fn [edn]
-                     (set-state! {:doc (read-string edn)})
-                     (.pushState js/history #js {} ""
-                                 (cond-> path
-                                   (not (str/ends-with? path "/"))
-                                   (str "/"))))) ;; trailing slash is needed to make relative paths work
-            (.catch (fn [e] (js/console.error "Fetch failed" e ))))))))
+        (.pushState js/history #js {:edn_path edn-path} ""
+                    (cond-> path
+                      (not (str/ends-with? path "/"))
+                      (str "/"))) ;; a trailing slash is needed to make relative paths work
+        (fetch+set-state edn-path)))))
+
+(defn load->fetch [{:keys [current-path]} _e]
+  ;; TODO: consider fixing this discrepancy via writing EDN one step deeper in directory
+  (let [edn-path (-> (.-pathname js/document.location)
+                     (str/replace #"/(index.html)?$" "")
+                     (cond->
+                       (empty? current-path)
+                       (str "/index.edn")
+                       (seq current-path)
+                       (str ".edn")))]
+    (.pushState js/history #js {:edn_path edn-path} "" nil)
+    (fetch+set-state edn-path)))
+
+(defn popstate->fetch [^js e]
+  (when-some [edn-path (.. e -state -edn_path)]
+    (.preventDefault e)
+    (fetch+set-state edn-path)))
 
 (defn setup-router! [{:as state :keys [render-router]}]
   (when (and (exists? js/document) (exists? js/window))
@@ -776,7 +815,9 @@
                        :bundle
                        [(gevents/listen js/window gevents/EventType.HASHCHANGE (partial handle-hashchange state) false)]
                        :fetch-edn
-                       [(gevents/listen js/document gevents/EventType.CLICK click->xhr-request false)]
+                       [(gevents/listen js/document gevents/EventType.CLICK click->fetch false)
+                        (gevents/listen js/window gevents/EventType.POPSTATE popstate->fetch false)
+                        (gevents/listen js/window gevents/EventType.LOAD (partial load->fetch state) false)]
                        :serve
                        [(gevents/listen js/document gevents/EventType.CLICK handle-anchor-click false)
                         (gevents/listen js/window gevents/EventType.POPSTATE handle-history-popstate false)
@@ -791,14 +832,12 @@
   (swap! !doc re-eval-viewer-fns)
   (mount))
 
-(defn ^:export init [{:as state :keys [bundle? path->doc current-path]}]
+(defn ^:export init [{:as state :keys [render-router path->doc]}]
   (setup-router! state)
-  (set-state! (if (static-app? state)
-                {:doc (get path->doc (or (if bundle?
-                                           (path-from-url-hash (->URL (.-href js/location)))
-                                           current-path)
-                                         ""))}
-                state))
+  (when (contains? #{:bundle :serve} render-router)
+    (set-state! (case render-router
+                  :bundle {:doc (get path->doc (or (path-from-url-hash (->URL (.-href js/location))) ""))}
+                  :serve state)))
   (mount))
 
 

.github/workflows/main.yml
@@ -148,7 +148,7 @@ jobs:
           clojure -J-Dclojure.main.report=stderr -X:demo:nextjournal/clerk :git/sha '"${{ github.sha }}"' :git/url '"https://github.com/nextjournal/clerk"' :paths '["book.clj" "CHANGELOG.md" "editor.clj"]'
 
       - name: 🏗 Build Clerk Static App with default Notebooks
-        run: clojure -J-Dclojure.main.report=stderr -X:demo:nextjournal/clerk :git/sha '"${{ github.sha }}"' :git/url '"https://github.com/nextjournal/clerk"' :bundle true
+        run: clojure -J-Dclojure.main.report=stderr -X:demo:nextjournal/clerk :git/sha '"${{ github.sha }}"' :git/url '"https://github.com/nextjournal/clerk"' :package :single-file
 
       - name: 📠 Copy book & static build to bucket under SHA
         run: |

notebooks/document_linking.clj
@@ -1,6 +1,7 @@
 ;; # 🖇️ Document Linking
 (ns document-linking
-  {:nextjournal.clerk/toc true}
+  {:nextjournal.clerk/no-cache true
+   :nextjournal.clerk/toc true}
   (:require [nextjournal.clerk :as clerk]))
 
 ;; ## `clerk/doc-url` helper

src/nextjournal/clerk.clj
@@ -415,9 +415,16 @@
     (beholder/stop watcher)
     (reset! !watcher nil)))
 
+(defn deprecate+migrate-opts [{:as opts :keys [bundle?]}]
+  (when (contains? opts :bundle?)
+    (println "\nThe `:bundle(?)` option is deprecated and will be dropped in 1.0.0. Use `:package :single-file` instead.\n"))
+  (-> opts (dissoc :bundle?)
+      (cond-> bundle?
+        (assoc :package :single-page))))
+
 (defn ^:private normalize-opts [opts]
-  (set/rename-keys opts #_(into {} (map (juxt identity #(keyword (str (name %) "?")))) [:bundle :browse :dashboard])
-                   {:bundle :bundle?, :browse :browse?, :dashboard :dashboard? :compile-css :compile-css? :ssr :ssr? :exclude-js :exclude-js?}))
+  (deprecate+migrate-opts
+   (set/rename-keys opts {:bundle :bundle?, :browse :browse?, :dashboard :dashboard? :compile-css :compile-css? :ssr :ssr? :exclude-js :exclude-js?})))
 
 (defn ^:private started-via-bb-cli? [opts]
   (contains? (meta opts) :org.babashka/cli))
@@ -437,7 +444,7 @@
 
   When both `:paths` and `:paths-fn` are given, `:paths` takes precendence.
 
-  Can be called multiple times and Clerk will happily serve you according to the latest config."  
+  Can be called multiple times and Clerk will happily serve you according to the latest config."
   {:org.babashka/cli {:spec {:watch-paths {:desc "Paths on which to watch for changes and show a changed document."
                                            :coerce []}
                              :paths {:desc "Restricts serving to the given paths, supports glob patterns. Will disable Clerk's homepage when set."
@@ -492,25 +499,32 @@
   "Creates a static html build from a collection of notebooks.
 
   Options:
-  - `:paths`     - a vector of relative paths to notebooks to include in the build
-  - `:paths-fn`  - a symbol resolving to a 0-arity function returning computed paths
-  - `:index`     - a string allowing to override the name of the index file, will be added to `:paths`
+  - `:paths`     a vector of relative paths to notebooks to include in the build
+  - `:paths-fn`  a symbol resolving to a 0-arity function returning computed paths
+  - `:index`     a string allowing to override the name of the index file, will be added to `:paths`
 
   Passing at least one of the above is required. When both `:paths` and `:paths-fn` are given, `:paths` takes precendence.
 
-  - `:bundle`      - if true results in a single self-contained html file including inlined images
-  - `:compile-css` - if true compiles css file containing only the used classes
-  - `:ssr`         - if true runs react server-side-rendering and includes the generated markup in the html
-  - `:browse`      - if true will open browser with the built file on success
-  - `:dashboard`   - if true will start a server and show a rich build report in the browser (use with `:bundle` to open browser)
-  - `:out-path`    - a relative path to a folder to contain the static pages (defaults to `\"public/build\"`)
-  - `:git/sha`, `:git/url` - when both present, each page displays a link to `(str url \"blob\" sha path-to-notebook)`
+  - `:package`     a keyword to specify how the static build should be bundled:
+    - `:directory` (default) constructs a distinct html file for each document in `:paths`
+    - `:single-file` bundles all documents into a single html file
+  - `:bundle`      [DEPRECATED use :pacakge :single-page instead] if true results in a single self-contained html file including inlined images
+  - `:compile-css` if true compiles css file containing only the used classes
+  - `:ssr`         if true runs react server-side-rendering and includes the generated markup in the html
+  - `:browse`      if true will open browser with the built file on success
+  - `:dashboard`   if true will start a server and show a rich build report in the browser (use with `:bundle` to open browser)
+  - `:out-path`    a relative path to a folder to contain the static pages (defaults to `\"public/build\"`)
+  - `:git/sha`, `:git/url` when both present, each page displays a link to `(str url \"blob\" sha path-to-notebook)`
   "
   {:org.babashka/cli {:spec {:paths {:desc "Paths to notebooks toc include in the build, supports glob patterns."
                                      :coerce []}
                              :paths-fn {:desc "Symbol resolving to a 0-arity function returning computed paths."
                                         :coerce :symbol}
                              :index {:desc "Override the name of the index file (default `index.clj|md`), will be added to paths."}
+                             :package {:desc "Indicates how the static build should be packaged"
+                                       :coerce :keyword
+                                       :default :directory
+                                       :validate #{:directory :single-file}}
                              :bundle {:desc "Flag to build a self-contained html file inlcuding inlined images"}
                              :browse {:desc "Opens the browser on boot when set."}
                              :dashboard {:desc "Flag to serve a dashboard with the build progress."}

src/nextjournal/clerk/builder.clj
@@ -129,18 +129,10 @@
 (def builtin-index
   (io/resource "nextjournal/clerk/index.clj"))
 
-(defn validate-render-router! [{:as opts :keys [render-router bundle?]}]
-  (if bundle?
-    (when (not= render-router :bundle)
-      (throw (ex-info "Incompatible options: `:bundle?` implies `:render-router` is `:bundle`." {:opts opts})))
-    (when (not= render-router :fetch-edn)
-      (throw (ex-info "Only `:fetch-edn` value is currently allowed for the `:render-router` mode. A value of `:bundle` is currently implied by the `:bundle?` option." {:opts opts})))))
-
-(defn process-build-opts [{:as opts :keys [paths index expand-paths? render-router bundle?]}]
-  (when render-router
-    (validate-render-router! opts))
+(defn process-build-opts [{:as opts :keys [package index expand-paths?]}]
   (merge {:out-path default-out-path
-          :bundle? false
+          :package :directory
+          :render-router :fetch-edn
           :browse? false
           :report-fn (if @webserver/!server build-ui-reporter stdout-reporter)}
          (let [opts+index (cond-> opts
@@ -150,7 +142,7 @@
            (-> opts'
                (update :resource->url #(merge {} %2 %1) @config/!resource->url)
                (cond-> #_opts'
-                 (and bundle? (not render-router))
+                 (= :single-file package)
                  (assoc :render-router :bundle)
                  expand-paths?
                  (dissoc :expand-paths?)
@@ -163,10 +155,9 @@
 #_(process-build-opts {:paths ["notebooks/rule_30.clj"
                                "notebooks/markdown.md"] :expand-paths? true})
 
-(defn build-static-app-opts [{:as opts :keys [bundle? out-path browse? index]} docs]
+(defn build-static-app-opts [opts docs]
   (let [path->doc (into {} (map (juxt (comp str fs/strip-ext strip-index (partial viewer/map-index opts) :file) :viewer)) docs)]
     (assoc opts
-           :bundle? bundle?
            :path->doc path->doc
            :paths (vec (keys path->doc)))))
 
@@ -191,27 +182,27 @@
 
 (defn cleanup [build-opts]
   (select-keys build-opts
-               [:bundle? :render-router :path->doc :current-path :resource->url :exclude-js? :index :html]))
+               [:package :render-router :path->doc :current-path :resource->url :exclude-js? :index :html]))
 
 (defn write-static-app!
   [opts docs]
-  (let [{:keys [bundle? render-router out-path browse? ssr?]} opts
+  (let [{:keys [package out-path browse? ssr?]} opts
         index-html (str out-path fs/file-separator "index.html")
         {:as static-app-opts :keys [path->doc]} (build-static-app-opts opts docs)]
     (when-not (contains? (set (keys path->doc)) "")
       (throw (ex-info "Index must have been processed at this point" {:static-app-opts static-app-opts})))
     (when-not (fs/exists? (fs/parent index-html))
       (fs/create-dirs (fs/parent index-html)))
-    (if bundle?
+    (if (= :single-file package)
       (spit index-html (view/->html (cleanup static-app-opts)))
       (doseq [[path doc] path->doc]
         (let [out-html (fs/file out-path path "index.html")]
           (fs/create-dirs (fs/parent out-html))
-          (when (= :fetch-edn render-router)
-            (spit (fs/file out-path (str (or (not-empty path) "index") ".edn"))
-                  (viewer/->edn doc)))
+          (spit (fs/file out-path (str (or (not-empty path) "index") ".edn"))
+                (viewer/->edn doc))
           (spit out-html (view/->html (-> static-app-opts
-                                          (assoc :path->doc (hash-map path doc) :current-path path)
+                                          (dissoc :path->doc)
+                                          (assoc :current-path path)
                                           (cond-> ssr? ssr!)
                                           cleanup))))))
     (when browse?
@@ -262,14 +253,14 @@
 
 (defn doc-url
   ([opts file path] (doc-url opts file path nil))
-  ([opts file path fragment]
-   (if (:bundle? opts)
+  ([{:as opts :keys [package]} file path fragment]
+   (if (= :single-file package)
      (cond-> (str "#/" path)
        fragment (str ":" fragment))
      (str (viewer/relative-root-prefix-from (viewer/map-index opts file)) path
           (when fragment (str "#" fragment))))))
 
-(defn build-static-app! [{:as opts :keys [bundle?]}]
+(defn build-static-app! [opts]
   (let [{:as opts :keys [download-cache-fn upload-cache-fn report-fn compile-css? expanded-paths error]}
         (process-build-opts (assoc opts :expand-paths? true))
         start (System/nanoTime)
@@ -303,7 +294,6 @@
                                                                             viewer/doc-url (partial doc-url opts file)]
                                                                     (let [doc (eval/eval-analyzed-doc doc)]
                                                                       (assoc doc :viewer (view/doc->viewer (assoc opts
-                                                                                                                  :static-build? true
                                                                                                                   :nav-path (if (instance? java.net.URL file)
                                                                                                                               (str "'" (:ns doc))
                                                                                                                               (str file)))
@@ -330,24 +320,21 @@
     (report-fn {:stage :finished :state state :duration duration :total-duration (eval/elapsed-ms start)})))
 
 (comment
-  (build-static-app! {:paths clerk-docs :bundle? true})
+  (build-static-app! {:paths clerk-docs :package :single-file})
   (build-static-app! {:paths ["notebooks/editor.clj"] :browse? true})
   (build-static-app! {:paths ["CHANGELOG.md" "notebooks/editor.clj"] :browse? true})
-  (build-static-app! {:paths ["index.clj" "notebooks/links.md" "notebooks/rule_30.clj" "notebooks/markdown.md"] :bundle? true :browse? true})
-  (build-static-app! {:paths ["notebooks/links.md" "notebooks/rule_30.clj" "notebooks/markdown.md"] :bundle? true :browse? true})
-  (build-static-app! {:paths ["index.clj" "notebooks/rule_30.clj" "notebooks/markdown.md"] :bundle? false :browse? true})
+  (build-static-app! {:paths ["index.clj" "notebooks/links.md" "notebooks/rule_30.clj" "notebooks/markdown.md"] :browse? true})
+  (build-static-app! {:paths ["notebooks/links.md" "notebooks/rule_30.clj" "notebooks/markdown.md"] :package :single-file})
+  (build-static-app! {:paths ["index.clj" "notebooks/rule_30.clj" "notebooks/markdown.md"] :browse? true})
   (build-static-app! {:paths ["notebooks/viewers/**"]})
   (build-static-app! {:index "notebooks/rule_30.clj" :git/sha "bd85a3de12d34a0622eb5b94d82c9e73b95412d1" :git/url "https://github.com/nextjournal/clerk"})
   (reset! config/!resource->url @config/!asset-map)
   (swap! config/!resource->url dissoc "/css/viewer.css")
 
-  (build-static-app! {:bundle? true
-                      :render-router :bundle
-                      :browse true
+  (build-static-app! {:browse true
                       :index "notebooks/rule_30.clj"})
 
-  (build-static-app! {:render-router :fetch-edn
-                      :index "notebooks/document_linking.clj"
+  (build-static-app! {:index "notebooks/document_linking.clj"
                       :paths ["notebooks/viewers/html.clj" "notebooks/rule_30.clj"]})
 
   (build-static-app! {:ssr? true
@@ -361,7 +348,10 @@
                       ;; test against cljs release `bb build:js`
                       :resource->url {"/js/viewer.js" "./build/viewer.js"}
                       :index "notebooks/rule_30.clj"})
+
   (fs/delete-tree "public/build")
+  (print (:out (sh "tree public/build")))
+
   (build-static-app! {:compile-css? true
                       :index "notebooks/rule_30.clj"
                       :paths ["notebooks/hello.clj"
@@ -370,9 +360,12 @@
                       :resource->url {"/js/viewer.js" "/viewer.js"}
                       :paths ["notebooks/cherry.clj"]
                       :out-path "build"})
-  (build-static-app! {:paths ["CHANGELOG.md"
+
+  ;; test doc-url links
+  (build-static-app! {:index "notebooks/document_linking.clj"
+                      :paths ["CHANGELOG.md"
                               "notebooks/markdown.md"
-                              "notebooks/viewers/html.clj"]
-                      :bundle? true
+                              "notebooks/viewers/html.clj"
+                              "notebooks/viewers/image.clj"]
                       :git/sha "d60f5417"
                       :git/url "https://github.com/nextjournal/clerk"}))

src/nextjournal/clerk/viewer.cljc
@@ -462,7 +462,6 @@
                    (store+get-cas-url! (assoc doc+blob-opts :ext image-type) value)))
        result)))
 
-#_(nextjournal.clerk.builder/build-static-app! {:paths ["image.clj" "notebooks/image.clj" "notebooks/viewers/image.clj"] :bundle? false :browse? false})
 #_(nextjournal.clerk.builder/build-static-app! {:paths ["image.clj" "notebooks/image.clj" "notebooks/viewers/image.clj"] :browse? false})
 
 #?(:clj
@@ -512,12 +511,12 @@
 
 (defn transform-result [{:as wrapped-value :keys [path]}]
   (let [{:as _cell :keys [form id settings] ::keys [result doc]} (:nextjournal/value wrapped-value)
-        {:keys [static-build? bundle?]} doc
+        {:keys [package]} doc
         {:nextjournal/keys [value blob-id viewers]} result
         blob-mode (cond
-                    (and (not static-build?) blob-id) :lazy-load
-                    bundle? :inline ;; TODO: provide a separte setting for this
-                    :else :file)
+                    (= :single-file package) :inline
+                    (= :directory package) :file
+                    blob-id :lazy-load)
         #?(:clj blob-opts :cljs _) (assoc doc :blob-mode blob-mode :blob-id blob-id)
         opts-from-block (-> settings
                             (select-keys (keys viewer-opts-normalization))
@@ -569,14 +568,14 @@
     (md.parser/insert-sidenote-containers (assoc cell-doc :footnotes footnotes))
     cell-doc))
 
-(defn process-image-source [src {:as doc :keys [file bundle?]}]
+(defn process-image-source [src {:as doc :keys [file package]}]
   #?(:cljs src
      :clj  (cond
              (not (fs/exists? src)) src
-             (false? bundle?) (str (relative-root-prefix-from (map-index doc file))
-                                   (store+get-cas-url! (assoc doc :ext (fs/extension src))
-                                                       (fs/read-all-bytes src)))
-             bundle? (data-uri-base64-encode (fs/read-all-bytes src) (Files/probeContentType (fs/path src)))
+             (= :directory package) (str (relative-root-prefix-from (map-index doc file))
+                                         (store+get-cas-url! (assoc doc :ext (fs/extension src))
+                                                             (fs/read-all-bytes src)))
+             (= :single-file package) (data-uri-base64-encode (fs/read-all-bytes src) (Files/probeContentType (fs/path src)))
              :else (str "/_fs/" src))))
 
 #?(:clj
@@ -1163,7 +1162,7 @@
             ""
             (if (fs/exists? "index.clj") "index.clj" "'nextjournal.clerk.index"))))
 
-(defn header [{:as opts :keys [file file-path nav-path static-build? ns] :git/keys [url sha]}]
+(defn header [{:as opts :keys [file-path nav-path package ns] :git/keys [url sha]}]
   (html [:div.viewer.w-full.max-w-prose.px-8.not-prose.mt-3
          [:div.mb-8.text-xs.sans-serif.text-slate-400
           (when (and (not (route-index? opts))
@@ -1178,7 +1177,7 @@
               {:href (doc-url (index-path opts))} "Index"]
              [:span.mx-2 "•"]])
           [:span
-           (if static-build? "Generated with " "Served from ")
+           (if package "Generated with " "Served from ")
            [:a.font-medium.border-b.border-dotted.border-slate-300.hover:text-indigo-500.hover:border-indigo-500.dark:border-slate-500.dark:hover:text-white.dark:hover:border-white.transition
             {:href "https://clerk.vision"} "Clerk"]
            (let [default-index? (= 'nextjournal.clerk.index (some-> ns ns-name))]
@@ -1216,7 +1215,7 @@
       (update :file str)
 
       (select-keys [:atom-var-name->state
-                    :blocks :bundle?
+                    :blocks :package
                     :doc-css-class
                     :error
                     :file

commit 2e953be7ed02e092f7ede867645a76320bbeb744

Fix scrolling to anchors in serve mode
a regression introduced with f8377d6116ccecee00ecf1dcc2fa843555cac9d8

src/nextjournal/clerk/render.cljs
@@ -112,7 +112,7 @@
   (r/with-let [root-ref-fn (fn [el]
                              (when (and el (exists? js/document))
                                (code/setup-dark-mode!)
-                               (when-some [heading (when (and (exists? js/location) (= :directory package))
+                               (when-some [heading (when (and (exists? js/location) (not= :single-file package))
                                                      (try (some-> js/location .-hash not-empty js/decodeURI (subs 1) js/document.getElementById)
                                                           (catch js/Error _
                                                             (js/console.warn (str "Clerk render-notebook, invalid hash: "
@@ -124,7 +124,7 @@
      [:div.fixed.top-2.left-2.md:left-auto.md:right-2.z-10
       [dark-mode-toggle]]
      (when (and toc toc-visibility)
-       [navbar/view toc (assoc render-opts :set-hash? (= :directory package) :toc-visibility toc-visibility)])
+       [navbar/view toc (assoc render-opts :set-hash? (not= :single-file package) :toc-visibility toc-visibility)])
      [:div.flex-auto.w-screen.scroll-container
       (into
        [:> (.-div motion)