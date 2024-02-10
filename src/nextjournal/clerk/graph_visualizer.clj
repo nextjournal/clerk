ec2c6823b74ed8b3200efa057cc641c5ca2f47b4
Andrea Amantini <lo.zampino@gmail.com>
2024-02-10 12:47:01 +0100

    Add ns filter to graph visualizer

--- src/nextjournal/clerk/graph_visualizer.clj
+++ src/nextjournal/clerk/graph_visualizer.clj
@@ -10,6 +10,7 @@
    (arrowic/as-svg
     (arrowic/with-graph (arrowic/create-graph)
       (let [vars->verticies (into {} (map (juxt identity arrowic/insert-vertex!)) (keys ->analysis-info))]
+        (assert (not-empty vars->verticies))
         (doseq [var (keys ->analysis-info)]
           (doseq [dep (dep/immediate-dependencies graph var)]
             (when (and (vars->verticies var)
@@ -17,14 +18,22 @@
               (arrowic/insert-edge! (vars->verticies var) (vars->verticies dep))))))))))
 
 
-(defn graph-file [file]
-  (-> (analyzer/analyze-file {:graph (dep/graph)} file )
-      analyzer/build-graph
-      dep-svg))
+(defn graph-file
+  ([file] (graph-file {} file))
+  ([{:keys [key-filter-fn] :or {key-filter-fn identity}} file]
+   (-> (analyzer/analyze-file {:graph (dep/graph)} file)
+       analyzer/build-graph
+       (update :->analysis-info
+               (partial into {}
+                        (filter (comp key-filter-fn key))))
+       dep-svg)))
 
 ^{::clerk/width :full}
 (graph-file "src/nextjournal/clerk/classpath.clj")
 
-
 ^{::clerk/width :full}
-(graph-file "src/nextjournal/clerk/parser.cljc")
+(graph-file {:key-filter-fn (comp #{"nextjournal.clerk.parser"
+                                    "nextjournal.clerk.analyzer"
+                                    "nextjournal.clerk.classpath"
+                                    "nextjournal.clerk.config"} namespace)}
+            "src/nextjournal/clerk/analyzer.clj")
