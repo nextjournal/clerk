1dc17ddcf24fe639cfb5431cf60b55f1dd08b277
Michiel Borkent <michielborkent@gmail.com>
2024-09-03 17:28:22 +0200

    Add transitive test

--- test/nextjournal/clerk/fixtures/require_cljs_with_transitive_missing_cljs_file.clj
+++ test/nextjournal/clerk/fixtures/require_cljs_with_transitive_missing_cljs_file.clj
@@ -1,9 +1,9 @@
-(ns nextjournal.clerk.fixtures.require-cljs-with-missing-cljs-file
+(ns nextjournal.clerk.fixtures.require-cljs-with-transitive-missing-cljs-file
   (:require [nextjournal.clerk :as clerk]))
 
 (def viewer
   {:require-cljs true
-   :render-fn 'not-existing/dude
+   :render-fn 'nextjournal.clerk.fixtures.render-fns/foobar
    :transform-fn identity})
 
 (clerk/with-viewer viewer 1)
--- test/nextjournal/clerk/require_cljs_test.clj
+++ test/nextjournal/clerk/require_cljs_test.clj
@@ -3,7 +3,10 @@
             [nextjournal.clerk :as clerk]))
 
 (deftest throw-on-missing-cljs-source-test
-  (is (thrown-with-msg? Exception #"Could not find" (clerk/show! 'nextjournal.clerk.fixtures.require-cljs-with-missing-cljs-file))))
+  (is (thrown-with-msg? Exception #"Could not find source for CLJS namespace: not-existing"
+                        (clerk/show! 'nextjournal.clerk.fixtures.require-cljs-with-missing-cljs-file)))
+  (is (thrown-with-msg? Exception #"Could not find source for CLJS namespace: non-existing-namespace"
+                        (clerk/show! 'nextjournal.clerk.fixtures.require-cljs-with-transitive-missing-cljs-file))))
 
 ;;;; Scratch
 
