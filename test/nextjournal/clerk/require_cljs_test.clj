91c5d71df43049e5f296bc9d0960d015c1d6e125
Michiel Borkent <michielborkent@gmail.com>
2024-09-03 17:09:21 +0200

    Add test for missing cljs source

---
+++ test/nextjournal/clerk/require_cljs_test.clj
@@ -0,0 +1,12 @@
+(ns nextjournal.clerk.require-cljs-test
+  (:require [clojure.test :as t :refer [deftest is]]
+            [nextjournal.clerk :as clerk]))
+
+(deftest throw-on-missing-cljs-source-test
+  (is (thrown-with-msg? Exception #"Could not find" (clerk/show! 'nextjournal.clerk.fixtures.require-cljs-with-missing-cljs-file))))
+
+;;;; Scratch
+
+(comment
+  (t/run-tests)
+  )
