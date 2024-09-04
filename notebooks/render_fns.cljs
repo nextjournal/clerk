8df4baac59942ef0688a004956c93d6e6b8360ff
Martin Kavalar <martin@nextjournal.com>
2024-09-04 11:06:02 +0200

    Undo render change

--- notebooks/render_fns.cljs
+++ notebooks/render_fns.cljs
@@ -2,8 +2,5 @@
   "An illustration on how to write Clerk render functions in a cljs file."
   (:require ["framer-motion" :as framer-motion :refer [motion]]))
 
-;; Let's try this out.
-
 (defn motion-div [props]
   [:> (.-div motion) props])
-
