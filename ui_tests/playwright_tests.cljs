79c799282f135cc257b585ccef8d8f6512c45c2e
Michiel Borkent <michielborkent@gmail.com>
2024-09-09 18:29:30 +0200

    wip

--- ui_tests/playwright_tests.cljs
+++ ui_tests/playwright_tests.cljs
@@ -46,15 +46,22 @@
 
 (def console-errors (atom []))
 
-(defn test-notebook [page link]
-  (println "Visiting" link)
-  (p/do (goto page link)
-        (prn :done :gotoing)
-        #_(p/delay 50000)
-        (p/let [loc (.locator page "div")
-                loc (.first loc #js {:timeout 10000})
-                visible? (.isVisible loc #js {:timeout 10000})]
-          (is visible?))))
+(defn test-notebook
+  ([page url]
+   (println "Visiting" url)
+   (p/do (goto page url)
+         (p/let [loc (.locator page "div")
+                 loc (.first loc #js {:timeout 10000})
+                 visible? (.isVisible loc #js {:timeout 10000})]
+           (is visible?))))
+  ([page url link]
+   (p/let [txt (.innerText link)]
+     (println "Visiting" (str url "#/" txt))
+     (p/do (.click link)
+           (p/let [loc (.locator page "div")
+                   loc (.first loc #js {:timeout 10000})
+                   visible? (.isVisible loc #js {:timeout 10000})]
+             (is visible?))))))
 
 (deftest index-page-test
   (async done
@@ -68,21 +75,19 @@
                 _ (.on page "pageerror"
                        (fn [msg]
                          (swap! console-errors conj {:msg msg :notebook (.url page)})))]
-          (let [{:keys [index url]} (:index @!opts)]
+          (let [{:keys [index url]} @!opts]
             (if (false? index)
               (test-notebook page url)
-              (let [url (:url @!opts)]
-                (-> (p/let [_ (goto page url)
-                            _ (is (-> (.locator page "h1:has-text(\"Clerk\")")
-                                      (.isVisible #js {:timeout 10000})))
-                            links (-> (.locator page "text=/.*\\.clj$/i")
-                                      (.allInnerTexts))
-                            _ (is (pos? (count links)))
-                            links (map (fn [link]
-                                         (str url "#/" link)) links)
-                            #_#_links (filter (fn [link]
-                                            (str/includes? link "cherry")) links)]
-                      (p/run! #(test-notebook page %) links))))))
+              (-> (p/let [_ (goto page url)
+                          _ (is (-> (.locator page "h1:has-text(\"Clerk\")")
+                                    (.isVisible #js {:timeout 10000})))
+                          links (-> (.locator page "text=/.*\\.clj$/i")
+                                    (.all))
+                          _ (is (pos? (count links)))
+                          #_#_links (filter (fn [link]
+                                              (str/includes? link "cherry")) links)]
+                    (p/run! #(p/do (test-notebook page url %)
+                                   (.goBack page)) links)))))
           #_(p/delay 30000)
           (is (zero? (count @console-errors))
               (str/join "\n" (map (fn [{:keys [msg notebook]}]
@@ -136,7 +141,15 @@
   (.on p "console" (fn [msg]
                      (when (= "error" (.type msg))
                        (swap! console-errors conj msg))))
-  (await (goto p "https://snapshots.nextjournal.com/clerk/build/549f9956870c69ef0951ca82d55a8e5ec2e49ed4/index.html"))
+  (def url "https://snapshots.nextjournal.com/clerk/build/c617e6fae2734a75ef4c53b5c410a76cc0a52160/index.html")
+  (await (goto p url))
   (def loc (.first (.locator p "text=Clerk")))
   (await (.isVisible loc #js {:timeout 1000}))
+  (def links (await (-> (.locator p "text=/.*\\.clj$/i")
+                        #_(.allInnerTexts)
+                        (.all))))
+  (await (.click (second links)))
+  (def links (map (fn [link]
+                    (str url "#/" link)) links))
+  (goto p "https://snapshots.nextjournal.com/clerk/build/c617e6fae2734a75ef4c53b5c410a76cc0a52160/index.html#/notebooks/cards.clj")
   )
