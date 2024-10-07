bbf83caca728eea8c4bc064afebe959efe4f037a
Nikita Prokopov <niki@tonsky.me>
2024-10-07 16:51:52 +0200

    Check ns is required before sci-copy-nss

--- src/nextjournal/clerk/render/macros.clj
+++ src/nextjournal/clerk/render/macros.clj
@@ -2,7 +2,10 @@
 
 (defmacro sci-copy-nss [& nss]
   (into {} (for [[_ ns] nss]
-             [(list 'quote ns) `(sci.core/copy-ns ~ns (sci.core/create-ns '~ns))])))
+             `[(quote ~ns)
+               (if (empty? (ns-publics (quote ~ns)))
+                 (throw (ex-info (str "Namespace " (quote ~ns) " is empty, forgot to require?") {:ns (quote ~ns)}))
+                 (sci.core/copy-ns ~ns (sci.core/create-ns '~ns)))])))
 
 #_(macroexpand '(sci-copy-nss 'nextjournal.clerk.render.hoooks
                               'nextjournal.clerk.render.code))
