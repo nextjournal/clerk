1bbf7ba72143dad638460fd702bfe387c479ef36
Michiel Borkent <michielborkent@gmail.com>
2024-12-09 17:23:34 +0100

    wip

--- notebooks/sci_import_test.clj
+++ notebooks/sci_import_test.clj
@@ -2,6 +2,8 @@
   (:require [nextjournal.clerk :as clerk]
             [clojure.core :as b]))
 
+1
+
 (clerk/with-viewer
   {:render-fn
    '(fn [_]
@@ -10,6 +12,8 @@
         (js/import "https:/esm.sh/date-fns@3.0.0/index.mjs")
         (.then
          (fn [lib]
+           (js/console.log :>1 lib.formatRelative)
+           (js/console.log :>2 lib.subDays)
            (nextjournal.clerk/html
             [:pre (lib.formatRelative (lib.subDays (js/Date.) 2) (js/Date.))]))))))}
   nil)
