6943827733a9e4062301b812c6bf431ac367ea1b
Michiel Borkent <michielborkent@gmail.com>
2025-03-17 16:00:27 +0100

    wip [skip ci]

---
+++ notebooks/qualified_methods.clj
@@ -0,0 +1,18 @@
+(ns qualified-methods)
+
+(String/.length "foo")
+
+(map String/.length ["f" "fo" "foo"])
+
+String/1
+
+Integer/parseInt
+
+;; TODO:
+;; (String/new "dude")
+
+;; TODO
+;; (map String/new ["dude"])
+
+;; TODO
+;; (map Integer/parseInt ["1" "2" "3"])
