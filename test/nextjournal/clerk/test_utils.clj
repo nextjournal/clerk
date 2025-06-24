00f010eb05bdfc0c5913de62ffc1318d442090d7
Michiel Borkent <michielborkent@gmail.com>
2025-06-24 11:38:23 +0200

    Add timings

--- test/nextjournal/clerk/test_utils.clj
+++ test/nextjournal/clerk/test_utils.clj
@@ -2,12 +2,16 @@
   (:require [clojure.test :as t]))
 
 
+(def timer (atom nil))
+
 (defmethod t/report :begin-test-var [m]
   (binding [*out* *err*]
     (println "===" (-> m :var meta :name))
-    (println)))
+    (reset! timer (. System (nanoTime)))))
 
 (defmethod t/report :end-test-var [_m]
+  (println (str "Elapsed time: " (/ (double (- (. System (nanoTime)) @timer)) 1000000.0) " msecs"))
+  (println)
   (when-let [rc t/*report-counters*]
     (when-let [{:keys [:fail :error]} @rc]
       (when (and (= "true" (System/getenv "FAIL_FAST"))
