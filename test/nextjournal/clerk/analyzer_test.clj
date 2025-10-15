9ad61ff0710dcc21e3513094b9eb87adc9a26cb4
Martin Kavalar <martin@nextjournal.com>
2025-10-15 18:06:49 +1100

    Try removig with-ns-bindings from test

--- test/nextjournal/clerk/analyzer_test.clj
+++ test/nextjournal/clerk/analyzer_test.clj
@@ -389,8 +389,7 @@ my-uuid")]
       (is (empty? (ana/unhashed-deps ->analysis-info)))
       (is (match? {:jar string?} (->analysis-info 'weavejester.dependency/graph)))))
   (testing "should establish dependencies across files"
-    (let [{:keys [graph]} (with-ns-binding 'nextjournal.clerk
-                            (analyze-string (slurp "src/nextjournal/clerk.clj")))]
+    (let [{:keys [graph]} (analyze-string (slurp "src/nextjournal/clerk.clj"))]
       (is (dep/depends? graph 'nextjournal.clerk/show! 'nextjournal.clerk.analyzer/hash)))))
 
 (deftest graph-nodes-with-anonymous-ids
@@ -417,11 +416,10 @@ my-uuid")]
     (is (empty?
          (let [!missing-hash-store (atom [])]
            (reset! ana/!file->analysis-cache {})
-           (with-ns-binding 'nextjournal.clerk
-             (-> (parser/parse-file {:doc? true} "src/nextjournal/clerk.clj")
-                 ana/build-graph
-                 (assoc :record-missing-hash-fn (fn [report-entry] (swap! !missing-hash-store conj report-entry)))
-                 ana/hash))
+           (-> (parser/parse-file {:doc? true} "src/nextjournal/clerk.clj")
+               ana/build-graph
+               (assoc :record-missing-hash-fn (fn [report-entry] (swap! !missing-hash-store conj report-entry)))
+               ana/hash)
            (deref !missing-hash-store)))))
 
   (testing "known cases where missing hashes occur"
