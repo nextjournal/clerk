f2f2cf447e3b845113092ebf63cd350f2ea96083
Martin Kavalar <martin@nextjournal.com>
2025-11-24 14:47:29 +1100

    Adjust test

--- test/nextjournal/clerk/viewer_test.clj
+++ test/nextjournal/clerk/viewer_test.clj
@@ -241,7 +241,7 @@
 (deftest present
   (testing "only transform-fn can select viewer"
     (is (match? {:nextjournal/value [:div.viewer.markdown-viewer.w-full.max-w-prose.px-8 {}
-                                     ["h1" {:id "hello-markdown!"} [:<> "👋 Hello "] [:em [:<> "markdown"]] [:<> "!"]]]
+                                     [:h1 {:id "hello-markdown!"} [:<> "👋 Hello "] [:em [:<> "markdown"]] [:<> "!"]]]
                  :nextjournal/viewer {:name `v/markdown-node-viewer}}
                 (v/present (v/with-viewer {:transform-fn (comp v/md v/->value)}
                              "# 👋 Hello _markdown_!")))))
